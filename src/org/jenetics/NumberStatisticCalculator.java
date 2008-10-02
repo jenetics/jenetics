85df9a83840fa1e1da52a0bd51eaed4b49be4e9e
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2008-10-02 19:40:17 +0000

    Implement concurrency support.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%4070

--- src/org/jenetics/NumberStatisticCalculator.java
+++ src/org/jenetics/NumberStatisticCalculator.java
@@ -24,26 +24,26 @@ import java.util.List;
 
 import javolution.context.StackContext;
 
-import org.jscience.mathematics.number.Number;
+import org.jscience.mathematics.structure.Ring;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version $Id: NumberStatisticCalculator.java,v 1.2 2008-08-28 21:21:13 fwilhelm Exp $
+ * @version $Id: NumberStatisticCalculator.java,v 1.3 2008-10-02 19:40:17 fwilhelm Exp $
  */
-class NumberStatisticCalculator<G extends Gene<?>, N extends Number<N>>
-	extends StatisticCalculator<G, N>
+class NumberStatisticCalculator<G extends Gene<?>, R extends Ring<R> & Comparable<R>>
+	extends StatisticCalculator<G, R>
 {
 
 	public NumberStatisticCalculator() {
 	}
 	
 	@Override
-	public NumberStatistic<G, N> evaluate(final List<? extends Phenotype<G, N>> population) {
-		final Statistic<G, N> s = super.evaluate(population);
+	public NumberStatistic<G, R> evaluate(final List<? extends Phenotype<G, R>> population) {
+		final Statistic<G, R> s = super.evaluate(population);
 		final int size = population.size();
 		
-		N fitnessSum = null;
-		N fitnessSquareSum = null;
+		R fitnessSum = null;
+		R fitnessSquareSum = null;
 		if (size > 0) {
 			fitnessSum = population.get(0).getFitness();
 			fitnessSquareSum = fitnessSum.times(fitnessSum);
@@ -52,9 +52,9 @@ class NumberStatisticCalculator<G extends Gene<?>, N extends Number<N>>
 		StackContext.enter();
 		try {
 			for (int i = 1; i < size; ++i) {
-				final Phenotype<G, N> phenotype = population.get(i); 
+				final Phenotype<G, R> phenotype = population.get(i); 
 
-				N fitness = phenotype.getFitness();
+				R fitness = phenotype.getFitness();
 				fitnessSum = fitnessSum.plus(fitness);
 				fitnessSquareSum = fitnessSquareSum.plus(fitness.times(fitness));
 			}
@@ -63,78 +63,78 @@ class NumberStatisticCalculator<G extends Gene<?>, N extends Number<N>>
 			StackContext.exit();
 		}
 		
-		return new NumberStatistic<G, N>(s, null, null);
+		return new NumberStatistic<G, R>(s, null, null);
 	}
 	
-	static double torben(double m[]){
-		int i;
-		int less;
-		int greater;
-		int equal;
-		double min = m[0];
-		double max = m[0];
-		double guess;
-		double maxltguess;
-		double mingtguess;
-		
-		for (i = 1 ; i < m.length; i++) {
-			if (m[i] < min) {
-				min = m[i];
-			}
-			if (m[i] > max) {
-				max = m[i];
-			}
-		}
-		
-		while (true) {
-			guess = (min+max)/2;
-			less = 0; 
-			greater = 0; 
-			equal = 0;
-			maxltguess = min;
-			mingtguess = max;
-			
-			for (i = 0; i < m.length; i++) {
-				if (m[i] < guess) {
-					less++;
-					if (m[i] > maxltguess) {
-						maxltguess = m[i];
-					}
-				} else if (m[i] > guess) {
-					greater++;
-					if (m[i] < mingtguess) {
-						mingtguess = m[i] ;
-					}
-				} else {
-					equal++;
-				}
-			}
-			if (less <= (m.length + 1)/2 && greater <= (m.length + 1)/2) {
-				break ;
-			} else if (less > greater) {
-				max = maxltguess ;
-			} else {
-				min = mingtguess;
-			}
-		}
-		
-		if (less >= (m.length + 1)/2) {
-			return maxltguess;
-		} else if (less+equal >= (m.length + 1)/2) {
-			return guess;
-		} else {
-			return mingtguess;
-		}
-	}
-	
-	public static void main(String[] args) {
-		double[] array = new double[1001];
-		for (int i = 0; i < array.length; ++i) {
-			array[i] = i;
-		}
-		
-		System.out.println(torben(array));
-	}
+//	static double torben(double m[]){
+//		int i;
+//		int less;
+//		int greater;
+//		int equal;
+//		double min = m[0];
+//		double max = m[0];
+//		double guess;
+//		double maxltguess;
+//		double mingtguess;
+//		
+//		for (i = 1 ; i < m.length; i++) {
+//			if (m[i] < min) {
+//				min = m[i];
+//			}
+//			if (m[i] > max) {
+//				max = m[i];
+//			}
+//		}
+//		
+//		while (true) {
+//			guess = (min+max)/2;
+//			less = 0; 
+//			greater = 0; 
+//			equal = 0;
+//			maxltguess = min;
+//			mingtguess = max;
+//			
+//			for (i = 0; i < m.length; i++) {
+//				if (m[i] < guess) {
+//					less++;
+//					if (m[i] > maxltguess) {
+//						maxltguess = m[i];
+//					}
+//				} else if (m[i] > guess) {
+//					greater++;
+//					if (m[i] < mingtguess) {
+//						mingtguess = m[i] ;
+//					}
+//				} else {
+//					equal++;
+//				}
+//			}
+//			if (less <= (m.length + 1)/2 && greater <= (m.length + 1)/2) {
+//				break ;
+//			} else if (less > greater) {
+//				max = maxltguess ;
+//			} else {
+//				min = mingtguess;
+//			}
+//		}
+//		
+//		if (less >= (m.length + 1)/2) {
+//			return maxltguess;
+//		} else if (less+equal >= (m.length + 1)/2) {
+//			return guess;
+//		} else {
+//			return mingtguess;
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		double[] array = new double[1001];
+//		for (int i = 0; i < array.length; ++i) {
+//			array[i] = i;
+//		}
+//		
+//		System.out.println(torben(array));
+//	}
 
 }
 
