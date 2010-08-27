57e743a19ec9bcc16fa3d895d1f9e679baf930e7
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2010-08-27 13:33:31 +0000

    Comment
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%40518

--- src/main/java/org/jenetics/Crossover.java
+++ src/main/java/org/jenetics/Crossover.java
@@ -81,7 +81,7 @@ public abstract class Crossover<G extends Gene<?, G>> extends Recombination<G> {
 
 	/**
 	 * Template method which performs the crossover. The arguments given are 
-	 * mutable non null arrays. The two arrays have the same length.
+	 * mutable non null arrays of the same length.
 	 */
 	protected abstract void crossover(final Array<G> that, final Array<G> other);
 
