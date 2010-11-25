b8eca7c0e71b2ac10275efddd37543125c667ec8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2010-11-25 22:13:46 +0000

    Add test for GaussianMutator class.

--- src/test/java/org/jenetics/GaussianMutatorTest.java
+++ src/test/java/org/jenetics/GaussianMutatorTest.java
@@ -22,6 +22,18 @@
  */
 package org.jenetics;
 
+import java.util.Random;
+
+import org.jenetics.stat.Distribution;
+import org.jenetics.stat.Distribution.Domain;
+import org.jenetics.stat.Histogram;
+import org.jenetics.stat.NormalDistribution;
+import org.jenetics.stat.Variance;
+import org.jscience.mathematics.function.Function;
+import org.jscience.mathematics.number.Float64;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
@@ -32,6 +44,37 @@ public class GaussianMutatorTest extends MutatorTestBase {
 	@Override
 	public Mutator<Float64Gene> newMutator(double p) {
 		return new GaussianMutator<Float64Gene>(p);
+	}
+	
+	@Test(invocationCount = 20, successPercentage = 95)
+	public void mutate() {
+		final Random random = new Random();
+		
+		final double min = 0;
+		final double max = 10;
+		final double mean = 5;
+		final double var = Math.pow((max - min)/4.0, 2);
+		
+		final Float64Gene gene = Float64Gene.valueOf(mean, min, max);
+		final GaussianMutator<Float64Gene> mutator = new GaussianMutator<Float64Gene>();
+		
+		final Histogram<Double> histogram = Histogram.valueOf(0.0, 10.0, 10);
+		final Variance<Double> variance = new Variance<Double>();
+		
+		for (int i = 0; i < 10000; ++i) {
+			final double value = mutator.mutate(gene, random).doubleValue();
+		
+			histogram.accumulate(value);
+			variance.accumulate(value);
+		}
+		
+		final Domain<Double> domain = new Domain<Double>(min, max);
+		final Distribution<Double> distribution = 
+			new NormalDistribution<Double>(domain, mean, var);
+		
+		final Function<Double, Float64> cdf = distribution.cdf();
+		final double χ2 = histogram.χ2(cdf);
+		Assert.assertTrue(χ2 < 28, String.format("χ2 must be smaller than 28: %f", χ2)); // TODO: remove magic number
 	}
 	
 }
