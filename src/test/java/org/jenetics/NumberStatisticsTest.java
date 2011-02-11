48fb3886de994b24957a2f6fb4757a82b8e09b33
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2011-02-11 19:00:00 +0000

    Incomplete - # 21: Code and test improvements https://sourceforge.net/apps/trac/jenetics/ticket/21

--- src/test/java/org/jenetics/NumberStatisticsTest.java
+++ src/test/java/org/jenetics/NumberStatisticsTest.java
@@ -1,39 +1,49 @@
 package org.jenetics;
 
-import java.io.IOException;
+import java.util.Random;
 
-import javolution.xml.stream.XMLStreamException;
+import org.jscience.mathematics.number.Float64;
 
-import org.testng.annotations.Test;
-
-import org.jenetics.util.SerializeUtils;
+import org.jenetics.util.Factory;
+import org.jenetics.util.ObjectTester;
+import org.jenetics.util.RandomRegistry;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @version $Id$
  */
-public class NumberStatisticsTest {
+public class NumberStatisticsTest 
+	extends ObjectTester<NumberStatistics<Float64Gene, Float64>> 
+{
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Test
-	public void xmlSerialize() throws XMLStreamException {
-		final NumberStatistics numberStatistics = new NumberStatistics(
-				Optimize.MAXIMUM, 234234, null, null, 
-				0, 0, 0, 3.234, 42.234, 23, 23, 23
-			);
-		
-		SerializeUtils.testXMLSerialization(numberStatistics);
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Test
-	public void objectSerialize() throws IOException {
-		final NumberStatistics numberStatistics = new NumberStatistics(
-				Optimize.MAXIMUM, 234234, null, null, 
-				0, 0, 0, 3.234, 42.234, 23, 23, 23
-			);
-		
-		SerializeUtils.testSerialization(numberStatistics);
+	final Factory<NumberStatistics<Float64Gene, Float64>>
+	_factory = new Factory<NumberStatistics<Float64Gene, Float64>>() {
+		@Override
+		public NumberStatistics<Float64Gene, Float64> newInstance() {
+			final Random random = RandomRegistry.getRandom();
+			
+			final NumberStatistics.Builder<Float64Gene, Float64> 
+			builder = new NumberStatistics.Builder<Float64Gene, Float64>();
+			builder.ageMean(random.nextDouble());
+			builder.ageVariance(random.nextDouble());
+			builder.invalid(random.nextInt(1000));
+			builder.killed(random.nextInt(1000));
+			builder.samples(random.nextInt(100000));
+			builder.generation(random.nextInt(1000));
+			builder.fitnessMean(random.nextDouble());
+			builder.fitnessVariance(random.nextDouble());
+			builder.standardError(random.nextDouble());
+			builder.bestPhenotype(TestUtils.newFloat64Phenotype());
+			builder.worstPhenotype(TestUtils.newFloat64Phenotype());
+			
+			return builder.build();
+		}
+	};
+	@Override
+	protected Factory<NumberStatistics<Float64Gene, Float64>> getFactory() {
+		return _factory;
 	}
 	
 }
+
+
