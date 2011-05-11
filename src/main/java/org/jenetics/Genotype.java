42a2e03c58648a3bfe27e2d8a6f75055cc784faa
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2011-05-11 19:46:40 +0000

    Incomplete - # 21: Code and test improvements https://sourceforge.net/apps/trac/jenetics/ticket/21

--- src/main/java/org/jenetics/Genotype.java
+++ src/main/java/org/jenetics/Genotype.java
@@ -172,7 +172,7 @@ public final class Genotype<T extends Gene<?, T>>
 	 */
 	@Override
 	public Genotype<T> newInstance() {
-		final Array<Chromosome<T>> chromosomes = new Array<Chromosome<T>>(length());
+		final Array<Chromosome<T>> chromosomes = new Array<Chromosome<T>>(length());		
 		for (int i = 0; i < length(); ++i) {
 			chromosomes.set(i, _chromosomes.get(i).newInstance());
 		}
