e83371b35bbed019daa18df9a51a0953d6f0639d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2011-08-30 19:49:17 +0000

    https://sourceforge.net/apps/trac/jenetics/ticket/40 Rename org.jenetics.util.Validator to org.jenetics.util.validation.
    --HG--
    rename : src/test/java/org/jenetics/util/ValidatorTest.java => src/test/java/org/jenetics/util/validationTest.java
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%40971

--- src/test/java/org/jenetics/util/validationTest.java
+++ src/test/java/org/jenetics/util/validationTest.java
@@ -29,7 +29,7 @@ import org.testng.annotations.Test;
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @version $Id$
  */
-public class ValidatorTest {
+public class validationTest {
 
 	@Test
 	public void rangeCheckPredicate1() {
