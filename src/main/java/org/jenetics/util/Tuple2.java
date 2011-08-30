cb94e55ef4808ed3d1e54549799fd6be336ec267
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2011-08-30 20:30:07 +0000

    --HG-- extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%40973

---
+++ src/main/java/org/jenetics/util/Tuple2.java
@@ -0,0 +1,68 @@
+/*
+ * Java Genetic Algorithm Library (@!identifier!@).
+ * Copyright (c) @!year!@ Franz Wilhelmstötter
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *     
+ */
+package org.jenetics.util;
+
+import static org.jenetics.util.ObjectUtils.eq;
+import static org.jenetics.util.ObjectUtils.hashCodeOf;
+
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version $Id: org.eclipse.jdt.ui.prefs 421 2010-03-18 22:41:17Z fwilhelm $
+ */
+class Tuple2<T1, T2> {
+	
+	public final T1 _1;
+	public final T2 _2;
+	
+	public Tuple2(final T1 t1, final T2 t2) {
+		_1 = t1;
+		_2 = t2;
+	}
+	
+	@Override
+	public int hashCode() {
+		return hashCodeOf(Tuple2.class).and(_1).and(_2).value();
+	}
+	
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj == null || obj.getClass() != getClass()) {
+			return false;
+		}
+		
+		final Tuple2<?, ?> tuple = (Tuple2<?, ?>)obj;
+		return eq(_1, tuple._1) && eq(_2, tuple._2);
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + _1 + ", " + _2 + ")";
+	}
+	
+}
+
+
