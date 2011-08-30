e5ca27809c718bb905e8a7af98ac33d080bbcd7c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2011-08-30 20:30:45 +0000

    --HG-- extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%40974

--- src/main/java/org/jenetics/util/Tuple2.java
+++ src/main/java/org/jenetics/util/Tuple2.java
@@ -28,7 +28,7 @@ import static org.jenetics.util.ObjectUtils.hashCodeOf;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version $Id: org.eclipse.jdt.ui.prefs 421 2010-03-18 22:41:17Z fwilhelm $
+ * @version $Id$
  */
 class Tuple2<T1, T2> {
 	
