8ad3a94d0a82e73a77a1b5a97291a9ba8e325e6c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-02-14 22:25:52 +0000

    Add serialize test class.

--- src/example/java/org/jenetics/examples/Serial.java
+++ src/example/java/org/jenetics/examples/Serial.java
@@ -1,3 +1,25 @@
+/*
+ * Java Genetic Algorithm Library (@!identifier!@).
+ * Copyright (c) @!year!@ Franz Wilhelmstötter
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *     
+ */
 package org.jenetics.examples;
 
 import java.io.File;
@@ -12,6 +34,10 @@ import org.jenetics.Genotype;
 import org.jenetics.util.Function;
 import org.jenetics.util.IO;
 
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version $Id: org.eclipse.jdt.ui.prefs 421 2010-03-18 22:41:17Z fwilhelm $
+ */
 public class Serial {
 
 	static class Id implements Function<Genotype<Float64Gene>, Float64> {
