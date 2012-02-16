3fe1a6ca7ff552f8a7304d7f444b9bc8e0645415
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-02-16 19:41:47 +0000

    Streamline OnesCounting example.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401294

--- src/example/java/org/jenetics/examples/OnesCounting.java
+++ src/example/java/org/jenetics/examples/OnesCounting.java
@@ -78,10 +78,10 @@ public class OnesCounting {
 		ga.setStatisticsCalculator(new NumberStatistics.Calculator<BitGene, Integer>());
 		ga.setPopulationSize(50);
 		ga.setSelectors(new RouletteWheelSelector<BitGene, Integer>());
-		ga.setAlterer(new CompositeAlterer<>(
+		ga.setAlterers(
 			new Mutator<BitGene>(0.55), 
 			new SinglePointCrossover<BitGene>(0.06)
-		));
+		);
 		
 		final int generations = 100;
 		
