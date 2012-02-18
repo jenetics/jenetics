3652710f78030097db82a0de8832f618930a688c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-02-18 14:52:49 +0000

    Remove trailing spaces.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401312

--- src/main/java/org/jenetics/AbstractAlterer.java
+++ src/main/java/org/jenetics/AbstractAlterer.java
@@ -1,17 +1,17 @@
 /*
  * Java Genetic Algorithm Library (@!identifier!@).
  * Copyright (c) @!year!@ Franz Wilhelmstötter
- *  
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -28,23 +28,23 @@ import static org.jenetics.util.object.hashCodeOf;
 
 /**
  * Abstract implementation of the alterer interface.
- * 
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @version $Id$
  */
-public abstract class AbstractAlterer<G extends Gene<?, G>> 
-	implements Alterer<G> 
+public abstract class AbstractAlterer<G extends Gene<?, G>>
+	implements Alterer<G>
 {
 	public static final double DEFAULT_ALTER_PROBABILITY = 0.2;
-	
+
 	/**
-	 * The altering probability. 
+	 * The altering probability.
 	 */
 	protected final double _probability;
-	
+
 	/**
 	 * Constructs an alterer with a given recombination probability.
-	 * 
+	 *
 	 * @param probability The recombination probability.
 	 * @throws IllegalArgumentException if the {@code probability} is not in the
 	 *         valid range of {@code [0, 1]}.
@@ -52,21 +52,21 @@ public abstract class AbstractAlterer<G extends Gene<?, G>>
 	protected AbstractAlterer(final double probability) {
 		_probability = checkProbability(probability);
 	}
-	
+
 	/**
 	 * Return the recombination/alter probability for this alterer.
-	 * 
+	 *
 	 * @return The recombination probability.
 	 */
 	public double getProbability() {
 		return _probability;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return hashCodeOf(getClass()).and(_probability).value();
 	}
-	
+
 	@Override
 	public boolean equals(final Object obj) {
 		if (obj == this) {
@@ -75,7 +75,7 @@ public abstract class AbstractAlterer<G extends Gene<?, G>>
 		if (!(obj instanceof AbstractAlterer<?>)) {
 			return false;
 		}
-		
+
 		final AbstractAlterer<?> alterer = (AbstractAlterer<?>)obj;
 		return eq(_probability, alterer._probability);
 	}
