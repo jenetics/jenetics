b032cdae4dd49ebfa3acc9f583cec8479084914f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-02-19 13:58:08 +0000

    Write manual - Examples.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401321

---
+++ doc/documentation/code/RealFunction.java
@@ -0,0 +1,55 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static org.jenetics.ExponentialScaler.SQR_SCALER;
+
+import org.jscience.mathematics.number.Float64;
+
+import org.jenetics.Float64Chromosome;
+import org.jenetics.Float64Gene;
+import org.jenetics.GeneticAlgorithm;
+import org.jenetics.Genotype;
+import org.jenetics.MeanAlterer;
+import org.jenetics.Mutator;
+import org.jenetics.NumberStatistics;
+import org.jenetics.Optimize;
+import org.jenetics.util.Factory;
+import org.jenetics.util.Function;
+
+final class Real 
+	implements Function<Genotype<Float64Gene>, Float64> 
+{
+	@Override
+	public Float64 apply(Genotype<Float64Gene> genotype) {
+		final double x = genotype.getGene().doubleValue();
+		return Float64.valueOf(cos(0.5 + sin(x)) * cos(x));
+	}
+}
+
+public class RealFunction {
+	public static void main(String[] args) {
+		Factory<Genotype<Float64Gene>> gtf = Genotype.valueOf(
+			new Float64Chromosome(0.0, 2.0 * PI)
+		);
+		Function<Genotype<Float64Gene>, Float64> ff = new Real();
+		GeneticAlgorithm<Float64Gene, Float64> ga = 
+		new GeneticAlgorithm<>(
+			gtf, ff, Optimize.MINIMUM
+		);
+
+		ga.setStatisticsCalculator(
+			new NumberStatistics.Calculator<Float64Gene, Float64>()
+		);
+		ga.setFitnessScaler(SQR_SCALER);
+		ga.setPopulationSize(20);
+		ga.setAlterers(
+			new Mutator<Float64Gene>(0.03),
+			new MeanAlterer<Float64Gene>(0.6)
+		);
+
+		ga.setup();
+		ga.evolve(100);
+		System.out.println(ga.getBestStatistics());
+	}
+}
+
