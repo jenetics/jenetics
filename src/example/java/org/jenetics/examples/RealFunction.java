7076f1e10d84fff0c15ac7bac71ee50103fae51f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-02-19 13:56:37 +0000

    Update examples.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401320

--- src/example/java/org/jenetics/examples/RealFunction.java
+++ src/example/java/org/jenetics/examples/RealFunction.java
@@ -18,18 +18,17 @@
  *
  * Author:
  * 	 Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
- * 	
+ *
  */
 package org.jenetics.examples;
 
+import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
-import static java.lang.Math.toRadians;
 import static org.jenetics.ExponentialScaler.SQR_SCALER;
 
-import java.io.Serializable;
+import org.jscience.mathematics.number.Float64;
 
-import org.jenetics.CompositeAlterer;
 import org.jenetics.Float64Chromosome;
 import org.jenetics.Float64Gene;
 import org.jenetics.GeneticAlgorithm;
@@ -37,76 +36,43 @@ import org.jenetics.Genotype;
 import org.jenetics.MeanAlterer;
 import org.jenetics.Mutator;
 import org.jenetics.NumberStatistics;
-import org.jenetics.RouletteWheelSelector;
-import org.jenetics.util.Function;
+import org.jenetics.Optimize;
 import org.jenetics.util.Factory;
-import org.jscience.mathematics.number.Float64;
+import org.jenetics.util.Function;
 
-/**
- * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version $Id$
- */
-public class RealFunction {
-	private static final class FF
-		implements Function<Genotype<Float64Gene>, Float64>,
-					Serializable
-	{
-		private static final long serialVersionUID = 1L;
-		
-		@Override
-		public Float64 apply(final Genotype<Float64Gene> genotype) {
-			final double radians = toRadians(converter.apply(genotype).doubleValue());
-			return Float64.valueOf(cos(sin(radians)*cos(radians)));
-		}
-
-		private Function<Genotype<Float64Gene>, Float64>
-		converter = new  Function<Genotype<Float64Gene>, Float64>() {
-			@Override
-			public Float64 apply(final Genotype<Float64Gene> value) {
-				return value.getGene().getAllele();
-			}
-		};
-		
-		@Override
-		public String toString() {
-			return "cos(sin(x)*cos(c))";
-		}
+final class Real
+	implements Function<Genotype<Float64Gene>, Float64>
+{
+	@Override
+	public Float64 apply(Genotype<Float64Gene> genotype) {
+		final double x = genotype.getGene().doubleValue();
+		return Float64.valueOf(cos(0.5 + sin(x)) * cos(x));
 	}
-	
-	
-	public static void main(String[] args) {
-		final Factory<Genotype<Float64Gene>> gtf = Genotype.valueOf(
-				new Float64Chromosome(0, 360)
-			);
-		final FF ff = new FF();
-		final GeneticAlgorithm<Float64Gene, Float64> ga = new GeneticAlgorithm<>(gtf, ff);
-		
-		ga.setStatisticsCalculator(new NumberStatistics.Calculator<Float64Gene, Float64>());
-		ga.setFitnessScaler(SQR_SCALER);
-		ga.setPopulationSize(10);
-		ga.setAlterer(new CompositeAlterer<>(
-			new Mutator<Float64Gene>(0.03),
-			new MeanAlterer<Float64Gene>(0.6)
-		));
-		ga.setSelectors(new RouletteWheelSelector<Float64Gene, Float64>());
-		
-		final int generations = 10;
-		
-		GAUtils.printConfig(
-				"Real function",
-				ga,
-				generations,
-				((CompositeAlterer<?>)ga.getAlterer()).getAlterers().toArray()
-			);
-		
-		GAUtils.execute(ga, generations, 1);
-	}
-	
 }
 
+public class RealFunction {
+	public static void main(String[] args) {
+		Factory<Genotype<Float64Gene>> gtf = Genotype.valueOf(
+			new Float64Chromosome(0.0, 2.0 * PI)
+		);
+		Function<Genotype<Float64Gene>, Float64> ff = new Real();
+		GeneticAlgorithm<Float64Gene, Float64> ga =
+		new GeneticAlgorithm<>(
+			gtf, ff, Optimize.MINIMUM
+		);
 
+		ga.setStatisticsCalculator(
+			new NumberStatistics.Calculator<Float64Gene, Float64>()
+		);
+		ga.setFitnessScaler(SQR_SCALER);
+		ga.setPopulationSize(20);
+		ga.setAlterers(
+			new Mutator<Float64Gene>(0.03),
+			new MeanAlterer<Float64Gene>(0.6)
+		);
 
-
-
-
-
+		ga.setup();
+		ga.evolve(100);
+		System.out.println(ga.getBestStatistics());
+	}
+}
