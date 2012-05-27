7d57270f997023980687d98fe2df55f9194d566d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-05-27 21:58:16 +0000

    Improve javadoc.

--- src/main/java/org/jenetics/util/MSeq.java
+++ src/main/java/org/jenetics/util/MSeq.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * Author:
- * 	 Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
  *
  */
 package org.jenetics.util;
