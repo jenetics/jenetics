141a9db95e4cb6a14a2218682742ef6e2d8ead15
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-05-31 19:58:10 +0000

    Update SVN properties.

--- src/test/java/org/jenetics/util/TestContext.java
+++ src/test/java/org/jenetics/util/TestContext.java
@@ -26,7 +26,7 @@ import javolution.context.ConcurrentContext;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version $Id: org.eclipse.jdt.ui.prefs 421 2010-03-18 22:41:17Z fwilhelm $
+ * @version $Id$
  */
 public final class TestContext extends ConcurrentContext {
 
