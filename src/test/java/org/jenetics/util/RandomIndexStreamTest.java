f19bcddaab188c02e39e1a377fa5cff1621b5aba
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-06-09 18:08:48 +0000

    Add some random IndexStream tests.

--- src/test/java/org/jenetics/util/RandomIndexStreamTest.java
+++ src/test/java/org/jenetics/util/RandomIndexStreamTest.java
@@ -44,82 +44,82 @@ public class RandomIndexStreamTest {
 	public void repeatable() {
 		final IndexStream stream1 = IndexStream.Random(1000, 0.5, new Random(1));
 		final IndexStream stream2 = IndexStream.Random(1000, 0.5, new Random(1));
-		
+
 		for (int i = stream1.next(); i != -1; i = stream1.next()) {
 			Assert.assertEquals(i, stream2.next());
 		}
 		Assert.assertEquals(stream2.next(), -1);
 	}
-	
+
 	@Test
 	public void iterateP0() {
 		final IndexStream it = IndexStream.Random(1000, 0, new Random());
-		
+
 		for (int i = it.next(); i != -1; i = it.next()) {
 			Assert.assertTrue(false);
 		}
-		
+
 		for (int i = 0; i < 100; ++i) {
 			Assert.assertEquals(it.next(), -1);
 		}
 	}
-	
+
 	@Test
 	public void iterateP1() {
 		final IndexStream it = IndexStream.Random(1000, 1, new Random());
-		
+
 		int count = 0;
 		for (int i = it.next(); i != -1; i = it.next()) {
 			Assert.assertEquals(i, count);
 			++count;
 		}
-		
+
 		Assert.assertEquals(count, 1000);
 	}
-	
+
 	@Test(dataProvider = "probabilities")
 	public void distribution(final Integer n, final Double p) {
 		final double mean = n*p;
 		final double var = n*p*(1 - p);
-		
+
 		final Random random = new Random();
 		final Range<Long> domain = new Range<>(0L, n.longValue());
-		
+
 		final Histogram<Long> histogram = Histogram.valueOf(
 					domain.getMin(), domain.getMax(), 10
-				);	
+				);
 		final Variance<Long> variance = new Variance<>();
-		for (int i = 0; i < 1000; ++i) {
+		for (int i = 0; i < 2500; ++i) {
 			final long k = k(n, p, random);
-			
+
 			histogram.accumulate(k);
 			variance.accumulate(k);
 		}
-				
+
 		// Normal distribution as approximation for binomial distribution.
 		assertDistribution(histogram, new NormalDistribution<>(domain, mean, var));
 	}
-	
+
 	double var(final double p, final long N) {
 		return N*p*(1.0 - p);
 	}
-	
+
 	double mean(final double p, final long N) {
 		return N*p;
 	}
-	
+
 	long k(final int n, final double p, final Random random) {
 		final IndexStream it = IndexStream.Random(n, p, random);
-		
+
 		int kt = 0;
 		for (int i = it.next(); i != -1; i = it.next()) {
 //			final ProbabilityIndexIterator itt =
 //				new ProbabilityIndexIterator(n, p, random);
-//			
+//
 //			for (int j = itt.next(); j != -1; j = itt.next()) {
 //				final ProbabilityIndexIterator ittt =
 //					new ProbabilityIndexIterator(n, p, random);
-//				
+//
 //				for (int k = ittt.next(); k != -1; k = ittt.next()) {
 //					++kt;
 //				}
@@ -128,11 +128,19 @@ public class RandomIndexStreamTest {
 		}
 		return kt;
 	}
-	
+
 	@DataProvider(name = "probabilities")
 	public Object[][] probabilities() {
 		return new Object[][] {
 				//    n,                p
+				{ new Integer(1115),  new Double(0.015) },
+				{ new Integer(1150),  new Double(0.015) },
+				{ new Integer(1160),  new Double(0.015) },
+				{ new Integer(1170),  new Double(0.015) },
+				{ new Integer(11100), new Double(0.015) },
+				{ new Integer(11200), new Double(0.015) },
+				{ new Integer(11500), new Double(0.015) },
+
 				{ new Integer(1115),  new Double(0.15) },
 				{ new Integer(1150),  new Double(0.15) },
 				{ new Integer(1160),  new Double(0.15) },
@@ -140,7 +148,7 @@ public class RandomIndexStreamTest {
 				{ new Integer(11100), new Double(0.15) },
 				{ new Integer(11200), new Double(0.15) },
 				{ new Integer(11500), new Double(0.15) },
-				
+
 				{ new Integer(515),   new Double(0.5) },
 				{ new Integer(1115),  new Double(0.5) },
 				{ new Integer(1150),  new Double(0.5) },
@@ -148,8 +156,8 @@ public class RandomIndexStreamTest {
 				{ new Integer(1170),  new Double(0.5) },
 				{ new Integer(11100), new Double(0.5) },
 				{ new Integer(11200), new Double(0.5) },
-				{ new Integer(11500), new Double(0.5) },	
-				
+				{ new Integer(11500), new Double(0.5) },
+
 				{ new Integer(515),   new Double(0.85) },
 				{ new Integer(1115),  new Double(0.85) },
 				{ new Integer(1150),  new Double(0.85) },
@@ -157,7 +165,16 @@ public class RandomIndexStreamTest {
 				{ new Integer(1170),  new Double(0.85) },
 				{ new Integer(11100), new Double(0.85) },
 				{ new Integer(11200), new Double(0.85) },
-				{ new Integer(11500), new Double(0.85) }
+				{ new Integer(11500), new Double(0.85) },
+
+				{ new Integer(515),   new Double(0.99) },
+				{ new Integer(1115),  new Double(0.99) },
+				{ new Integer(1150),  new Double(0.99) },
+				{ new Integer(1160),  new Double(0.99) },
+				{ new Integer(1170),  new Double(0.99) },
+				{ new Integer(11100), new Double(0.99) },
+				{ new Integer(11200), new Double(0.99) },
+				{ new Integer(11500), new Double(0.99) }
 		};
 	}
 }
