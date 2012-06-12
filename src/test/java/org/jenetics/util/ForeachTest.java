3a344ec0a28d1a70398f226c69ae63e3c80f79d9
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-06-12 17:32:45 +0000

    Streamline method names.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401568

--- src/test/java/org/jenetics/util/ForeachTest.java
+++ src/test/java/org/jenetics/util/ForeachTest.java
@@ -5,16 +5,15 @@ public class ForeachTest {
 	public interface P1<T> {
 		public boolean apply(final T value);
 	}
-	
+
 	public interface P2<T> {
 		public Boolean apply(final T value);
 	}
-	
+
 	public static void main(final String[] args) throws Exception {
 		Thread.sleep(100);
-		
-		final int iterations = 2000000000;
-		
+		final int iterations = 2_000_000_000;
+
 		long begin = System.currentTimeMillis();
 		iterate1(iterations, new P1<Long>() {
 			@Override public boolean apply(Long value) {
@@ -23,8 +22,8 @@ public class ForeachTest {
 		});
 		long end = System.currentTimeMillis();
 		System.out.println("P1: " + (end - begin));
-		
-		
+
+
 		begin = System.currentTimeMillis();
 		iterate2(iterations, new P2<Long>() {
 			@Override public Boolean apply(Long value) {
@@ -33,27 +32,27 @@ public class ForeachTest {
 		});
 		end = System.currentTimeMillis();
 		System.out.println("P2: " + (end - begin));
-		
+
 	}
-	
-	
+
+
 	static long iterate1(final long iterations, final P1<Long> predicate) {
 		long i = 0;
 		while (i < iterations && predicate.apply(i)) {
 			i += 1;
-			
+
 		}
-		
+
 		return i;
 	}
-	
+
 	static long iterate2(final long iterations, final P2<Long> predicate) {
 		long i = 0;
 		while (i < iterations && predicate.apply(i)) {
 			i += 1;
 		}
-		
+
 		return i;
 	}
-	
+
 }
