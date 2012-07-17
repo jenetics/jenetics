72cbbbb1dc67e827a8ab85e82e9e07a774e86b54
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-07-17 18:49:29 +0000

    (Re)organize scripts.
    --HG--
    extra : convert_revision : svn%3Aa2645ae0-aa55-4c4e-bf6c-d367ac68132e/trunk%401611

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-VERSION=`cat VERSION`
-CLS_PATH="./build/main/jenetics-all-${VERSION}.jar:./build/main/jenetics-examples-${VERSION}.jar:."
+VERSION=`cat ../VERSION`
+
+CLS_PATH="../build/main/jenetics-all-${VERSION}.jar:../build/main/jenetics-examples-${VERSION}.jar:."
 
 java -cp $CLS_PATH org.jenetics.examples.Knapsack
 java -cp $CLS_PATH org.jenetics.examples.OnesCounting
