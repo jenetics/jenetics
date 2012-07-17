7fee7a5dc1d71e62a2a6d0c9cf4f3f94ce53bb96
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-07-17 20:07:04 +0000

    Update profiling scripts.

--- script/profile-examples.sh
+++ script/profile-examples.sh
@@ -8,23 +8,32 @@
 #    http://kcachegrind.sourceforge.net/html/Home.html
 ##
 
+SCRIPT_DIR=`readlink -f $0`
+SCRIPT_DIR=`dirname ${SCRIPT_DIR}`
+
+VERSION=`cat ${SCRIPT_DIR}/../VERSION`
+CLS_PATH=`readlink -f ${SCRIPT_DIR}/../build/main/jenetics-all-${VERSION}.jar`
+CLS_PATH=${CLS_PATH}:`readlink -f ${SCRIPT_DIR}/../build/main/jenetics-examples-${VERSION}.jar`:.
+
 agent_param() {
 	output_file=$1
-	agent=`pwd`/../lib/build/libjgrind-x86_64.so
-	agent=`readlink -f ${agent}`
-	agent="${agent}=output=${output_file}:include=org.jenetics"
+	agent=`readlink -f ${SCRIPT_DIR}/../lib/build/libjgrind-x86_64.so`
+	agent="${agent}=output=${SCRIPT_DIR}/${output_file}:include=org.jenetics"
 	
 	echo ${agent}
 }
 
-VERSION=`cat ../VERSION`
-CLS_PATH="../build/main/jenetics-all-${VERSION}.jar:../build/main/jenetics-examples-${VERSION}.jar:."
+main() {
+	java -agentpath:`agent_param Knapsack.jgrind` -cp $CLS_PATH org.jenetics.examples.Knapsack
+	java -agentpath:`agent_param OnesCounting.jgrind` -cp $CLS_PATH org.jenetics.examples.OnesCounting
+	java -agentpath:`agent_param RealFunction.jgrind` -cp $CLS_PATH org.jenetics.examples.RealFunction
+	java -agentpath:`agent_param StringGenerator.jgrind` -cp $CLS_PATH org.jenetics.examples.StringGenerator
+	java -agentpath:`agent_param Transformation.jgrind` -cp $CLS_PATH org.jenetics.examples.Transformation
+	java -agentpath:`agent_param TravelingSalesman.jgrind` -cp $CLS_PATH org.jenetics.examples.TravelingSalesman
+}
 
-java -agentpath:`agent_param Knapsack.jgrind` -cp $CLS_PATH org.jenetics.examples.Knapsack
-java -agentpath:`agent_param OnesCounting.jgrind` -cp $CLS_PATH org.jenetics.examples.OnesCounting
-java -agentpath:`agent_param RealFunction.jgrind` -cp $CLS_PATH org.jenetics.examples.RealFunction
-java -agentpath:`agent_param StringGenerator.jgrind` -cp $CLS_PATH org.jenetics.examples.StringGenerator
-java -agentpath:`agent_param Transformation.jgrind` -cp $CLS_PATH org.jenetics.examples.Transformation
-java -agentpath:`agent_param TravelingSalesman.jgrind` -cp $CLS_PATH org.jenetics.examples.TravelingSalesman
+main $*
+
+
 
 
