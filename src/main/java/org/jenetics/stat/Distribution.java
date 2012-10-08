5d6879e95f2cd3cd7395805ec986523428d151bd
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-10-08 23:19:40 +0200

    Remove $Revision$ from @version tag.

--- src/main/java/org/jenetics/stat/Distribution.java
+++ src/main/java/org/jenetics/stat/Distribution.java
@@ -33,7 +33,7 @@ import org.jenetics.util.Range;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &ndash; <em>$Revision$</em>
+ * @version 1.0
  */
 public interface Distribution<C extends Comparable<? super C>> {
 
--- src/main/java/org/jenetics/stat/LinearDistribution.java
+++ src/main/java/org/jenetics/stat/LinearDistribution.java
@@ -68,7 +68,7 @@ import org.jenetics.util.Range;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &ndash; <em>$Revision$</em>
+ * @version 1.0
  */
 public class LinearDistribution<
 	N extends Number & Comparable<? super N>
