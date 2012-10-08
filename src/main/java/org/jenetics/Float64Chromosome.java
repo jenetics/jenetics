ffd720f7d3e9ab59c537879266609b8ce3d84dc4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-10-08 23:12:18 +0200

    Remove $Revision$ from @version tag.

--- src/main/java/org/jenetics/Float64Chromosome.java
+++ src/main/java/org/jenetics/Float64Chromosome.java
@@ -44,7 +44,7 @@ import org.jenetics.util.ISeq;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &ndash; <em>$Revision$</em>
+ * @version 1.0
  */
 public class Float64Chromosome
 	extends NumberChromosome<Float64, Float64Gene>
