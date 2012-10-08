fc5675e34d21b2fa6465feaf941cf54b80d4ed2f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-10-08 23:19:40 +0200

    Remove $Revision$ from @version tag.

--- src/main/java/org/jenetics/stat/Quantile.java
+++ src/main/java/org/jenetics/stat/Quantile.java
@@ -46,7 +46,7 @@ import org.jenetics.util.MappedAccumulator;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &ndash; <em>$Revision$</em>
+ * @version 1.0
  */
 public class Quantile<N extends Number> extends MappedAccumulator<N> {
 
