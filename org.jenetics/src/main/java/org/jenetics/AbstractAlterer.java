3f490f45d9a6b7a7e47815fb06fd40ce83cccc66
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-11-06 21:30:11 +0100

    Keyword extension test.
    --HG--
    branch : r1.1.0

--- org.jenetics/src/main/java/org/jenetics/AbstractAlterer.java
+++ org.jenetics/src/main/java/org/jenetics/AbstractAlterer.java
@@ -31,7 +31,7 @@ import static org.jenetics.util.object.hashCodeOf;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &dash; $Date$
+ * @version 1.0 &mdash; $Date$
  */
 public abstract class AbstractAlterer<G extends Gene<?, G>>
 	implements Alterer<G>
