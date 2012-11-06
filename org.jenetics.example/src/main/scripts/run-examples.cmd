c8e90fa4493d6183f62ee1e283134fb8652a3802
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2012-11-06 18:43:05 +0100

    Add copyright notice.
    --HG--
    branch : r1.1.0

--- org.jenetics.example/src/main/scripts/run-examples.cmd
+++ org.jenetics.example/src/main/scripts/run-examples.cmd
@@ -1,3 +1,23 @@
+rem Java Genetic Algorithm Library (@__identifier__@).
+rem Copyright (c) @__year__@ Franz Wilhelmstötter
+rem
+rem This library is free software; you can redistribute it and/or
+rem modify it under the terms of the GNU Lesser General Public
+rem License as published by the Free Software Foundation; either
+rem version 2.1 of the License, or (at your option) any later version.
+rem
+rem This library is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+rem Lesser General Public License for more details.
+rem
+rem You should have received a copy of the GNU Lesser General Public
+rem License along with this library; if not, write to the Free Software
+rem Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+rem
+rem Author:
+rem   Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+
 set VERSION=1.0.1
 set CLS_PATH=./build/main/jenetics-all-%VERSION%.jar;./build/main/jenetics-examples-%VERSION%.jar;.
 
