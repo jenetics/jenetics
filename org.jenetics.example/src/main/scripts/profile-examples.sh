4a9d392bce38ff32e346919b59aa7c400ff88aeb
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-11-06 18:43:05 +0100

    Add copyright notice.

--- org.jenetics.example/src/main/scripts/profile-examples.sh
+++ org.jenetics.example/src/main/scripts/profile-examples.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+# Java Genetic Algorithm Library (@__identifier__@).
+# Copyright (c) @__year__@ Franz Wilhelmstötter
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Author:
+#   Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+
 ##
 # Run the examples for profiling, using the JGrind library:
 #     https://bitbucket.org/fwilhelm/jgrind
-# 
+#
 # The created reports can be viewed with the KCachgrind application:
 #    http://kcachegrind.sourceforge.net/html/Home.html
 ##
@@ -19,7 +39,7 @@ agent_param() {
 	output_file=$1
 	agent=`readlink -f ${SCRIPT_DIR}/../lib/build/libjgrind-x86_64.so`
 	agent="${agent}=output=${SCRIPT_DIR}/${output_file}:include=org.jenetics"
-	
+
 	echo ${agent}
 }
 
