eb020359f66c37cc40e19f84bf05da6cb44307dd
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-11-06 21:32:54 +0100

    Keyword extension test.

--- org.jenetics.example/src/main/java/org/jenetics/example/gui/SelectorPanel.java
+++ org.jenetics.example/src/main/java/org/jenetics/example/gui/SelectorPanel.java
@@ -1,12 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ * 	 Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
  */
-package org.jenetics.examples.gui;
+package org.jenetics.example.gui;
 
 /**
- *
- * @author fwilhelm
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @since 1.0
+ * @version 1.0 &mdash; $Date: 2012-11-06 $
  */
 public class SelectorPanel extends javax.swing.JPanel {
 	private static final long serialVersionUID = 1L;
