d83c8bac8705e72ea04148201570e61e80f088e8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-11-23 11:41:26 +0100

    Code reformatting.

--- org.jenetics/src/main/java/org/jenetics/util/HQ64Random.java
+++ org.jenetics/src/main/java/org/jenetics/util/HQ64Random.java
@@ -48,9 +48,9 @@ package org.jenetics.util;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.1
- * @version 1.1 &mdash; <em>$Date: 2012-11-22 $</em>
+ * @version 1.1 &mdash; <em>$Date: 2012-11-23 $</em>
  */
-public class HQ64Random extends Random64{
+public class HQ64Random extends Random64 {
 
 	private static final long serialVersionUID = 1L;
 
