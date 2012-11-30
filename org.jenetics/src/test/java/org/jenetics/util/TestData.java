dcf07b25abb0f2897064db413a8930f825bea3cd
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-11-30 23:50:26 +0100

    Simplify test code.

--- org.jenetics/src/test/java/org/jenetics/util/TestData.java
+++ org.jenetics/src/test/java/org/jenetics/util/TestData.java
@@ -1,3 +1,25 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
 package org.jenetics.util;
 
 import java.io.BufferedReader;
@@ -6,6 +28,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version <em>$Date: 2012-11-30 $</em>
+ */
 public class TestData implements Iterable<String[]> {
 
 	private final String _resource;
