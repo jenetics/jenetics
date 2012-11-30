194d6a3d5116e99ac63ff74e2ad08e575a7af26f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-11-30 22:46:14 +0100

    Add @version tag.

--- org.jenetics/src/test/java/org/jenetics/BitChromosomeTest.java
+++ org.jenetics/src/test/java/org/jenetics/BitChromosomeTest.java
@@ -38,6 +38,7 @@ import org.jenetics.util.Factory;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version <em>$Date: 2012-11-30 $</em>
  */
 public class BitChromosomeTest extends ChromosomeTester<BitGene> {
 
