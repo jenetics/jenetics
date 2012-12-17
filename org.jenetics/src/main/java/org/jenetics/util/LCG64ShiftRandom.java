8dacf8d0d8c5924d2c51f97b722de9d3e0027737
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2012-12-17 20:40:54 +0100

    Fix javadoc.

--- org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
+++ org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
@@ -71,7 +71,7 @@ import java.io.Serializable;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.1
- * @version 1.1 &mdash; <em>$Date: 2012-12-12 $</em>
+ * @version 1.1 &mdash; <em>$Date: 2012-12-17 $</em>
  */
 public class LCG64ShiftRandom extends Random64 {
 
@@ -84,7 +84,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 1.1 &mdash; <em>$Date: 2012-12-12 $</em>
+	 * @version 1.1 &mdash; <em>$Date: 2012-12-17 $</em>
 	 */
 	public static final class Param implements Serializable {
 
@@ -184,7 +184,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 1.1 &mdash; <em>$Date: 2012-12-12 $</em>
+	 * @version 1.1 &mdash; <em>$Date: 2012-12-17 $</em>
 	 */
 	public static class ThreadLocal extends java.lang.ThreadLocal<LCG64ShiftRandom> {
 		private static final long STEP_BASE = 1L << 56;
@@ -271,7 +271,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 1.1 &mdash; <em>$Date: 2012-12-12 $</em>
+	 * @version 1.1 &mdash; <em>$Date: 2012-12-17 $</em>
 	 */
 	public static class ThreadSafe extends LCG64ShiftRandom {
 		private static final long serialVersionUID = 1L;
@@ -412,7 +412,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 * Changes the internal state of the PRNG in a way that future calls to
 	 * {@link #nextLong()} will generated the s<sup>th</sup> sub-stream of
 	 * p<sup>th</sup> sub-streams. <i>s</i> must be within the range of
-	 * {@code [0, n)}. This method is mainly used for <i>parallelization</i>
+	 * {@code [0, p-1)}. This method is mainly used for <i>parallelization</i>
 	 * via <i>leapfrogging</i>.
 	 *
 	 * @param p the overall number of sub-streams
