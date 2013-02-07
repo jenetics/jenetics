df71886f0a14e5329ccc973335cc48897d800aa6
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-02-07 20:24:22 +0100

    Some code re-formatting.

--- org.jenetics/src/main/java/org/jenetics/ProbabilitySelector.java
+++ org.jenetics/src/main/java/org/jenetics/ProbabilitySelector.java
@@ -49,7 +49,7 @@ import org.jenetics.util.RandomRegistry;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2013-01-24 $</em>
+ * @version 1.0 &mdash; <em>$Date: 2013-02-07 $</em>
  */
 public abstract class ProbabilitySelector<
 	G extends Gene<?, G>,
@@ -195,7 +195,7 @@ public abstract class ProbabilitySelector<
 	/**
 	 * Perform a binary-search on the summed probability array.
 	 */
-	final static int indexOf(final double[] incremental, final double value) {
+	final static int indexOf(final double[] incremental, final double v) {
 		int imin = 0;
 		int imax = incremental.length;
 
@@ -204,11 +204,11 @@ public abstract class ProbabilitySelector<
 
 			if (imid == 0) {
 				return imid;
-			} else if (incremental[imid] >= value && incremental[imid - 1] < value) {
+			} else if (incremental[imid] >= v && incremental[imid - 1] < v) {
 				return imid;
-			} else if (incremental[imid] <= value) {
+			} else if (incremental[imid] <= v) {
 				imin = imid + 1;
-			} else if (incremental[imid] > value) {
+			} else if (incremental[imid] > v) {
 				imax = imid;
 			}
 		}
