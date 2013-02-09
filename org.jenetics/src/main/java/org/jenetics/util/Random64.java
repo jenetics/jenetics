e06e41bf03699eb8ba1d213234690e486fe3144e
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-02-09 19:26:34 +0100

    Improvements in Random64 class.

--- org.jenetics/src/main/java/org/jenetics/util/Random64.java
+++ org.jenetics/src/main/java/org/jenetics/util/Random64.java
@@ -27,17 +27,17 @@ import java.util.Random;
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.1
- * @version 1.1 &mdash; <em>$Date: 2012-12-05 $</em>
+ * @version 1.1 &mdash; <em>$Date: 2013-02-09 $</em>
  */
 abstract class Random64 extends Random {
 
 	private static final long serialVersionUID = 1L;
 
-	protected Random64() {
-	}
-
 	protected Random64(long seed) {
 		super(seed);
+	}
+
+	protected Random64() {
 	}
 
 	// Force to explicitly override the Random.nextLong() method.
@@ -45,13 +45,18 @@ abstract class Random64 extends Random {
 	public abstract long nextLong();
 
 	@Override
-	protected int next(final int bits) {
-		return (int)(nextLong() >>> (64 - bits));
+	public boolean nextBoolean() {
+		return (nextLong() & 0x8000000000000000L) != 0L;
 	}
 
 	@Override
 	public int nextInt() {
 		return (int)(nextLong() >>> 32);
+	}
+
+	@Override
+	protected int next(final int bits) {
+		return (int)(nextLong() >>> (64 - bits));
 	}
 
 	/**
@@ -78,10 +83,37 @@ abstract class Random64 extends Random {
 		return toDouble(nextLong());
 	}
 
+	/*
+	 * Conversion methods used by the 'Random' engine from the JDK.
+	 */
+
+	static float toFloat(final int x) {
+		return (x >>> 8)/((float)(1 << 24));
+	}
+
+	static float toFloat(final long x) {
+		return (int)(x >>> 40)/((float)(1 << 24));
+	}
+
 	static double toDouble(final long x) {
 		return (((x >>> 38) << 27) + (((int)x) >>> 5))/(double)(1L << 53);
 	}
 
+	/*
+	 * This method is used by the Apache Commons BitStreamGenerator.
+	 */
+
+	static float toFloat2(final int x) {
+		return (x >>> 9)*0x1.0p-23f;
+	}
+
+	static float toFloat2(final long x) {
+		return (int)(x >>> 41)*0x1.0p-23f;
+	}
+
+	static double toDouble2(final long x) {
+		return (x & 0xFFFFFFFFFFFFFL)*0x1.0p-52d;
+	}
 
 
 }
