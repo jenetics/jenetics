7b2d70052878f650624b2a2d2985a0a9427db940
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-03-14 21:56:03 +0100

    Add 'LCG32ShiftRandom' class.

---
+++ org.jenetix/src/main/java/org/jenetix/util/LCG32ShiftRandom.java
@@ -0,0 +1,32 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
+package org.jenetix.util;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @since 1.0
+ * @version 1.0 &mdash; <em>$Date: 2013-03-14 $</em>
+ */
+public class LCG32ShiftRandom {
+
+}
