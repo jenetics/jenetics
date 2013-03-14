ea114896da27a637927002e7f96ce863cac67d49
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-03-14 23:30:26 +0100

    Implement RealGene.

--- org.jenetix/src/main/java/org/jenetix/RealGene.java
+++ org.jenetix/src/main/java/org/jenetix/RealGene.java
@@ -22,11 +22,96 @@
  */
 package org.jenetix;
 
+import javolution.context.ObjectFactory;
+
+import org.jscience.mathematics.number.Real;
+import org.jscience.mathematics.structure.GroupMultiplicative;
+
+import org.jenetics.NumberGene;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
  * @version 1.0 &mdash; <em>$Date: 2013-03-14 $</em>
  */
-public class RealGene {
+public class RealGene
+	extends NumberGene<Real, RealGene>
+	implements GroupMultiplicative<RealGene>
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Real TWO = Real.ONE.plus(Real.ONE);
+
+	RealGene() {
+	}
+
+	@Override
+	public RealGene newInstance() {
+		int digits = Real.getExactPrecision();
+		return null;
+	}
+
+	@Override
+	public RealGene mean(final RealGene that) {
+		final Real mean = _value.plus(that._value).divide(TWO);
+		return newInstance(mean);
+	}
+
+	@Override
+	public RealGene inverse() {
+		return valueOf(_value.inverse(), _min, _max);
+	}
+
+	@Override
+	public RealGene newInstance(final Real value) {
+		return valueOf(value, _min, _max);
+	}
+
+	@Override
+	public RealGene newInstance(final Number value) {
+		return valueOf(Real.valueOf(value.doubleValue()), _min, _max);
+	}
+
+	/* *************************************************************************
+	 *  Static object creation methods
+	 * ************************************************************************/
+
+	private static final ObjectFactory<RealGene> FACTORY =
+		new ObjectFactory<RealGene>() {
+			@Override protected RealGene create() {
+				return new RealGene();
+			}
+		};
+
+	/**
+	 * Create a new random {@code RealGene} with the given value and the given
+	 * range. If the {@code value} isn't within the closed interval [min, max],
+	 * no exception is thrown. In this case the method {@link RealGene#isValid()}
+	 * returns {@code false}.
+	 *
+	 * @param value the value of the gene.
+	 * @param min the minimal valid value of this gene.
+	 * @param max the maximal valid value of this gene.
+	 * @return the new created gene with the given {@code value}.
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 */
+	public static RealGene valueOf(
+		final Real value,
+		final Real min,
+		final Real max
+	) {
+		final RealGene gene = FACTORY.object();
+		gene.set(value, min, max);
+		return gene;
+	}
 
 }
+
+
+
+
+
+
+
+
