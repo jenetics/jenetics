14abbd89448e58f8ebec3af124caa7afb90993e3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-03-28 21:47:33 +0100

    Add abstract Random32 class.

--- org.jenetics/src/main/java/org/jenetics/util/Random32.java
+++ org.jenetics/src/main/java/org/jenetics/util/Random32.java
@@ -9,7 +9,7 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
@@ -20,22 +20,61 @@
  *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
  *
  */
-package org.jenetix.util;
+package org.jenetics.util;
 
-import org.jenetics.util.Random32;
+import java.util.Random;
 
 /**
+ * Base class for random generators which create 32 bit random values natively.
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2013-03-28 $</em>
+ * @since 1.1 xxx
+ * @version 1.2 xxx &mdash; <em>$Date: 2013-03-28 $</em>
  */
-public class LCG32ShiftRandom extends Random32 {
+public abstract class Random32 extends PRNG {
 
 	private static final long serialVersionUID = 1L;
 
+	protected Random32(long seed) {
+		super(seed);
+	}
+
+	protected Random32() {
+	}
+
+	// Force to explicitly override the Random.nextInt() method.
 	@Override
-	public int nextInt() {
-		return 0;
+	public abstract int nextInt();
+
+
+	@Override
+	public long nextLong() {
+		return ((long)(nextInt()) << 32) + nextInt();
+	}
+
+	@Override
+	protected int next(final int bits) {
+		return nextInt() >>> (32 - bits);
+	}
+
+	@Override
+	public float nextFloat() {
+		return math.random.toFloat2(nextInt());
+	}
+
+	/**
+	 * Optimized version of the {@link Random#nextDouble()} method for 32-bit
+	 * random engines.
+	 */
+	@Override
+	public double nextDouble() {
+		return math.random.toDouble2(nextInt(), nextInt());
 	}
 
 }
+
+
+
+
+
+
