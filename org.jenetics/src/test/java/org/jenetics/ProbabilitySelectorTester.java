7d081d4a787c0e900b7de65aac1df6386781d4af
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-06-02 11:59:00 +0200

    Mark non-array methods in 'arrays' class as deprecated.
    --HG--
    branch : r1.3.0

--- org.jenetics/src/test/java/org/jenetics/ProbabilitySelectorTester.java
+++ org.jenetics/src/test/java/org/jenetics/ProbabilitySelectorTester.java
@@ -28,15 +28,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import org.jscience.mathematics.number.Float64;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import org.jscience.mathematics.number.Float64;
-
 import org.jenetics.util.RandomRegistry;
-import org.jenetics.util.arrays;
+import org.jenetics.util.lists;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
@@ -126,7 +125,7 @@ public abstract class ProbabilitySelectorTester<
 	@Test
 	public void probabilities() {
 		final Population<Float64Gene, Float64> population = TestUtils.newFloat64Population(100);
-		arrays.shuffle(population, new Random(System.currentTimeMillis()));
+		lists.shuffle(population, new Random(System.currentTimeMillis()));
 
 		final S selector = getFactory().newInstance();
 		final double[] props = selector.probabilities(population, 23);
