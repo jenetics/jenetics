181054e3c7219af158a6a17d707422d5446f6093
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-06-07 15:48:47 +0200

    Update version string to 1.3.

--- org.jenetics/src/main/java/org/jenetics/util/bit.java
+++ org.jenetics/src/main/java/org/jenetics/util/bit.java
@@ -41,7 +41,7 @@ import org.jscience.mathematics.number.LargeInteger;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2013-06-03 $</em>
+ * @version 1.3 &mdash; <em>$Date: 2013-06-07 $</em>
  */
 public final class bit extends StaticObject {
 	private bit() {}
