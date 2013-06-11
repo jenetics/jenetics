18338a4777f6918400bbe84d19c9b0fb28e69541
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-06-11 18:01:09 +0200

    Improve Javadoc.

--- org.jenetics/src/main/java/org/jenetics/GeneticAlgorithm.java
+++ org.jenetics/src/main/java/org/jenetics/GeneticAlgorithm.java
@@ -53,11 +53,11 @@ import org.jenetics.util.functions;
  *
  * [code]
  * public static void main(final String[] args) {
- *     Factory<Genotype<BitGene>> gtf = Genotype.valueOf(
+ *     final Factory<Genotype<BitGene>> gtf = Genotype.valueOf(
  *         BitChromosome.valueOf(10, 0.5)
  *     );
- *     Function<Genotype<BitGene> Float64> ff = ...
- *     GeneticAlgorithm<BitGene, Float64>
+ *     final Function<Genotype<BitGene> Float64> ff = ...
+ *     final GeneticAlgorithm<BitGene, Float64>
  *     ga = new GeneticAlgorithm<>(gtf, ff, Optimize.MAXIMUM)
  *
  *     ga.setup();
@@ -137,7 +137,7 @@ import org.jenetics.util.functions;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2013-06-03 $</em>
+ * @version 1.0 &mdash; <em>$Date: 2013-06-11 $</em>
  */
 public class GeneticAlgorithm<
 	G extends Gene<?, G>,
