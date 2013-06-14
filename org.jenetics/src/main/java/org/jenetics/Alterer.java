adf1c6b4c75528616c5aa18d98cae7b4a3a55c9f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-06-14 18:28:16 +0200

    Prepare for version v1.2.0.

--- org.jenetics/src/main/java/org/jenetics/Alterer.java
+++ org.jenetics/src/main/java/org/jenetics/Alterer.java
@@ -29,7 +29,7 @@ package org.jenetics;
  * {@link GeneticAlgorithm#setAlterers(Alterer...)} method.
  *
  * [code]
- * GeneticAlgorithm<Float64Gene, Float64> ga = ...
+ * final GeneticAlgorithm<Float64Gene, Float64> ga = ...
  * ga.setAlterers(
  *     new Crossover<Float64Gene>(0.1),
  *     new Mutator<Float64Gene>(0.05),
@@ -43,7 +43,7 @@ package org.jenetics;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2012-11-06 $</em>
+ * @version 1.0 &mdash; <em>$Date: 2013-06-14 $</em>
  */
 public interface Alterer<G extends Gene<?, G>> {
 
