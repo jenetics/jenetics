23ca9d7f0ac2f3a1e7825e4afe330a8503120390
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-06-21 22:48:51 +0200

    Add test base class for 'Seq' interface.
    --HG--
    branch : org.jenetics-bit_gene_seq

---
+++ org.jenetics/src/test/java/org/jenetics/util/SeqTestBase.java
@@ -0,0 +1,53 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
+package org.jenetics.util;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version <em>$Date$</em>
+ */
+public abstract class SeqTestBase<T> {
+
+	protected abstract Seq<T> newSeq(final int length);
+
+	@Test
+	public void forEach() {
+		final Seq<T> seq = newSeq(1000);
+
+		final AtomicInteger counter = new AtomicInteger(0);
+		seq.forEach(new Function<T, Void>() {
+			@Override public Void apply(final T value) {
+				counter.incrementAndGet();
+				return null;
+			}
+		});
+
+		Assert.assertEquals(counter.get(), seq.length());
+	}
+
+}
