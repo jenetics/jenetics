a4d1c73f27c62772b7fd579ec511b9b6f850f209
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-07-01 19:36:02 +0200

    Add 'ISeq' test cases.

---
+++ org.jenetics/src/test/java/org/jenetics/util/ISeqTestBase.java
@@ -0,0 +1,42 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
+package org.jenetics.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version <em>$Date: 2013-07-01 $</em>
+ */
+public abstract class ISeqTestBase extends SeqTestBase {
+
+	@Override
+	protected abstract ISeq<Integer> newSeq(final int length);
+
+	@Test(dataProvider = "sequences")
+	public void copy(final ISeq<Integer> seq) {
+		final MSeq<Integer> copy = seq.copy();
+		Assert.assertEquals(copy, seq);
+	}
+}
