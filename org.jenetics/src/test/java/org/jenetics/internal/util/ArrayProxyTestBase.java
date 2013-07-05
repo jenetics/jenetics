d67343f036dd84936fd0a4cb9ad3fad7d20b0231
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-07-05 17:38:04 +0200

    Implement tests for 'ArrayProxyImpl' class.

--- org.jenetics/src/test/java/org/jenetics/internal/util/ArrayProxyTestBase.java
+++ org.jenetics/src/test/java/org/jenetics/internal/util/ArrayProxyTestBase.java
@@ -24,6 +24,10 @@ package org.jenetics.internal.util;
 
 import java.util.Random;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import org.jenetics.util.math;
 
 /**
@@ -40,4 +44,144 @@ public abstract class ArrayProxyTestBase<T> {
 		return newArrayProxyElement(new Random(math.random.seed()));
 	}
 
+	@Test
+	public void uncheckedOffsetGetSet() {
+		final long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<T> proxy = newArrayProxy(10_000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.uncheckedOffsetSet(i, newArrayProxyElement(random));
+		}
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			final T actual = proxy.uncheckedOffsetGet(i);
+			final T expected = newArrayProxyElement(random);
+
+			Assert.assertEquals(actual, expected);
+		}
+	}
+
+	@Test
+	public void uncheckedGetSet() {
+		final long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<T> proxy = newArrayProxy(10_000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.uncheckedSet(i, newArrayProxyElement(random));
+		}
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			final T actual = proxy.uncheckedGet(i);
+			final T expected = newArrayProxyElement(random);
+
+			Assert.assertEquals(actual, expected);
+		}
+	}
+
+	@Test
+	public void getSet() {
+		final long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<T> proxy = newArrayProxy(10_000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, newArrayProxyElement(random));
+		}
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			final T actual = proxy.get(i);
+			final T expected = newArrayProxyElement(random);
+
+			Assert.assertEquals(actual, expected);
+		}
+	}
+
+	@Test(
+		dataProvider = "outofboundsdata",
+		expectedExceptions = IndexOutOfBoundsException.class
+	)
+	public void outOfBoundsGet(final Integer length, final Integer index) {
+		final ArrayProxy<T> proxy = newArrayProxy(length);
+		proxy.get(index);
+	}
+
+	@Test(
+		dataProvider = "outofboundsdata",
+		expectedExceptions = IndexOutOfBoundsException.class
+	)
+	public void outOfBoundsSet(final Integer length, final Integer index) {
+		final ArrayProxy<T> proxy = newArrayProxy(length);
+		proxy.get(index);
+	}
+
+	@DataProvider(name = "outofboundsdata")
+	public Object[][] getOutOfBoundsData() {
+		return new Object[][] {
+			{3, -1},
+			{3, 3},
+			{10, -234},
+			{10, 233},
+			{100, -292929},
+			{100, 200}
+		};
+	}
+
+	@Test(dataProvider = "subintintdata")
+	public void subIntInt(
+		final Integer length,
+		final Integer start,
+		final Integer end
+	) {
+		final long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<T> proxy = newArrayProxy(length);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, newArrayProxyElement(random));
+		}
+
+		random.setSeed(seed);
+		for (int i = 0; i < start; ++i) {
+			final T actual = proxy.get(i);
+			final T expected = newArrayProxyElement(random);
+
+			Assert.assertEquals(actual, expected);
+		}
+
+		final ArrayProxy<T> sub = proxy.sub(start, end);
+		for (int i = 0; i < (end - start); ++i) {
+			final T expected = newArrayProxyElement(random);
+
+			Assert.assertEquals(sub.get(i), expected);
+			Assert.assertEquals(proxy.get(i + start), expected);
+
+			Assert.assertEquals(sub.uncheckedGet(i), expected);
+			Assert.assertEquals(proxy.uncheckedGet(i + start), expected);
+
+			Assert.assertEquals(sub.uncheckedOffsetGet(i + start), expected);
+			Assert.assertEquals(proxy.uncheckedOffsetGet(i + start), expected);
+		}
+	}
+
+	@DataProvider(name = "subintintdata")
+	public Object[][] getSubIntIntData() {
+		return new Object[][] {
+			{1000, 0, 1000},
+			{1000, 2, 1000},
+			{1000, 100, 900},
+			{1000, 5, 600},
+			{10000, 8800, 9400},
+			{10000, 4, 9800}
+		};
+	}
+
 }
+
+
+
+
