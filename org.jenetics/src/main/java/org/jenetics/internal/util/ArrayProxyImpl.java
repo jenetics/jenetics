474ec87ab1052b96c8ffe8892762b6e7212cd036
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-07-08 21:51:11 +0200

    Remove uncommented method.
    --HG--
    branch : org.jenetics-bit_gene_seq

--- org.jenetics/src/main/java/org/jenetics/internal/util/ArrayProxyImpl.java
+++ org.jenetics/src/main/java/org/jenetics/internal/util/ArrayProxyImpl.java
@@ -59,25 +59,6 @@ public class ArrayProxyImpl<T> extends ArrayProxy<T> {
 		return new ArrayProxyImpl<>(_array, start + _start, end + _start);
 	}
 
-	/*
-	@Override
-	public void swap(
-		final int start,
-		final int end,
-		final ArrayProxy<T> other,
-		final int otherStart
-	) {
-		cloneIfSealed();
-		other.cloneIfSealed();
-
-		for (int i = (end - start); --i >= 0;) {
-			final T temp = uncheckedGet(i + start);
-			uncheckedSet(i + start, other.uncheckedGet(otherStart + i));
-			other.uncheckedSet(otherStart + i, temp);
-		}
-	}
-	*/
-
 	@Override
 	public void cloneIfSealed() {
 		if (_sealed) {
