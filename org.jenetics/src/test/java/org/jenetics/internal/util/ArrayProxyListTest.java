80ff93ac6c2c1e28072cb1b0945b28703135a028
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-07-10 20:26:29 +0200

    Add 'ArrayProxyListTest' class.
    --HG--
    branch : r1.4.0

---
+++ org.jenetics/src/test/java/org/jenetics/internal/util/ArrayProxyListTest.java
@@ -0,0 +1,188 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
+package org.jenetics.internal.util;
+
+import java.util.List;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import org.jenetics.util.math;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version <em>$Date$</em>
+ */
+public class ArrayProxyListTest {
+
+	@Test
+	public void size() {
+		final ArrayProxy<Integer> proxy = new ArrayProxyImpl<>(1000);
+		final List<Integer> list = new ArrayProxyList<>(proxy);
+
+		Assert.assertEquals(list.size(), proxy._length);
+	}
+
+	@Test
+	public void get() {
+		final long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Integer> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextInt());
+		}
+
+		final List<Integer> list = new ArrayProxyList<>(proxy);
+
+		for (int i = 0; i < proxy._length; ++i) {
+			final Integer actual = list.get(i);
+			final Integer expected = proxy.get(i);
+			Assert.assertEquals(actual, expected);
+		}
+	}
+
+	@Test
+	public void set() {
+		long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Integer> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextInt());
+		}
+
+		final List<Integer> list = new ArrayProxyList<>(proxy);
+
+		seed = math.random.seed();
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			list.set(i, random.nextInt());
+		}
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			Assert.assertEquals(proxy.get(i), list.get(i));
+			Assert.assertEquals(list.get(i).intValue(), random.nextInt());
+		}
+	}
+
+	@Test
+	public void indexOf() {
+		long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Long> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextLong());
+		}
+
+		final List<Long> list = new ArrayProxyList<>(proxy);
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			final int index = list.indexOf(random.nextLong());
+			Assert.assertEquals(index, i);
+		}
+	}
+
+	@Test
+	public void contains() {
+		long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Long> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextLong());
+		}
+
+		final List<Long> list = new ArrayProxyList<>(proxy);
+
+		random.setSeed(seed);
+		for (int i = 0; i < proxy._length; ++i) {
+			Assert.assertTrue(
+				list.contains(random.nextLong()),
+				"Must contain the given value."
+			);
+		}
+	}
+
+	@Test
+	public void toArray() {
+		long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Long> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextLong());
+		}
+
+		final List<Long> list = new ArrayProxyList<>(proxy);
+		final Object[] array = list.toArray();
+
+		for (int i = 0; i < array.length; ++i) {
+			Assert.assertEquals(array[i], proxy.get(i));
+		}
+	}
+
+	@Test
+	public void toArrayLong() {
+		long seed = math.random.seed();
+		final Random random = new Random(seed);
+
+		final ArrayProxy<Long> proxy = new ArrayProxyImpl<>(1000);
+		for (int i = 0; i < proxy._length; ++i) {
+			proxy.set(i, random.nextLong());
+		}
+
+		final List<Long> list = new ArrayProxyList<>(proxy);
+		final Long[] array = list.toArray(new Long[0]);
+
+		for (int i = 0; i < array.length; ++i) {
+			Assert.assertEquals(array[i], proxy.get(i));
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
