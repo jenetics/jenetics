41ee10a0cb75d4451b492fad723d9cfafab931a9
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-07-10 15:54:29 +0200

    Simplify 'bit.flip' method.
    --HG--
    branch : r1.4.0

--- org.jenetics/src/main/java/org/jenetics/util/bit.java
+++ org.jenetics/src/main/java/org/jenetics/util/bit.java
@@ -358,18 +358,7 @@ public final class bit extends StaticObject {
 	 * @throws NullPointerException if the {@code data} array is {@code null}.
 	 */
 	public static byte[] flip(final byte[] data, final int index) {
-		final int bytes = index >>> 3; // = index/8
-		final int bits = index & 7;    // = index%8
-		int d = data[bytes] & 0xFF;
-
-		if ((d & (1 << bits)) == 0) {
-			d |= (1 << bits);
-		} else {
-			d &= ~(1 << bits);
-		}
-		data[bytes] = (byte)d;
-
-		return data;
+		return get(data, index) ? unset(data, index) : set(data, index);
 	}
 
 	/**
