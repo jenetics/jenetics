331ef51e815c068fb63207c276a06f1f1063d9bf
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-07-10 19:32:01 +0200

    Fix 'ArraySeqIterator' implementation.

--- org.jenetics/src/main/java/org/jenetics/util/ArraySeqIterator.java
+++ org.jenetics/src/main/java/org/jenetics/util/ArraySeqIterator.java
@@ -39,12 +39,12 @@ class ArraySeqIterator<T> implements ListIterator<T> {
 
 	public ArraySeqIterator(final ArraySeq<T> array) {
 		_array = array;
-		_pos = array._start - 1;
+		_pos = array._start;
 	}
 
 	@Override
 	public boolean hasNext() {
-		return _pos < _array._end - 1;
+		return _pos < _array._end;
 	}
 
 	@Override
@@ -53,12 +53,12 @@ class ArraySeqIterator<T> implements ListIterator<T> {
 		if (!hasNext()) {
 			throw new NoSuchElementException();
 		}
-		return (T)_array._array.data[++_pos];
+		return (T)_array._array.data[_pos++];
 	}
 
 	@Override
 	public int nextIndex() {
-		return _pos + 1;
+		return _pos;
 	}
 
 	@Override
