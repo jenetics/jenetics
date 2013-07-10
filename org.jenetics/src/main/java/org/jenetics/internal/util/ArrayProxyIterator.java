69d011eebc90f0fe13e519a5368db25c322a08d8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-07-10 19:11:41 +0200

    Fix ListIterator implementation.
    --HG--
    branch : r1.4.0

--- org.jenetics/src/main/java/org/jenetics/internal/util/ArrayProxyIterator.java
+++ org.jenetics/src/main/java/org/jenetics/internal/util/ArrayProxyIterator.java
@@ -35,7 +35,7 @@ public class ArrayProxyIterator<T> implements ListIterator<T> {
 
 	final ArrayProxy<T> _proxy;
 
-	private int _pos = -1;
+	private int _pos = 0;
 
 	public ArrayProxyIterator(final ArrayProxy<T> proxy) {
 		_proxy = proxy;
@@ -43,7 +43,7 @@ public class ArrayProxyIterator<T> implements ListIterator<T> {
 
 	@Override
 	public boolean hasNext() {
-		return _pos < _proxy._length - 1;
+		return _pos < _proxy._length;
 	}
 
 	@Override
@@ -51,7 +51,7 @@ public class ArrayProxyIterator<T> implements ListIterator<T> {
 		if (!hasNext()) {
 			throw new NoSuchElementException();
 		}
-		return _proxy.uncheckedGet(++_pos);
+		return _proxy.uncheckedGet(_pos++);
 	}
 
 	@Override
