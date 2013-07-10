66fb54750ad447b0d645b2f005d067d775db62b5
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-07-10 15:55:02 +0200

    Simplify 'bit.flip' test.

--- org.jenetics/src/test/java/org/jenetics/util/bitTest.java
+++ org.jenetics/src/test/java/org/jenetics/util/bitTest.java
@@ -33,7 +33,7 @@ import org.testng.annotations.Test;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version <em>$Date: 2013-07-05 $</em>
+ * @version <em>$Date: 2013-07-10 $</em>
  */
 public class bitTest {
 
@@ -214,12 +214,12 @@ public class bitTest {
 
 	@Test
 	public void flip() {
-		final long seed = System.currentTimeMillis();
+		final long seed = math.random.seed();
 		final Random random = new Random(seed);
-		final byte[] data = new byte[4];
+		final byte[] data = new byte[1000];
 
-		for (int i = 0; i < data.length*8; ++i) {
-			bit.set(data, i, random.nextBoolean());
+		for (int i = 0; i < data.length; ++i) {
+			data[i] = (byte)random.nextInt();
 		}
 
 		final byte[] cdata = data.clone();
