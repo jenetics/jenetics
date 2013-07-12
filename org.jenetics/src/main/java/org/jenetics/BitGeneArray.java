7c011aee25329d4db9a92fd608baf6fd76cf5a88
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-07-12 22:50:12 +0200

    Optimize 'BitGeneArray.Proxy.copy()' method.

--- org.jenetics/src/main/java/org/jenetics/BitGeneArray.java
+++ org.jenetics/src/main/java/org/jenetics/BitGeneArray.java
@@ -31,7 +31,7 @@ import org.jenetics.util.bit;
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.4
- * @version 1.4 &mdash; <em>$Date: 2013-07-11 $</em>
+ * @version 1.4 &mdash; <em>$Date: 2013-07-12 $</em>
  */
 final class BitGeneArray extends ArrayProxyMSeq<BitGene> {
 
@@ -156,8 +156,20 @@ final class BitGeneArray extends ArrayProxyMSeq<BitGene> {
 		@Override
 		public Proxy copy() {
 			final Proxy proxy = new Proxy(_length);
-			for (int i = _length; --i >= 0;) {
-				bit.set(proxy._array, i, bit.get(_array, i + _start));
+			if (_start == 0 && _end == _length) {
+				proxy._array = _array.clone();
+			} else if (_start == 0) {
+				System.arraycopy(
+					_array, 0, proxy._array, 0, proxy._array.length
+				);
+			} else if ((_start & 7) == 0) {
+				System.arraycopy(
+					_array, _start >>> 3, proxy._array, 0, proxy._array.length
+				);
+			} else {
+				for (int i = _length; --i >= 0;) {
+					bit.set(proxy._array, i, bit.get(_array, i + _start));
+				}
 			}
 			return proxy;
 		}
