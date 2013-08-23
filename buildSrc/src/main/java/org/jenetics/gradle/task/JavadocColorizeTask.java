e47f7faa62337e4a164b5f62d482fee2e4edf73c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-08-23 13:00:50 +0200

    Build script improvements.

--- buildSrc/src/main/java/org/jenetics/gradle/task/JavadocColorizeTask.java
+++ buildSrc/src/main/java/org/jenetics/gradle/task/JavadocColorizeTask.java
@@ -1,14 +1,55 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *     Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ *
+ */
 package org.jenetics.gradle.task;
 
+import java.io.File;
+
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @since 1.4
+ * @version 1.4 &mdash; <em>$Date: 2013-08-23 $</em>
+ */
 public class JavadocColorizeTask extends DefaultTask {
 
+	private File _directory;
+
+	@Input
+	public File getDirectory() {
+		return _directory;
+	}
+
+	public void setDirectory(final File directory) {
+		_directory = directory;
+	}
 
 	@TaskAction
 	public void run() {
 		System.out.println("Executing: " + getClass().getName());
+		System.out.println("Directory: " + getDirectory());
 	}
 
 }
