eef6b848d4db2c9953b6f68c967c44066b7a3251
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-10-07 11:50:26 +0200

    Remove unused import.

--- org.jenetics/src/test/java/org/jenetics/GeneticAlgorithmTest.java
+++ org.jenetics/src/test/java/org/jenetics/GeneticAlgorithmTest.java
@@ -25,23 +25,21 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 
-import javolution.context.ConcurrentContext;
 import javolution.context.LocalContext;
-
-import org.jscience.mathematics.number.Float64;
-import org.testng.Assert;
-import org.testng.Reporter;
-import org.testng.annotations.Test;
 
 import org.jenetics.util.Concurrent;
 import org.jenetics.util.Factory;
 import org.jenetics.util.ForkJoinContext;
 import org.jenetics.util.Function;
 import org.jenetics.util.RandomRegistry;
+import org.jscience.mathematics.number.Float64;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version <em>$Date: 2013-10-06 $</em>
+ * @version <em>$Date: 2013-10-07 $</em>
  */
 public class GeneticAlgorithmTest {
 
