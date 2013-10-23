5bed4e5f3be87f82e55a2b5ae9bcb1e96642b1bc
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-10-23 20:21:04 +0200

    Organize import statements.
    --HG--
    branch : r1.5.0

--- org.jenetics/src/test/java/org/jenetics/GeneticAlgorithmTest.java
+++ org.jenetics/src/test/java/org/jenetics/GeneticAlgorithmTest.java
@@ -27,14 +27,15 @@ import java.util.concurrent.ForkJoinPool;
 
 import javolution.context.LocalContext;
 
-import org.jenetics.util.Concurrent;
-import org.jenetics.util.Factory;
-import org.jenetics.util.Function;
-import org.jenetics.util.RandomRegistry;
 import org.jscience.mathematics.number.Float64;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
+
+import org.jenetics.util.Concurrent;
+import org.jenetics.util.Factory;
+import org.jenetics.util.Function;
+import org.jenetics.util.RandomRegistry;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
