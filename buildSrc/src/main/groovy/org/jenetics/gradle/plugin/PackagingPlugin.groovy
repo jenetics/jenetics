83c24de72c73aeb63d6f6b6a17389d9240d7ad45
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-10-25 22:32:25 +0200

    Implement 'PackagingPlugin'.

--- buildSrc/src/main/groovy/org/jenetics/gradle/plugin/PackagingPlugin.groovy
+++ buildSrc/src/main/groovy/org/jenetics/gradle/plugin/PackagingPlugin.groovy
@@ -23,6 +23,10 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
 
+import org.apache.tools.ant.filters.ReplaceTokens
+
+import org.jenetics.gradle.Version
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since @__version__@
@@ -30,15 +34,57 @@ import org.gradle.api.tasks.bundling.Jar
  */
 class PackagingPlugin implements Plugin<Project> {
 
+	private static final def TEXT_FILE_PATTERN = [
+		'**/*.java',
+		'**/*.gradle',
+		'**/*.cpp',
+		'**/*.hpp',
+		'**/*.c',
+		'**/*.h',
+		'**/*.bat',
+		'**/*.sh',
+		'**/*.txt',
+		'**/*.properties',
+		'**/*.md',
+		'**/*.log'
+	]
+
+	private final Calendar now = Calendar.getInstance()
+	private final int year = now.get(Calendar.YEAR)
+	private final String copyrightYear = "2007-${year}"
+
+	private Project project = null
+	private String identifier = null
+	private String version = null
+	private File buildDir = null
+
+	private def textContentReplacements = [:]
+
 	@Override
 	void apply(final Project project) {
+		this.project = project
+
+		version = project.rootProject.version
+		buildDir = project.rootProject.buildDir
+		identifier = "${project.rootProject.name}-${version}"
+		textContentReplacements = [
+			__identifier__: identifier,
+			__year__: copyrightYear
+		]
+
 		project.extensions.create('packaging', PackagingPluginExtension)
 
-		jarjar(project)
-		packaging(project)
+		if (project.plugins.hasPlugin('java')) {
+			jarjar()
+		}
+		packaging()
 	}
 
-	private void jarjar(final Project project) {
+	private boolean isRootProject() {
+		return project == project.rootProject
+	}
+
+	private void jarjar() {
 		project.task('jarjar', type: Jar, dependsOn: 'jar') {
 			baseName = "${project.name}-all"
 
@@ -63,9 +109,71 @@ class PackagingPlugin implements Plugin<Project> {
 		}
 	}
 
-	private void packaging(final Project project) {
+	private void packaging() {
 		project.task('packaging') {
+			ext {
+				identifier = this.identifier
+				exportDir = new File("${buildDir}/package/${identifier}")
+				exportProjectDir = new File("${exportDir}/project")
+				exportProjectLibDir = new File("${exportProjectDir}/buildSrc/lib")
+				exportLibDir = new File("${exportDir}/lib")
+				exportJavadocDir = new File("${exportDir}/javadoc")
+				exportReportDir = new File("${exportDir}/report")
+				exportScriptDir = new File("${exportDir}/script")
+			}
 
+			doLast {
+				if (isRootProject()) {
+					copyRoot()
+				} else {
+					copy()
+				}
+			}
+		}
+	}
+
+	private void copy() {
+		println("Copying '${project}'")
+	}
+
+	private void copyRoot() {
+		def task = project.tasks.getByName('packaging')
+
+		// Copy the files in the root directory.
+		project.copy {
+			from('.') {
+				include '*'
+				excludes = [
+					'org.*', '.gradle', 'gradle-app.setting', '.hgignore',
+					'.hgtags', '*.iml', '*.ipr', '*.iws', '.project',
+					'.classpath', '.settings', 'build', 'out'
+				]
+			}
+			into task.exportProjectDir
+			filter(ReplaceTokens, tokens: textContentReplacements)
+		}
+
+		copyDir('gradle', task.exportProjectDir)
+	}
+
+	private void copyDir(final String source, final File target) {
+		// Copy the text files with text pattern replacement.
+		project.copy {
+			from(source) {
+				includes = TEXT_FILE_PATTERN
+				into source
+			}
+			into target
+			filter(ReplaceTokens, tokens: textContentReplacements)
+		}
+
+		// Copy the rest, without replacement.
+		project.copy {
+			from(source) {
+				excludes = TEXT_FILE_PATTERN
+				into source
+			}
+			into target
 		}
 	}
 
@@ -76,6 +184,32 @@ class PackagingPluginExtension {
 	String author = 'Franz Wilhelmstötter'
 	String url = 'http://jenetics.sourceforge.net'
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
