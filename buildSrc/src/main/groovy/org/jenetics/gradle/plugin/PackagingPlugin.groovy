17fec8758b8224bbce2e96f0ca674a872f197136
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-10-26 00:59:49 +0200

    Implement 'PackagingPlugin'.

--- buildSrc/src/main/groovy/org/jenetics/gradle/plugin/PackagingPlugin.groovy
+++ buildSrc/src/main/groovy/org/jenetics/gradle/plugin/PackagingPlugin.groovy
@@ -30,7 +30,7 @@ import org.jenetics.gradle.Version
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since @__version__@
- * @version @__version__@ &mdash; <em>$Date: 2013-10-25 $</em>
+ * @version @__version__@ &mdash; <em>$Date: 2013-10-26 $</em>
  */
 class PackagingPlugin implements Plugin<Project> {
 
@@ -119,7 +119,10 @@ class PackagingPlugin implements Plugin<Project> {
 			dependencies += TASK_NAME_JARJAR
 		}
 		if (project.tasks.findByPath('build') != null) {
-			//dependencies += 'build'
+			dependencies += 'build'
+		}
+		if (project.tasks.findByPath('jarjar') != null) {
+			dependencies += 'jarjar'
 		}
 		if (project.tasks.findByPath('javadoc') != null) {
 			dependencies += 'javadoc'
@@ -144,17 +147,66 @@ class PackagingPlugin implements Plugin<Project> {
 					copy()
 				}
 			}
+		}
 
+		// Copy the test-reports
+		if (project.tasks.findByPath('test') != null) {
+			project.tasks.findByPath('test').doLast {
+				project.copy {
+					from("${project.buildDir}/reports") {
+						include 'tests/**'
+						include 'jacoco/**'
+						include '*.gradle'
+						exclude '.gradle'
+					}
+					into exportReportDir
+				}
+			}
 		}
 
 		// Copy the javadoc.
 		if (project.tasks.findByPath('javadoc') != null) {
 			project.tasks.findByPath('javadoc').doLast {
 				copyDir(
-					new File("${project.buildDir}/docs/javadoc"),
+					new File(project.buildDir, 'docs/javadoc'),
 					project.name,
 					exportJavadocDir
 				)
+			}
+		}
+
+		// Copy the pdf manual.
+		if (project.tasks.findByPath('lyx') != null) {
+			project.tasks.findByPath('build').doLast {
+				project.copy {
+					from("${project.buildDir}/doc") {
+						include '*.pdf'
+					}
+					into exportDir
+				}
+			}
+		}
+
+		// Copy the external libraries.
+		if (project.plugins.hasPlugin('java')) {
+			project.tasks.findByPath('build').doLast {
+				// Copy the external jar dependencies.
+				project.configurations.testRuntime.each { jar ->
+					if (jar.name.endsWith('.jar') &&
+						!jar.name.startsWith('org.jeneti'))
+					{
+						project.copy {
+							from jar
+							into exportProjectLibDir
+						}
+					}
+				}
+
+				// Copy the build library
+				project.copy {
+					from("${project.buildDir}/libs")
+					into exportLibDir
+				}
 			}
 		}
 
@@ -249,7 +301,8 @@ class PackagingPlugin implements Plugin<Project> {
 		'**/*.md',
 		'**/*.log',
 		'**/*.xml',
-		'**/*.html'
+		'**/*.html',
+		'**/*.lyx'
 	]
 
 }
