d817165d78b0b8d113f7c7608e73bd503d1c85b2
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-11-23 00:39:34 +0100

    Add 'dieharder' test results for 'LCG64ShiftRandom' class.
    --HG--
    branch : r1.5.0

--- org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
+++ org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
@@ -610,6 +610,7 @@ public class LCG64ShiftRandom extends Random64 {
 # Testing: org.jenetics.util.LCG64ShiftRandom                                 #
 #=============================================================================#
 #=============================================================================#
+# Linux 3.11.0-13-generic (amd64)                                             #
 # java version "1.7.0_45"                                                     #
 # Java(TM) SE Runtime Environment (build 1.7.0_45-b18)                        #
 # Java HotSpot(TM) 64-Bit Server VM (build 24.45-b08)                         #
