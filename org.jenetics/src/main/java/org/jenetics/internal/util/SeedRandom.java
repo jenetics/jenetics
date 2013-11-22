2691b615c64612a3c58541a64c8f36baf3308a71
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-11-23 00:39:48 +0100

    Add 'dieharder' test results for 'SeedRandom' class.
    --HG--
    branch : r1.5.0

--- org.jenetics/src/main/java/org/jenetics/internal/util/SeedRandom.java
+++ org.jenetics/src/main/java/org/jenetics/internal/util/SeedRandom.java
@@ -43,6 +43,7 @@ public class SeedRandom extends Random64 {
 # Testing: org.jenetics.internal.util.SeedRandom                              #
 #=============================================================================#
 #=============================================================================#
+# Linux 3.11.0-13-generic (amd64)                                             #
 # java version "1.7.0_45"                                                     #
 # Java(TM) SE Runtime Environment (build 1.7.0_45-b18)                        #
 # Java HotSpot(TM) 64-Bit Server VM (build 24.45-b08)                         #
