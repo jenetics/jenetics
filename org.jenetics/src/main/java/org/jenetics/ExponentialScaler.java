f0d375aae82796b22dc6ac4c97994310d318aff8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2013-11-28 22:39:41 +0100

    Java 8 javadoc preparation.

--- org.jenetics/src/main/java/org/jenetics/ExponentialScaler.java
+++ org.jenetics/src/main/java/org/jenetics/ExponentialScaler.java
@@ -34,13 +34,13 @@ import org.jenetics.util.Function;
 /**
  * Implements an exponential fitness scaling, whereby all fitness values are
  * modified the following way.
- * <p/><img src="doc-files/exponential-scaler.gif"
+ * <p><img src="doc-files/exponential-scaler.gif"
  *          alt="f_s=\left(a\cdot f+b \rigth)^c"
- *     />.</p>
+ *     >.</p>
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2013-09-01 $</em>
+ * @version 1.0 &mdash; <em>$Date: 2013-11-28 $</em>
  */
 public final class ExponentialScaler
 	implements
