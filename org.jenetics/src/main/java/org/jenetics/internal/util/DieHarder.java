42d59295c300860b1f993d6cbba2a849d776ca94
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-01-10 19:47:18 +0100

    Improve 'DieHarder' test class.

--- org.jenetics/src/main/java/org/jenetics/internal/util/DieHarder.java
+++ org.jenetics/src/main/java/org/jenetics/internal/util/DieHarder.java
@@ -27,11 +27,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
-
+import java.util.regex.Pattern;
 
 /**
  * Class for testing a given random engine using the
@@ -43,6 +44,119 @@ import java.util.Random;
  * @version 1.5 &mdash; <em>$Date: 2014-01-10 $</em>
  */
 public final class DieHarder {
+	private final Random _random;
+	private final Appendable _output;
+	private final boolean _verbose;
+
+	private List<TestResult> _results = new ArrayList<>();
+
+	public DieHarder(final Random random, final Appendable output, final boolean verbose) {
+		_random = Objects.requireNonNull(random);
+		_output = Objects.requireNonNull(output);
+		_verbose = verbose;
+	}
+
+	public DieHarder(final Random random) {
+		this(random, System.out, true);
+	}
+
+	public void run() throws IOException, InterruptedException {
+		final List<String> dieharderArgs = new ArrayList<>();
+		dieharderArgs.add("dieharder");
+
+		// The random number to test are read from stdin_input_raw.
+		dieharderArgs.add("-g");
+		dieharderArgs.add("200");
+
+		// Perform all random tests.
+		dieharderArgs.add("-a");
+
+		printv();
+
+		final long start = System.currentTimeMillis();
+		final ProcessBuilder builder = new ProcessBuilder(dieharderArgs);
+		final Process dieharder = builder.start();
+
+		final Thread randomizer = new Thread(new Randomizer(
+			_random,
+			dieharder.getOutputStream()
+		));
+		randomizer.start();
+
+		final BufferedReader stdout = new BufferedReader (
+			new InputStreamReader(dieharder.getInputStream())
+		);
+		String line = null;
+		while ((line = stdout.readLine()) != null) {
+			final TestResult result = TestResult.valueOf(line);
+			if (result != null) {
+				_results.add(result);
+				System.out.println(result);
+			}
+
+			if (_verbose) {
+				println(line);
+			}
+		}
+
+		dieharder.waitFor();
+		randomizer.interrupt();
+		final long sec = (System.currentTimeMillis() - start)/1000;
+
+		/*
+		printt(
+			"Summary: PASSED: %d, FAILED: %d, WEAK: %d",
+			_passedTestCounter, _failedTestCounter, _weakTestCounter
+		);
+		*/
+		printt("Runtime: %d:%02d:%02d", sec/3600, (sec%3600)/60, (sec%60));
+	}
+
+	public Random getRandom() {
+		return _random;
+	}
+
+	public List<TestResult> getResults() {
+		return Collections.unmodifiableList(_results);
+	}
+
+	private void printt(final String title, final Object... args) throws IOException {
+		println("#=============================================================================#");
+		println("# %-76s#", format(title, args));
+		println("#=============================================================================#");
+	}
+
+	private void printv() throws IOException {
+		println("#=============================================================================#");
+		println(
+			"# %-76s#",
+			format("%s %s (%s) ", p("os.name"), p("os.version"), p("os.arch"))
+		);
+		println(
+			"# %-76s#",
+			format("java version \"%s\"", p("java.version"))
+		);
+		println(
+			"# %-76s#",
+			format("%s (build %s)", p("java.runtime.name"), p("java.runtime.version"))
+		);
+		println(
+			"# %-76s#",
+			format("%s (build %s)", p("java.vm.name"), p("java.vm.version"))
+		);
+		println("#=============================================================================#");
+	}
+
+	private static String p(final String name) {
+		return System.getProperty(name);
+	}
+
+	private void println(final String pattern, final Object... args)
+		throws IOException
+	{
+		_output.append(format(pattern, args));
+		_output.append(System.lineSeparator());
+	}
 
 	/**
 	 * Writes random numbers to an given data output stream.
@@ -71,110 +185,108 @@ public final class DieHarder {
 			} catch (IOException ignore) {
 			}
 		}
-
 	}
 
+	private static final class TestResult {
+		public static enum Assessment {
+			PASSED, FAILED, WEAK;
+
+			/*
+			private Assessment valueOf(final String name) {
+				switch (name) {
+					case "PASSED": return PASSED;
+					case "FAILED": return FAILED;
+					case "WEAK": return WEAK;
+					default: return null;
+				}
+			}
+			*/
+		}
+
+		public final String testName;
+		public final int ntup;
+		public final int tsamples;
+		public final int psamples;
+		public final double pvalue;
+		public final Assessment assessment;
+
+		public TestResult(
+			final String testName,
+			final int ntup,
+			final int tsamples,
+			final int psamples,
+			final double pvalue,
+			final Assessment assessment
+		) {
+			this.testName = testName;
+			this.ntup = ntup;
+			this.tsamples = tsamples;
+			this.psamples = psamples;
+			this.pvalue = pvalue;
+			this.assessment = assessment;
+		}
+
+		public static TestResult valueOf(final String line) {
+			TestResult result = null;
+			if (isResultLine(line)) {
+				final String[] columns = line.split(Pattern.quote("|"));
+				result = new TestResult(
+					columns[0].trim(),
+					Integer.parseInt(columns[1].trim()),
+					Integer.parseInt(columns[2].trim()),
+					Integer.parseInt(columns[3].trim()),
+					Double.parseDouble(columns[4].trim()),
+					Assessment.valueOf(columns[5].trim())
+				);
+			}
+
+			return result;
+		}
+
+		private static boolean isResultLine(final String line) {
+			return line.contains("PASSED") ||
+				line.contains("FAILED") ||
+				line.contains("WEAK");
+		}
+
+		@Override
+		public String toString() {
+			return String.format(
+				"Result[name=%s, ntup=%d, tsamples=%d, psamples=%d, pvalue=%s, assessment=%s]",
+				testName, ntup, tsamples, psamples, pvalue, assessment
+			);
+		}
+	}
+
+	/**
+	 * Command line interface for the dieharder Random class tester.
+	 */
 	public static void main(final String[] args) throws Exception {
 		if ( args.length < 1) {
-			println("Usage: java org.jenetics.internal.util.DieHarder <random-class-name>");
-			return;
-		}
-
-		final String randomName = args[0];
-		Random random = null;
-		try {
-			random = (Random)Class.forName(randomName).newInstance();
-			printt(
-				"Testing: %s (%s)",
-				randomName,
-				new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())
+			System.out.println(
+				"Usage: java org.jenetics.internal.util.DieHarder <random-class-name>"
 			);
-		} catch (Exception e) {
-			System.out.println("Can't create random class " + randomName);
 			return;
 		}
 
-		final List<String> dieharderArgs = new ArrayList<>();
-		dieharderArgs.add("dieharder");
-		for (int i  = 1; i < args.length; ++i) {
-			dieharderArgs.add(args[i]);
+		final DieHarder dieHarder = new DieHarder(newInstance(args[0]));
+		dieHarder.printt(
+			"Testing: %s (%s)",
+			dieHarder.getRandom().getClass().getName(),
+			new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())
+		);
+
+		dieHarder.run();
+	}
+
+	private static Random newInstance(final String randomName) {
+		try {
+			return (Random)Class.forName(randomName).newInstance();
+		} catch (Exception e) {
+			System.err.println("Can't create random class " + randomName);
+			System.exit(1);
 		}
-		dieharderArgs.add("-g");
-		dieharderArgs.add("200");
-
-		printv();
-
-		final long start = System.currentTimeMillis();
-		final ProcessBuilder builder = new ProcessBuilder(dieharderArgs);
-		final Process dieharder = builder.start();
-
-		final Thread randomizer = new Thread(new Randomizer(
-			random,
-			dieharder.getOutputStream()
-		));
-		randomizer.start();
-
-		int passedTestCounter = 0;
-		int failedTestCounter = 0;
-		int weakTestCounter = 0;
-		
-		final BufferedReader stdout = new BufferedReader (
-			new InputStreamReader(dieharder.getInputStream())
-		);
-		String line = null;
-		while ((line = stdout.readLine()) != null) {
-			if (line.contains("PASSED")) ++passedTestCounter;
-			if (line.contains("FAILED")) ++failedTestCounter;
-			if (line.contains("WEAK")) ++weakTestCounter;
-			
-			System.out.println(line);
-		}
-
-		dieharder.waitFor();
-		randomizer.interrupt();
-		final long sec = (System.currentTimeMillis() - start)/1000;
-
-		printt(
-			"Summary: PASSED: %d, FAILED: %d, WEAK: %d", 
-			passedTestCounter, failedTestCounter, weakTestCounter
-		);
-		printt("Runtime: %d:%02d:%02d", sec/3600, (sec%3600)/60, (sec%60));
-
-	}
-
-	private static void printt(final String title, final Object... args) {
-		println("#=============================================================================#");
-		println("# %-76s#", format(title, args));
-		println("#=============================================================================#");
-	}
-
-	private static void printv() {
-		println("#=============================================================================#");
-		println(
-			"# %-76s#",
-			format("%s %s (%s) ", p("os.name"), p("os.version"), p("os.arch"))
-		);
-		println(
-			"# %-76s#",
-			format("java version \"%s\"", p("java.version"))
-		);
-		println(
-			"# %-76s#",
-			format("%s (build %s)", p("java.runtime.name"), p("java.runtime.version"))
-		);
-		println(
-			"# %-76s#",
-			format("%s (build %s)", p("java.vm.name"), p("java.vm.version"))
-		);
-		println("#=============================================================================#");
-	}
-
-	private static String p(final String name) {
-		return System.getProperty(name);
-	}
-
-	private static void println(final String pattern, final Object... args) {
-		System.out.println(format(pattern, args));
+		return null;
 	}
 
 }
