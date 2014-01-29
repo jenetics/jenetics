e07b6f2ff3a950b641546332b344c2345e19edc7
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-01-29 22:39:14 +0100

    Remove unused Factory objects.
    --HG--
    branch : org.jenetics-jaxb

--- org.jenetics/src/main/java/org/jenetics/internal/math/random.java
+++ org.jenetics/src/main/java/org/jenetics/internal/math/random.java
@@ -19,17 +19,6 @@
  */
 package org.jenetics.internal.math;
 
-import static org.jenetics.util.math.random.nextDouble;
-import static org.jenetics.util.math.random.nextInt;
-import static org.jenetics.util.math.random.nextLong;
-
-import java.util.Random;
-
-import org.jscience.mathematics.number.Float64;
-import org.jscience.mathematics.number.Integer64;
-
-import org.jenetics.util.Factory;
-import org.jenetics.util.RandomRegistry;
 import org.jenetics.util.StaticObject;
 
 /**
@@ -41,12 +30,6 @@ import org.jenetics.util.StaticObject;
  */
 public final class random extends StaticObject {
 	private random() {}
-
-	public static char nextPrintableChar(final Random random) {
-		final int low = 33;
-		final int high = 127;
-		return (char)(random.nextInt(high - low) + low);
-	}
 
 	/*
 	 * Conversion methods used by the 'Random' engine from the JDK.
@@ -86,139 +69,6 @@ public final class random extends StaticObject {
 
 	public static double toDouble2(final int a, final int b) {
 		return (((long)(a >>> 6) << 26) | (b >>> 6))*0x1.0p-52d;
-	}
-
-	public static Factory<Byte> ByteFactory(
-		final Random random,
-		final byte min, final byte max
-	) {
-		return new Factory<Byte>() {
-			@Override public Byte newInstance() {
-				return (byte)nextInt(random, min, max);
-			}
-		};
-	}
-
-	public static Factory<Byte> ByteFactory(final byte min, final byte max) {
-		return ByteFactory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Character> CharacterFactory(final Random random) {
-		return new Factory<Character>() {
-			@Override public Character newInstance() {
-				return nextPrintableChar(random);
-			}
-		};
-	}
-
-	public static Factory<Character> CharacterFactory() {
-		return CharacterFactory(RandomRegistry.getRandom());
-	}
-
-	public static Factory<String> StringFactory(final Random random) {
-		return new Factory<String>() {
-			@Override public String newInstance() {
-				final StringBuilder builder = new StringBuilder();
-				for (int i = 0, n = random.nextInt(20) + 5; i < n; ++i) {
-					builder.append(nextPrintableChar(random));
-				}
-				return builder.toString();
-			}
-		};
-	}
-
-	public static Factory<String> StringFactory() {
-		return StringFactory(RandomRegistry.getRandom());
-	}
-
-	public static Factory<Long> LongFactory(
-		final Random random,
-		final long min, final long max
-	) {
-		return new Factory<Long>() {
-			@Override public Long newInstance() {
-				return nextLong(random, min, max);
-			}
-		};
-	}
-
-	public static Factory<Short> ShortFactory(final short min, final short max) {
-		return ShortFactory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Short> ShortFactory(
-		final Random random,
-		final short min, final short max
-	) {
-		return new Factory<Short>() {
-			@Override public Short newInstance() {
-				return (short)nextInt(random, min, max);
-			}
-		};
-	}
-
-	public static Factory<Integer> IntegerFactory(final int min, final int max) {
-		return IntegerFactory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Integer> IntegerFactory(
-		final Random random,
-		final int min, final int max
-	) {
-		return new Factory<Integer>() {
-			@Override public Integer newInstance() {
-				return nextInt(random, min, max);
-			}
-		};
-	}
-
-	public static Factory<Long> LongFactory(final long min, final long max) {
-		return LongFactory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Float64> Float64Factory(
-		final Random random,
-		final double min, final double max
-	) {
-		return new Factory<Float64>() {
-			@Override public Float64 newInstance() {
-				return Float64.valueOf(nextDouble(random, min, max));
-			}
-		};
-	}
-
-	public static Factory<Float64> Float64Factory(final double min, final double max) {
-		return Float64Factory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Double> DoubleFactory(
-		final Random random,
-		final double min, final double max
-	) {
-		return new Factory<Double>() {
-			@Override public Double newInstance() {
-				return nextDouble(random, min, max);
-			}
-		};
-	}
-
-	public static Factory<Double> DoubleFactory(final double min, final double max) {
-		return DoubleFactory(RandomRegistry.getRandom(), min, max);
-	}
-
-	public static Factory<Integer64> Integer64Factory(
-		final Random random,
-		final long min, final long max
-	) {
-		return new Factory<Integer64>() {
-			@Override public Integer64 newInstance() {
-				return Integer64.valueOf(nextLong(random, min, max));
-			}
-		};
-	}
-
-	public static Factory<Integer64> Integer64Factory(final long min, final long max) {
-		return Integer64Factory(RandomRegistry.getRandom(), min, max);
 	}
 
 }
