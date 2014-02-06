f20243112dc04151f0a22ab758dd8cbbeff39320
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-02-06 21:16:03 +0100

    Add 'LongChromosome' class.
    --HG--
    branch : org.jenetics-number_gene

---
+++ org.jenetics/src/main/java/org/jenetics/LongChromosome.java
@@ -0,0 +1,88 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package org.jenetics;
+
+import static org.jenetics.util.object.hashCodeOf;
+
+import java.io.Serializable;
+
+import org.jenetics.util.Array;
+import org.jenetics.util.ISeq;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version @__version__@ &mdash; <em>$Date$</em>
+ * @since @__version__@
+ */
+public class LongChromosome
+	extends NumericChromosome<Long, LongGene>
+	implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+
+	protected LongChromosome(final ISeq<LongGene> genes) {
+		super(genes);
+	}
+
+	/**
+	 * Create a new random {@code LongChromosome}.
+	 *
+	 * @param min the min value of the {@link DoubleGene}s (inclusively).
+	 * @param max the max value of the {@link DoubleGene}s (inclusively).
+	 * @param length the length of the chromosome.
+	 */
+	public LongChromosome(final Long min,final Long max,final int length) {
+		this(
+			new Array<LongGene>(length).fill(new LongGene(min, max)).toISeq()
+		);
+		_valid = true;
+	}
+
+	/**
+	 * Create a new random {@code LongChromosome} of length one.
+	 *
+	 * @param min the minimal value of this chromosome (inclusively).
+	 * @param max the maximal value of this chromosome (inclusively).
+	 */
+	public LongChromosome(final Long min, final Long max) {
+		this(min, max, 1);
+	}
+
+	@Override
+	public LongChromosome newInstance(final ISeq<LongGene> genes) {
+		return new LongChromosome(genes);
+	}
+
+	@Override
+	public LongChromosome newInstance() {
+		return new LongChromosome(_min, _max, length());
+	}
+
+	@Override
+	public int hashCode() {
+		return hashCodeOf(getClass()).and(super.hashCode()).value();
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return o == this || o instanceof LongChromosome && super.equals(o);
+	}
+}
