f323e6c99b2615311a8c260abf8d1c27cd884511
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-02-14 17:17:02 +0100

    Remove usage of deprecated classes in javadoc.
    --HG--
    branch : org.jenetics-number_gene

--- org.jenetics/src/main/java/org/jenetics/Alterer.java
+++ org.jenetics/src/main/java/org/jenetics/Alterer.java
@@ -26,11 +26,11 @@ package org.jenetics;
  * {@link GeneticAlgorithm#setAlterers(Alterer...)} method.
  *
  * [code]
- * final GeneticAlgorithm〈Float64Gene, Float64〉 ga = ...
+ * final GeneticAlgorithm〈DoubleGene, Double〉 ga = ...
  * ga.setAlterers(
- *     new Crossover〈Float64Gene〉(0.1),
- *     new Mutator〈Float64Gene〉(0.05),
- *     new MeanAlterer〈Float64Gene〉(0.2)
+ *     new Crossover〈DoubleGene〉(0.1),
+ *     new Mutator〈DoubleGene〉(0.05),
+ *     new MeanAlterer〈DoubleGene〉(0.2)
  * );
  * [/code]
  *
