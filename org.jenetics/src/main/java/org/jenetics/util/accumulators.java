510b9bc8699957fdd669d8b9500314bbe8c3c4c3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-02-23 18:44:08 +0100

    Mark sum accumulator as deprecated.

--- org.jenetics/src/main/java/org/jenetics/util/accumulators.java
+++ org.jenetics/src/main/java/org/jenetics/util/accumulators.java
@@ -35,7 +35,7 @@ import org.jscience.mathematics.structure.GroupAdditive;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date: 2014-02-17 $</em>
+ * @version 1.0 &mdash; <em>$Date: 2014-02-23 $</em>
  */
 public final class accumulators extends StaticObject {
 	private accumulators() {}
@@ -56,7 +56,7 @@ public final class accumulators extends StaticObject {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.0
-	 * @version 1.0 &ndash; <em>$Date: 2014-02-17 $</em>
+	 * @version 1.0 &ndash; <em>$Date: 2014-02-23 $</em>
 	 */
 	public static final class Min<C extends Comparable<? super C>>
 		extends MappedAccumulator<C>
@@ -149,7 +149,7 @@ public final class accumulators extends StaticObject {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.0
-	 * @version 1.0 &ndash; <em>$Date: 2014-02-17 $</em>
+	 * @version 1.0 &ndash; <em>$Date: 2014-02-23 $</em>
 	 */
 	public static final class Max<C extends Comparable<? super C>>
 		extends MappedAccumulator<C>
@@ -242,7 +242,7 @@ public final class accumulators extends StaticObject {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.0
-	 * @version 1.0 &ndash; <em>$Date: 2014-02-17 $</em>
+	 * @version 1.0 &ndash; <em>$Date: 2014-02-23 $</em>
 	 */
 	public static final class MinMax<C extends Comparable<? super C>>
 		extends MappedAccumulator<C>
@@ -351,8 +351,11 @@ public final class accumulators extends StaticObject {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.0
-	 * @version 1.0 &ndash; <em>$Date: 2014-02-17 $</em>
+	 * @version 1.0 &ndash; <em>$Date: 2014-02-23 $</em>
+	 *
+	 * @deprecated Will be removed.
 	 */
+	@Deprecated
 	public static class Sum<G extends GroupAdditive<G>>
 		extends MappedAccumulator<G>
 	{
