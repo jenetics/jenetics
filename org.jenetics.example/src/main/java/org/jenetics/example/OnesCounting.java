8e138c3083b8382bca1d4f38e69c891a087fbb2d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-02-26 18:59:22 +0100

    Update One's counting example.

--- org.jenetics.example/src/main/java/org/jenetics/example/OnesCounting.java
+++ org.jenetics.example/src/main/java/org/jenetics/example/OnesCounting.java
@@ -21,7 +21,6 @@ package org.jenetics.example;
 
 import org.jenetics.BitChromosome;
 import org.jenetics.BitGene;
-import org.jenetics.Chromosome;
 import org.jenetics.GeneticAlgorithm;
 import org.jenetics.Genotype;
 import org.jenetics.Mutator;
@@ -36,21 +35,8 @@ final class OneCounter
 	implements Function<Genotype<BitGene>, Integer>
 {
 	@Override
-	public Integer apply(Genotype<BitGene> genotype) {
-		final Chromosome<BitGene> chromosome = genotype.getChromosome();
-
-		int count = 0;
-		if (chromosome instanceof BitChromosome) {
-			count = ((BitChromosome)chromosome).bitCount();
-		} else {
-			for (BitGene gene : genotype.getChromosome()) {
-				if (gene.getBit()) {
-					++count;
-				}
-			}
-		}
-
-		return count;
+	public Integer apply(final Genotype<BitGene> genotype) {
+		return ((BitChromosome)genotype.getChromosome()).bitCount();
 	}
 }
 
