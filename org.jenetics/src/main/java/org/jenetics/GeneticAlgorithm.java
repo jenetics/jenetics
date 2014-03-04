7bff82549af316b49c6a2901c694961c2a6af704
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-03-04 22:47:23 +0100

    Interface streamlining of 'Phenotype' class.
    --HG--
    branch : r1.6.0

--- org.jenetics/src/main/java/org/jenetics/GeneticAlgorithm.java
+++ org.jenetics/src/main/java/org/jenetics/GeneticAlgorithm.java
@@ -163,7 +163,7 @@ public class GeneticAlgorithm<
 
 	private final Factory<Genotype<G>> _genotypeFactory;
 	private final Factory<Phenotype<G, C>> _phenotypeFactory;
-	private final Function<Genotype<G>, ? extends C> _fitnessFunction;
+	private final Function<? super Genotype<G>, ? extends C> _fitnessFunction;
 	private Function<? super C, ? extends C> _fitnessScaler;
 
 	private double _offspringFraction = DEFAULT_OFFSPRING_FRACTION;
@@ -212,7 +212,7 @@ public class GeneticAlgorithm<
 	 */
 	public GeneticAlgorithm(
 		final Factory<Genotype<G>> genotypeFactory,
-		final Function<Genotype<G>, ? extends C> fitnessFunction,
+		final Function<? super Genotype<G>, ? extends C> fitnessFunction,
 		final Function<? super C, ? extends C> fitnessScaler,
 		final Optimize optimization
 	) {
