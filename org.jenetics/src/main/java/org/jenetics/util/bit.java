73eb7089367fc6aecc0f32106579bd2595ffa7a5
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-03-06 23:48:31 +0100

    Code improvements.
    --HG--
    branch : r1.6.0

--- org.jenetics/src/main/java/org/jenetics/util/bit.java
+++ org.jenetics/src/main/java/org/jenetics/util/bit.java
@@ -176,10 +176,29 @@ public final class bit extends StaticObject {
 		final byte[] data, final int start, final int end,
 		final byte[] otherData, final int otherStart
 	) {
+		int index = 0;
+		int otherIndex = 0;
+		boolean temp = false;
+
 		for (int i = (end - start); --i >= 0;) {
-			final boolean temp = get(data, i + start);
-			set(data, i + start, get(otherData, otherStart + i));
-			set(otherData, otherStart + i, temp);
+			index = i + start;
+			otherIndex = i + otherStart;
+
+			temp = (data[index >>> 3] & (1 << (index & 7))) != 0;
+			if ((otherData[otherIndex >>> 3] & (1 << (otherIndex & 7))) != 0) {
+				data[index >>> 3] |= 1 << (index & 7);
+			} else {
+				data[index >>> 3] &= ~(1 << (index & 7));
+			}
+			if (temp) {
+				otherData[otherIndex >>> 3] |= 1 << (otherIndex & 7);
+			} else {
+				otherData[otherIndex >>> 3] &= ~(1 << (otherIndex & 7));
+			}
+
+			//final boolean temp = get(data, i + start);
+			//set(data, i + start, get(otherData, otherStart + i));
+			//set(otherData, otherStart + i, temp);
 		}
 	}
 
