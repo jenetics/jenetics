54d3063999e8142bbc98e7e5bc495b220883e7fe
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-03-06 23:48:31 +0100

    Code improvements.

--- org.jenetics/src/main/java/org/jenetics/util/bit.java
+++ org.jenetics/src/main/java/org/jenetics/util/bit.java
@@ -38,7 +38,7 @@ import org.jscience.mathematics.number.LargeInteger;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.5 &mdash; <em>$Date: 2014-02-23 $</em>
+ * @version 1.5 &mdash; <em>$Date: 2014-03-06 $</em>
  */
 public final class bit extends StaticObject {
 	private bit() {}
@@ -176,10 +176,29 @@ public final class bit extends StaticObject {
 		final byte[] data, final int start, final int end,
 		final byte[] otherData, final int otherStart
 	) {
+		int index = 0;
+		int otherIndex = 0;
+		boolean temp = false;
+
 		for (int i = (end - start); --i >= 0;) {
-			final boolean temp = get(data, i + start);
-			set(data, i + start, get(otherData, otherStart + i));
-			set(otherData, otherStart + i, temp);
+			index = i + start;
+			otherIndex = i + otherStart;
+
+			temp = (data[index >>> 3] & (1 << (index & 7))) != 0;
+			if ((otherData[otherIndex >>> 3] & (1 << (otherIndex & 7))) != 0) {
+				data[index >>> 3] |= 1 << (index & 7);
+			} else {
+				data[index >>> 3] &= ~(1 << (index & 7));
+			}
+			if (temp) {
+				otherData[otherIndex >>> 3] |= 1 << (otherIndex & 7);
+			} else {
+				otherData[otherIndex >>> 3] &= ~(1 << (otherIndex & 7));
+			}
+
+			//final boolean temp = get(data, i + start);
+			//set(data, i + start, get(otherData, otherStart + i));
+			//set(otherData, otherStart + i, temp);
 		}
 	}
 
