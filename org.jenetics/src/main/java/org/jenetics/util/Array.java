8fcc947690dcbf10eb4d0f3bbf2e36480515a2d5
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-03-12 22:14:46 +0100

    Remove dependency to Javolution classes.
    --HG--
    branch : r2.0.0

--- org.jenetics/src/main/java/org/jenetics/util/Array.java
+++ org.jenetics/src/main/java/org/jenetics/util/Array.java
@@ -28,13 +28,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
 import java.util.RandomAccess;
-
-import javolution.context.StackContext;
-import javolution.util.FastList;
 
 /**
  * Array class which wraps the the java build in array type T[]. Once the array
@@ -98,30 +96,23 @@ public final class Array<T>
 	 *         {@code null}.
 	 */
 	public Array<T> filter(final Function<? super T, Boolean> predicate) {
-		StackContext.enter();
-		try {
-			final FastList<T> filtered = FastList.newInstance();
-			for (int i = 0, n = length(); i < n; ++i) {
-				@SuppressWarnings("unchecked")
-				final T value = (T)_array.data[i + _start];
+		final LinkedList<T> filtered = new LinkedList<>();
+		for (int i = 0, n = length(); i < n; ++i) {
+			@SuppressWarnings("unchecked")
+			final T value = (T)_array.data[i + _start];
 
-				if (predicate.apply(value) == Boolean.TRUE) {
-					filtered.add(value);
-				}
+			if (predicate.apply(value) == Boolean.TRUE) {
+				filtered.add(value);
 			}
-
-			final Array<T> copy = new Array<>(filtered.size());
-			int index = 0;
-			for (FastList.Node<T> n = filtered.head(), end = filtered.tail();
-				(n = n.getNext()) != end;)
-			{
-				copy.set(index++, n.getValue());
-			}
-
-			return copy;
-		} finally {
-			StackContext.exit();
 		}
+
+		final Array<T> copy = new Array<>(filtered.size());
+		int index = 0;
+		for (T element : filtered) {
+			copy.set(index++, element);
+		}
+
+		return copy;
 	}
 
 	@Override
