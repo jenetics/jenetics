560088cbee016db5520166de797b6c95debbd066
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-03-16 18:24:26 +0100

    Improve 'Array.filter' method.
    --HG--
    branch : r2.0.0

--- org.jenetics/src/main/java/org/jenetics/util/Array.java
+++ org.jenetics/src/main/java/org/jenetics/util/Array.java
@@ -98,20 +98,20 @@ public final class Array<T>
 	 */
 	public Array<T> filter(final Function<? super T, Boolean> predicate) {
 		final Stack<T> filtered = new Stack<>();
-		int index = 0;
+		int length = 0;
 		for (int i = 0, n = length(); i < n; ++i) {
 			@SuppressWarnings("unchecked")
 			final T value = (T)_array.data[i + _start];
 
 			if (predicate.apply(value) == Boolean.TRUE) {
 				filtered.push(value);
-				++index;
+				++length;
 			}
 		}
 
-		final Array<T> copy = new Array<>(index);
-		for (T e = filtered.pop(); e != null; e = filtered.pop()) {
-			copy.set(--index, e);
+		final Array<T> copy = new Array<>(length);
+		for (int i = length; --i >= 0;) {
+			copy._array.data[i] = filtered.pop();
 		}
 
 		return copy;
