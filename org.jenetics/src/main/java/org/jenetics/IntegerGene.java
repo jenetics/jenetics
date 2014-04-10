06a5b4cbc950f10b83a3c4e85ee46c24cb0ba6e6
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-04-10 17:42:32 +0200

    Update version string to '2.0.0'.

--- org.jenetics/src/main/java/org/jenetics/IntegerGene.java
+++ org.jenetics/src/main/java/org/jenetics/IntegerGene.java
@@ -41,8 +41,8 @@ import org.jenetics.util.RandomRegistry;
  * NumericGene implementation which holds a 32 bit integer number.
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version @__version__@ &mdash; <em>$Date: 2014-03-24 $</em>
- * @since @__version__@
+ * @version 2.0 &mdash; <em>$Date: 2014-04-10 $</em>
+ * @since 2.0
  */
 @XmlJavaTypeAdapter(IntegerGene.Model.Adapter.class)
 public final class IntegerGene
