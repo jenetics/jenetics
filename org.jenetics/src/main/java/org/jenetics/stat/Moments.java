dbbe8063f0d3936a316de3a95754f8e9180ed015
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-04-30 10:48:25 +0200

    Fix parallel moments calculation.

--- org.jenetics/src/main/java/org/jenetics/stat/Moments.java
+++ org.jenetics/src/main/java/org/jenetics/stat/Moments.java
@@ -79,28 +79,26 @@ class Moments {
 		final double m2 = _m2.doubleValue();
 		final double m3 = _m3.doubleValue();
 		
-		final double d = b._m1.doubleValue() - _m1.doubleValue();
-		final double d2 = d*d;
-		final double d3 = d2*d;
-		final double d4 = d3*d;
-		
 		final double pn = _n;
 		final double n = _n + b._n;
 		final double n2 = n*n;
+		
+		final double d = b._m1.doubleValue() - _m1.doubleValue();
+		final double d2 = d*d;
 
 		_n += b._n;
 		
 		_m1.add(d*b._n/n);
-
+		
 		_m2.add(b._m2).add(d2*pn*b._n/n);
-
+		
 		_m3.add(b._m3)
-			.add(d3*(pn*b._n*(pn - b._n)/n2))
+			.add(d2*d*(pn*b._n*(pn - b._n)/n2))
 			.add(3.0*d*(pn*b._m2.doubleValue() - b._n*m2)/n);
-
+		
 		_m4.add(b._m4)
-			.add(d4*(pn*b._n*(pn*pn - pn*b._n + b._n*b._n)/(n2*n)))
-			.add(6.0*d*d*(pn*pn*b._m2.doubleValue() + b._n*b._n*m2)/n2)
+			.add(d2*d2*(pn*b._n*(pn*pn - pn*b._n + b._n*b._n)/(n2*n)))
+			.add(6.0*d2*(pn*pn*b._m2.doubleValue() + b._n*b._n*m2)/n2)
 			.add(4.0*d*(pn*b._m3.doubleValue() - b._n*m3)/n);
 	}
 
