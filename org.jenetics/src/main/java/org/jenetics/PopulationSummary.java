dc176a044dbd3a9f9d59a2451c775164e4ccd7fe
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-10 21:45:40 +0200

    Implement PopulationSummary/Statistics classes.

--- org.jenetics/src/main/java/org/jenetics/PopulationSummary.java
+++ org.jenetics/src/main/java/org/jenetics/PopulationSummary.java
@@ -19,12 +19,16 @@
  */
 package org.jenetics;
 
+import java.util.Objects;
+
 import org.jenetics.stat.IntSummary;
 
 /**
+ * Contains statistical values about a given population.
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
- * @version 3.0 &mdash; <em>$Date$</em>
+ * @version 3.0 &mdash; <em>$Date: 2014-07-10 $</em>
  */
 public final class PopulationSummary<
 	G extends Gene<?, G>,
@@ -36,6 +40,18 @@ public final class PopulationSummary<
 	private final Phenotype<G, C> _worst;
 	private final IntSummary _ageSummary;
 
+	/**
+	 * Create a new population summary object.
+	 *
+	 * @param count the number of phenotypes this summary was build from
+	 * @param best the best phenotype
+	 * @param worst the worst phenotype
+	 * @param ageSummary the summary of the phenotype ages
+	 * @param <G> the gene type
+	 * @param <C> the fitness value type
+	 * @throws java.lang.NullPointerException if one of the parameters is
+	 *         {@code null}.
+	 */
 	private PopulationSummary(
 		final int count,
 		final Phenotype<G, C> best,
@@ -43,25 +59,93 @@ public final class PopulationSummary<
 		final IntSummary ageSummary
 	) {
 		_count = count;
-		_best = best;
-		_worst = worst;
-		_ageSummary = ageSummary;
+		_best = Objects.requireNonNull(best);
+		_worst = Objects.requireNonNull(worst);
+		_ageSummary = Objects.requireNonNull(ageSummary);
 	}
 
+	/**
+	 * The phenotype count this summary was build from.
+	 *
+	 * @return the number of phenotypes this summary was build from
+	 */
 	public int getCount() {
 		return _count;
 	}
 
-	public Phenotype<G, C> getWorst() {
-		return _worst;
-	}
-
+	/**
+	 * The best phenotype.
+	 *
+	 * @return the best phenotype
+	 */
 	public Phenotype<G, C> getBest() {
 		return _best;
 	}
 
+	/**
+	 * The worst phenotype.
+	 *
+	 * @return the worst phenotype
+	 */
+	public Phenotype<G, C> getWorst() {
+		return _worst;
+	}
+
+	/**
+	 * The summary of the phenotype ages.
+	 *
+	 * @return the summary of the phenotype ages
+	 */
 	public IntSummary getAgeSummary() {
 		return _ageSummary;
+	}
+
+	/**
+	 * Create a new population summary object.
+	 *
+	 * @param count the number of phenotypes this summary was build from
+	 * @param best the best phenotype
+	 * @param worst the worst phenotype
+	 * @param ageSummary the summary of the phenotype ages
+	 * @param <G> the gene type
+	 * @param <C> the fitness value type
+	 * @return a new population summary object
+	 * @throws java.lang.NullPointerException if one of the parameters is
+	 *         {@code null}.
+	 */
+	public static <G extends Gene<?, G>, C extends Comparable<? super C>>
+	PopulationSummary<G, C> of(
+		final int count,
+		final Phenotype<G, C> best,
+		final Phenotype<G, C> worst,
+		final IntSummary ageSummary
+	) {
+		return new PopulationSummary<>(
+			count,
+			best,
+			worst,
+			ageSummary
+		);
+	}
+
+	/**
+	 * Return a new population summary object.
+	 *
+	 * @param statistics the creating (mutable) statistics class
+	 * @param <G> the gene type
+	 * @param <C> the fitness value type
+	 * @return a new population summary object
+	 * @throws java.lang.NullPointerException if one of the parameters is
+	 *         {@code null}.
+	 */
+	public static <G extends Gene<?, G>, C extends Comparable<? super C>>
+	PopulationSummary<G, C> of(final PopulationSummaryStatistics<G, C> statistics) {
+		return of(
+			(int)statistics.getAgeSummary().getCount(),
+			statistics.getBest(),
+			statistics.getWorst(),
+			IntSummary.of(statistics.getAgeSummary())
+		);
 	}
 
 }
--- org.jenetics/src/main/java/org/jenetics/PopulationSummaryStatistics.java
+++ org.jenetics/src/main/java/org/jenetics/PopulationSummaryStatistics.java
@@ -19,10 +19,102 @@
  */
 package org.jenetics;
 
+import static java.util.Objects.requireNonNull;
+import static org.jenetics.internal.math.statistics.max;
+import static org.jenetics.internal.math.statistics.min;
+
+import java.util.IntSummaryStatistics;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collector;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
- * @version 3.0 &mdash; <em>$Date: 2014-06-04 $</em>
+ * @version 3.0 &mdash; <em>$Date: 2014-07-10 $</em>
  */
-public class PopulationSummaryStatistics {
+public class PopulationSummaryStatistics<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+	implements Consumer<Phenotype<G, C>>
+{
+
+	private final Optimize _optimize;
+	private final int _currentGeneration;
+
+	private Phenotype<G, C> _best = null;
+	private Phenotype<G, C> _worst = null;
+	private IntSummaryStatistics _ageSummary = new IntSummaryStatistics();
+
+	public PopulationSummaryStatistics(
+		final Optimize optimize,
+		final int currentGeneration
+	) {
+		_optimize = requireNonNull(optimize);
+		_currentGeneration = currentGeneration;
+	}
+
+	@Override
+	public void accept(final Phenotype<G, C> phenotype) {
+		_best = max(_optimize::compare, _best, phenotype);
+		_worst = min(_optimize::compare, _worst, phenotype);
+		_ageSummary.accept(phenotype.getAge(_currentGeneration));
+	}
+
+	public void combine(final PopulationSummaryStatistics<G, C> other) {
+		_best = max(_optimize::compare, _best, other._best);
+		_worst = min(_optimize::compare, _worst, other._worst);
+		_ageSummary.combine(other._ageSummary);
+	}
+
+	public Optimize getOptimize() {
+		return _optimize;
+	}
+
+	public int getGeneration() {
+		return _currentGeneration;
+	}
+
+	/**
+	 * The best phenotype.
+	 *
+	 * @return the best phenotype
+	 */
+	public Phenotype<G, C> getBest() {
+		return _best;
+	}
+
+	/**
+	 * The worst phenotype.
+	 *
+	 * @return the worst phenotype
+	 */
+	public Phenotype<G, C> getWorst() {
+		return _worst;
+	}
+
+	/**
+	 * The summary of the phenotype ages.
+	 *
+	 * @return the summary of the phenotype ages
+	 */
+	public IntSummaryStatistics getAgeSummary() {
+		return _ageSummary;
+	}
+
+	public static <T, G extends Gene<?, G>, C extends Comparable<? super C>>
+	Collector<T, ?, PopulationSummaryStatistics<G, C>> collector(
+		final Optimize optimize,
+		final int currentGeneration,
+		final Function<T, Phenotype<G, C>> mapper
+	) {
+		requireNonNull(mapper);
+		return Collector.of(
+			() -> new PopulationSummaryStatistics<>(optimize, currentGeneration),
+			(r, t) -> r.accept(mapper.apply(t)),
+			(a, b) -> {a.combine(b); return a;}
+		);
+	}
+
 }
