753c704588e2070a0eb3e7eedb70cc6f74becaa8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-22 23:11:24 +0200

    'De-optimize' modulus methods.

--- org.jenetix/src/main/java/org/jenetix/random/modulus.java
+++ org.jenetix/src/main/java/org/jenetix/random/modulus.java
@@ -19,45 +19,24 @@
  */
 package org.jenetix.random;
 
-import static java.lang.String.format;
-
 import org.jenetics.util.StaticObject;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since !__version__!
- * @version !__version__! &mdash; <em>$Date$</em>
+ * @version !__version__! &mdash; <em>$Date: 2014-07-22 $</em>
  */
 final class modulus extends StaticObject {
 	private modulus() {}
 
 	static final long VALUE = 0xFFFFFFFFL;
 
-	private static final long MASK = (1L << 32) - 1L;
-
-
 	static long mod(final long a) {
 		return a%VALUE;
 	}
 
-
-//	static long mod(final long x) {
-//		return x >= 0 ? modp(x) : -modp(-x);
-//	}
-
-	private static long modp(final long x) {
-		return x%VALUE;
-//		assert(x >= 0): format("%d not positive.", x);
-//
-//		long k = x;
-//		while (k > MASK) k = (k&MASK) + (k >>> 32);
-//		return k == MASK ? 0 : k;
-	}
-
 	static long add(final long a,final long b) {
-		final long r = a + b;
-		if (((a^r) & (b^r)) < 0) return modp(modp(a) + modp(b));
-		else return modp(r);
+		return mod(mod(a) + mod(b));
 	}
 
 	static long add(final long a,final long b, final long c) {
@@ -65,11 +44,11 @@ final class modulus extends StaticObject {
 	}
 
 	static long add(final long a,final long b, final long c, final long d) {
-		return modp(add(a, b) + add(c, d));
+		return mod(add(a, b) + add(c, d));
 	}
 
 	static long add(final long a,final long b, final long c, final long d, final long e) {
-		return modp(add(a, b, c, d) + modp(e));
+		return mod(add(a, b, c, d) + mod(e));
 	}
 
 }
