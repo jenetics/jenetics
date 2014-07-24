baec9e5ca83924aace5a79d5e1c48597a1a1602b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-25 00:04:05 +0200

    Add summary to DieHarder output.

--- org.jenetics/src/main/java/org/jenetics/internal/util/DieHarder.java
+++ org.jenetics/src/main/java/org/jenetics/internal/util/DieHarder.java
@@ -20,6 +20,8 @@
 package org.jenetics.internal.util;
 
 import static java.lang.String.format;
+import static java.util.regex.Pattern.quote;
+import static org.jenetics.internal.util.Equality.eq;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,8 +32,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.Random;
 
+import org.jenetics.internal.util.DieHarder.Result.Assessment;
 
 /**
  * Class for testing a given random engine using the
@@ -40,7 +46,7 @@ import java.util.Random;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.5
- * @version 1.5 &mdash; <em>$Date: 2014-02-15 $</em>
+ * @version !__version__! &mdash; <em>$Date: 2014-07-25 $</em>
  */
 public final class DieHarder {
 
@@ -49,7 +55,7 @@ public final class DieHarder {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.5
-	 * @version 1.5 &mdash; <em>$Date: 2014-02-15 $</em>
+	 * @version !__version__! &mdash; <em>$Date: 2014-07-25 $</em>
 	 */
 	private static final class Randomizer implements Runnable {
 		private final Random _random;
@@ -114,11 +120,16 @@ public final class DieHarder {
 		));
 		randomizer.start();
 
+		// The dieharder console output.
 		final BufferedReader stdout = new BufferedReader (
 			new InputStreamReader(dieharder.getInputStream())
 		);
+
+		final List<Result> results = new ArrayList<>();
+
 		String line = null;
 		while ((line = stdout.readLine()) != null) {
+			Result.parse(line).ifPresent(results::add);
 			System.out.println(line);
 		}
 
@@ -126,6 +137,19 @@ public final class DieHarder {
 		randomizer.interrupt();
 		final long sec = (System.currentTimeMillis() - start)/1000;
 
+		// Calculate statistics.
+		final long passed = results.stream()
+			.filter(r -> r.assessment == Assessment.PASSED)
+			.count();
+		final long weak = results.stream()
+			.filter(r -> r.assessment == Assessment.WEAK)
+			.count();
+		final long failed = results.stream()
+			.filter(r -> r.assessment == Assessment.FAILED)
+			.count();
+
+
+		printt("Summary: PASSED=%d, WEAK=%d, FAILED=%d", passed, weak, failed);
 		printt("Runtime: %d:%02d:%02d", sec/3600, (sec%3600)/60, (sec%60));
 
 	}
@@ -163,6 +187,135 @@ public final class DieHarder {
 
 	private static void println(final String pattern, final Object... args) {
 		System.out.println(format(pattern, args));
+	}
+
+
+
+	/**
+	 * Represents one DieHarder test result.
+	 *
+	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+	 * @since !__version__!
+	 * @version !__version__! &mdash; <em>$Date: 2014-07-25 $</em>
+	 */
+	static final class Result {
+
+		static enum Assessment {
+			PASSED,
+			FAILED,
+			WEAK;
+
+			static Optional<Assessment> of(final String assessment) {
+				switch (assessment) {
+					case "PASSED": return Optional.of(PASSED);
+					case "FAILED": return Optional.of(FAILED);
+					case "WEAK": return Optional.of(WEAK);
+					default: return Optional.empty();
+				}
+			}
+		}
+
+		final String testName;
+		final int ntup;
+		final int tsamples;
+		final int psamples;
+		final double pvalue;
+		final Assessment assessment;
+
+		private Result(
+			final String testName,
+			final int ntup,
+			final int tsamples,
+			final int psamples,
+			final double pvalue,
+			final Assessment assessment
+		) {
+			this.testName = testName;
+			this.ntup = ntup;
+			this.tsamples = tsamples;
+			this.psamples = psamples;
+			this.pvalue = pvalue;
+			this.assessment = assessment;
+		}
+
+		static Optional<Result> parse(final String line) {
+			final String[] parts = line.split(quote("|"));
+
+			if (parts.length == 6) {
+				final String name = parts[0].trim();
+				final OptionalInt ntup = parseInt(parts[1].trim());
+				final OptionalInt tsamples = parseInt(parts[2].trim());
+				final OptionalInt psamples = parseInt(parts[3].trim());
+				final OptionalDouble pvalue = parseDouble(parts[4].trim());
+				final Optional<Assessment> assessment = Assessment.of(parts[5].trim());
+
+				if (ntup.isPresent() &&
+					tsamples.isPresent() &&
+					psamples.isPresent() &&
+					pvalue.isPresent() &&
+					assessment.isPresent())
+				{
+					return Optional.of(new Result(
+						name,
+						ntup.getAsInt(),
+						tsamples.getAsInt(),
+						psamples.getAsInt(),
+						pvalue.getAsDouble(),
+						assessment.get()
+					));
+				}
+			}
+
+			return Optional.empty();
+		}
+
+		private static OptionalInt parseInt(final String value) {
+			try {
+				return OptionalInt.of(Integer.parseInt(value));
+			} catch (NumberFormatException e) {
+				return OptionalInt.empty();
+			}
+		}
+
+		private static OptionalDouble parseDouble(final String value) {
+			try {
+				return OptionalDouble.of(Double.parseDouble(value));
+			} catch (NumberFormatException e) {
+				return OptionalDouble.empty();
+			}
+		}
+
+		@Override
+		public int hashCode() {
+			return Hash.of(getClass())
+				.and(testName)
+				.and(ntup)
+				.and(tsamples)
+				.and(psamples)
+				.and(pvalue)
+				.and(assessment).value();
+		}
+
+		@Override
+		public boolean equals(final Object obj) {
+			return Equality.of(this, obj).test(result ->
+				eq(testName, result.testName) &&
+				eq(ntup, result.ntup) &&
+				eq(tsamples, result.tsamples) &&
+				eq(psamples, result.psamples) &&
+				eq(pvalue, result.psamples) &&
+				eq(assessment, result.assessment)
+			);
+		}
+
+		@Override
+		public String toString() {
+			return format(
+				"%s[ntup=%d, tsamples=%d, psamples=%d, pvalue=%f, assessment=%s]",
+				testName, ntup, tsamples, psamples, pvalue, assessment
+			);
+		}
+
 	}
 
 }
