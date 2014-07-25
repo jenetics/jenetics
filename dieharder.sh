895726dafe1c9963cbf483125389a642afc75880
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-25 14:54:15 +0200

    Extend dieharder.sh script.

--- dieharder.sh
+++ dieharder.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.KISSRandom -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.LCG64Random -a
 java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MRG2Random -a
 java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MRG3Random -a
 java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MRG4Random -a
 java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MRG5Random -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MT19937_32Random -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.MT19937_64Random -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.XOR64ShiftRandom -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.XORShiftRandom -a
+java -cp org.jenetix/build/libs/org.jenetix-jarjar-3.0.0-SNAPSHOT.jar org.jenetics.internal.util.DieHarder org.jenetix.random.Yarn2Random -a
