af162bde0233706010ee6ecdac8854ea99f321b0
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-29 23:07:16 +0200

    Implement 'SelectStage' class.

--- org.jenetics/src/main/java/org/jenetics/internal/engine/SelectStage.java
+++ org.jenetics/src/main/java/org/jenetics/internal/engine/SelectStage.java
@@ -19,10 +19,62 @@
  */
 package org.jenetics.internal.engine;
 
+import static org.jenetics.internal.util.NanoClock.timing;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.jenetics.Gene;
+import org.jenetics.Optimize;
+import org.jenetics.Population;
+import org.jenetics.Selector;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
  * @version 3.0 &mdash; <em>$Date: 2014-07-29 $</em>
  */
-public class SelectStage {
+public class SelectStage<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+{
+
+	public int survivors;
+	public int offspring;
+	private Selector<G, C> survivorSelector;
+	private Selector<G, C> offspringSelector;
+	public Optimize optimize;
+	public Executor executor;
+
+	public final AtomicLong time = new AtomicLong(0);
+
+	public Result select(final Population<G, C> population) {
+		return new Result(
+			CompletableFuture.supplyAsync(timing(time, () ->
+				survivorSelector.select(population, survivors, optimize)),
+				executor
+			),
+			CompletableFuture.supplyAsync(timing(time, () ->
+				offspringSelector.select(population, offspring, optimize)),
+				executor
+			)
+		);
+	}
+
+	public final class Result {
+		public final CompletionStage<Population<G, C>> survivors;
+		public final CompletionStage<Population<G, C>> offspring;
+
+		private Result(
+			final CompletionStage<Population<G, C>> survivors,
+			final CompletionStage<Population<G, C>> offspring
+		) {
+			this.survivors = survivors;
+			this.offspring = offspring;
+		}
+	}
+
 }
