e94812eabccaeb7443064313f5e756d929eeb3ae
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-07-30 23:41:32 +0200

    Move 'GA' class into 'org.jenetics.internal.engine' package. This is an test implementation class.

--- org.jenetics/src/main/java/org/jenetics/internal/engine/GA.java
+++ org.jenetics/src/main/java/org/jenetics/internal/engine/GA.java
@@ -17,12 +17,15 @@
  * Author:
  *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
  */
-package org.jenetics;
+package org.jenetics.internal.engine;
+
+import org.jenetics.Gene;
+import org.jenetics.Population;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
- * @version 3.0 &mdash; <em>$Date: 2014-06-25 $</em>
+ * @version 3.0 &mdash; <em>$Date$</em>
  */
 public class GA<
 	G extends Gene<?, G>,
