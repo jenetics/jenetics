2bd619b028b164885587e5ad33b62b899f6c641c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-08-01 22:28:46 +0200

    Implement 'SelectStage' class.

--- org.jenetics/src/main/java/org/jenetics/internal/engine/SelectStage.java
+++ org.jenetics/src/main/java/org/jenetics/internal/engine/SelectStage.java
@@ -20,16 +20,24 @@
 package org.jenetics.internal.engine;
 
 import static java.lang.Math.round;
+import static java.util.Objects.requireNonNull;
 
 import java.util.concurrent.CompletionStage;
+
+import org.jenetics.internal.util.Timer;
 
 import org.jenetics.Gene;
 import org.jenetics.Population;
 
 /**
+ * This <i>stage</i> selects the survivor and offspring population.
+ *
+ * @param <G> the gene type
+ * @param <C> the fitness type
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
- * @version 3.0 &mdash; <em>$Date: 2014-07-30 $</em>
+ * @version 3.0 &mdash; <em>$Date: 2014-08-01 $</em>
  */
 public class SelectStage<
 	G extends Gene<?, G>,
@@ -45,17 +53,22 @@ public class SelectStage<
 		_context = context;
 	}
 
-	public Result select(final Population<G, C> population) {
-		return new Result() {{
-			survivors = async(timing(() ->
-				_context.getSurvivorSelector()
-					.select(population, getSurvivorCount(), _context.getOptimize()))
-			);
-			offspring = async(timing(() ->
-				_context.getOffspringSelector()
-					.select(population, getOffspringCount(), _context.getOptimize()))
-			);
-		}};
+	public Result<G, C> select(final Population<G, C> population) {
+		final Timer timer = Timer.of(_context.getClock());
+
+		final CompletionStage<Population<G, C>> survivors = async(timer.timing(() ->
+				_context.getSurvivorSelector().select(
+					population, getSurvivorCount(), _context.getOptimize())
+			)
+		);
+
+		final CompletionStage<Population<G, C>> offspring = async(timer.timing(() ->
+			_context.getOffspringSelector().select(
+				population, getOffspringCount(), _context.getOptimize())
+			)
+		);
+
+		return new Result<>(timer, survivors, offspring);
 	}
 
 	private int getSurvivorCount() {
@@ -68,9 +81,39 @@ public class SelectStage<
 		);
 	}
 
-	public abstract class Result extends StageResult {
-		public CompletionStage<Population<G, C>> survivors;
-		public CompletionStage<Population<G, C>> offspring;
+	/**
+	 * Contains the <i>asynchronous</i> result of the selection stage.
+	 *
+	 * @param <G> the gene type
+	 * @param <C> the fitness type
+	 */
+	public static final class Result<
+		G extends Gene<?, G>,
+		C extends Comparable<? super C>
+	>
+		extends StageResult
+	{
+		private final CompletionStage<Population<G, C>> _survivors;
+		private final CompletionStage<Population<G, C>> _offspring;
+
+		private Result(
+			final Timer timer,
+			final CompletionStage<Population<G, C>> survivors,
+			final  CompletionStage<Population<G, C>> offspring
+		) {
+			super(timer);
+			_survivors = requireNonNull(survivors);
+			_offspring = requireNonNull(offspring);
+		}
+
+		public CompletionStage<Population<G, C>> getSurvivors() {
+			return _survivors;
+		}
+
+		public CompletionStage<Population<G, C>> getOffspring() {
+			return _offspring;
+		}
+
 	}
 
 }
