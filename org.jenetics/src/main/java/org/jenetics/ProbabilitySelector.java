e8128cd3bcd517e1eedff284807b6f88643a07f3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-08-10 11:30:07 +0200

    Improve probability array sorting.
    --HG--
    branch : r2.0.1

--- org.jenetics/src/main/java/org/jenetics/ProbabilitySelector.java
+++ org.jenetics/src/main/java/org/jenetics/ProbabilitySelector.java
@@ -151,7 +151,7 @@ public abstract class ProbabilitySelector<
 	static double[] revert(final double[] probabilities) {
 		final int N = probabilities.length;
 		final int[] indexes = sort(probabilities);
-		final double[] result = probabilities.clone();
+		final double[] result = new double[N];
 
 		for (int i = 0; i < N; ++i) {
 			result[indexes[N - i - 1]] = probabilities[indexes[i]];
