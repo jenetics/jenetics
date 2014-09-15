106dfd56c66bdd38bdd137dd9f2e16fc33798ae4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-09-15 20:04:09 +0200

    Move 'EngineBuilder' into 'EvolutionEngine'.

--- org.jenetics/src/main/java/org/jenetics/EvolutionEngine.java
+++ org.jenetics/src/main/java/org/jenetics/EvolutionEngine.java
@@ -22,17 +22,16 @@ package org.jenetics;
 import static java.lang.Math.round;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
-import static org.jenetics.internal.util.require.probability;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.jenetics.internal.engine.AlterResult;
+import org.jenetics.internal.engine.EngineBuilder;
 import org.jenetics.internal.engine.EvolutionDurations;
 import org.jenetics.internal.engine.EvolutionResult;
 import org.jenetics.internal.engine.EvolutionStart;
@@ -451,11 +450,11 @@ public final class EvolutionEngine<
 	 * @return a new engine builder
 	 */
 	public static <G extends Gene<?, G>, C extends Comparable<? super C>>
-	Builder<G, C> newBuilder(
+	EngineBuilder<G, C> newBuilder(
 		final Function<? super Genotype<G>, ? extends C> fitnessFunction,
 		final Factory<Genotype<G>> genotypeFactory
 	) {
-		return new Builder<>(genotypeFactory, fitnessFunction);
+		return new EngineBuilder<>(genotypeFactory, fitnessFunction);
 	}
 
 	/**
@@ -470,154 +469,12 @@ public final class EvolutionEngine<
 	 */
 	@SafeVarargs
 	public static <G extends Gene<?, G>, C extends Comparable<? super C>>
-	Builder<G, C> newBuilder(
+	EngineBuilder<G, C> newBuilder(
 		final Function<? super Genotype<G>, ? extends C> fitnessFunction,
 		final Chromosome<G>... chromosomes
 	) {
-		return new Builder<>(Genotype.of(chromosomes), fitnessFunction);
+		return new EngineBuilder<>(Genotype.of(chromosomes), fitnessFunction);
 	}
 
-
-
-
-	/**
-	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
-	 * @since 3.0
-	 * @version 3.0 &mdash; <em>$Date: 2014-09-15 $</em>
-	 */
-	public static final class Builder<
-		G extends Gene<?, G>,
-		C extends Comparable<? super C>
-		>
-	{
-
-		// No default values for this properties.
-		private Function<? super Genotype<G>, ? extends C> _fitnessFunction;
-		private Factory<Genotype<G>> _genotypeFactory;
-
-		// This are the properties which default values.
-		private Function<? super C, ? extends C> _fitnessScaler = Function.identity();
-		private Selector<G, C> _survivorsSelector = new TournamentSelector<>(3);
-		private Selector<G, C> _offspringSelector = new TournamentSelector<>(3);
-		private Alterer<G, C> _alterer = Alterer.of(
-			new SinglePointCrossover<G, C>(0.2),
-			new Mutator<G, C>(0.15)
-		);
-		private Optimize _optimize = Optimize.MAXIMUM;
-		private double _offspringFraction = 0.6;
-		private int _populationSize = 50;
-		private int _maximalPhenotypeAge = 70;
-
-		private Executor _executor = ForkJoinPool.commonPool();
-
-		private Builder(
-			final Factory<Genotype<G>> genotypeFactory,
-			final Function<? super Genotype<G>, ? extends C> fitnessFunction
-		) {
-			_genotypeFactory = requireNonNull(genotypeFactory);
-			_fitnessFunction = requireNonNull(fitnessFunction);
-		}
-
-		public Builder<G, C> fitnessFunction(
-			Function<? super Genotype<G>, ? extends C> function
-		) {
-			_fitnessFunction = requireNonNull(function);
-			return this;
-		}
-
-		public Builder<G, C> fitnessScaler(
-			final Function<? super C, ? extends C> scaler
-		) {
-			_fitnessScaler = requireNonNull(scaler);
-			return this;
-		}
-
-		public Builder<G, C> genotypeFactory(
-			final Factory<Genotype<G>> genotypeFactory
-		) {
-			_genotypeFactory = requireNonNull(genotypeFactory);
-			return this;
-		}
-
-		public Builder<G, C> offspringSelector(
-			final Selector<G, C> selector
-		) {
-			_offspringSelector = requireNonNull(selector);
-			return this;
-		}
-
-		public Builder<G, C> survivorsSelector(
-			final Selector<G, C> selector
-		) {
-			_survivorsSelector = requireNonNull(selector);
-			return this;
-		}
-
-		@SafeVarargs
-		public final Builder<G, C> alterers(final Alterer<G, C>... alterers) {
-			_alterer = Alterer.of(alterers);
-			return this;
-		}
-
-		public Builder<G, C> optimize(final Optimize optimize) {
-			_optimize = requireNonNull(optimize);
-			return this;
-		}
-
-		public Builder<G, C> offspringFraction(final double fraction) {
-			_offspringFraction = probability(fraction);
-			return this;
-		}
-
-		public Builder<G, C> populationSize(final int size) {
-			if (size < 1) {
-				throw new IllegalArgumentException(format(
-					"Population size must be greater than zero, but was %s.", size
-				));
-			}
-			_populationSize = size;
-			return this;
-		}
-
-		public Builder<G, C> maximalPhenotypeAge(final int age) {
-			if (age < 1) {
-				throw new IllegalArgumentException(format(
-					"Phenotype age must be greater than one, but was %s.", age
-				));
-			}
-			_maximalPhenotypeAge = age;
-			return this;
-		}
-
-		public Builder<G, C> executor(final Executor executor) {
-			_executor = requireNonNull(executor);
-			return this;
-		}
-
-		public EvolutionEngine<G, C> build() {
-			return new EvolutionEngine<>(
-				_fitnessFunction,
-				_fitnessScaler,
-				_genotypeFactory,
-				_survivorsSelector,
-				_offspringSelector,
-				_alterer,
-				_optimize,
-				getOffspringCount(),
-				getSurvivorsCount(),
-				_maximalPhenotypeAge,
-				_executor
-			);
-		}
-
-		private int getSurvivorsCount() {
-			return _populationSize - getOffspringCount();
-		}
-
-		private int getOffspringCount() {
-			return (int)round(_offspringFraction*_populationSize);
-		}
-
-	}
 
 }
