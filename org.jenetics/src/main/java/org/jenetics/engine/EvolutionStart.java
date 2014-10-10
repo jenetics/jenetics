9a6f5d8358d016d9a8f44bb9f14eace7f36e0d5b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-10-11 00:07:29 +0200

    Simplify class.

--- org.jenetics/src/main/java/org/jenetics/engine/EvolutionStart.java
+++ org.jenetics/src/main/java/org/jenetics/engine/EvolutionStart.java
@@ -20,12 +20,6 @@
 package org.jenetics.engine;
 
 import static java.util.Objects.requireNonNull;
-import static org.jenetics.internal.util.Equality.eq;
-
-import java.io.Serializable;
-
-import org.jenetics.internal.util.Equality;
-import org.jenetics.internal.util.Hash;
 
 import org.jenetics.Gene;
 import org.jenetics.Population;
@@ -38,76 +32,22 @@ import org.jenetics.Population;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 3.0
- * @version 3.0 &mdash; <em>$Date: 2014-09-15 $</em>
+ * @version 3.0 &mdash; <em>$Date: 2014-10-11 $</em>
  */
 final class EvolutionStart<
 	G extends Gene<?, G>,
 	C extends Comparable<? super C>
->
-	implements Serializable
-{
-	private static final long serialVersionUID = 1L;
+> {
 
-	private final Population<G, C> _population;
-	private final int _generation;
+	final Population<G, C> population;
+	final int generation;
 
-	private EvolutionStart(
+	EvolutionStart(
 		final Population<G, C> population,
 		final int generation
 	) {
-		_population = requireNonNull(population);
-		_generation = generation;
+		this.population = requireNonNull(population);
+		this.generation = generation;
 	}
 
-	/**
-	 * Return the start population.
-	 *
-	 * @return the start population
-	 */
-	public Population<G, C> getPopulation() {
-		return _population;
-	}
-
-	/**
-	 * Return the current generation.
-	 *
-	 * @return the current generation
-	 */
-	public int getGeneration() {
-		return _generation;
-	}
-
-	@Override
-	public int hashCode() {
-		return Hash.of(getClass())
-			.and(_population)
-			.and(_generation).value();
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		return Equality.of(this, obj).test(start ->
-			eq(_population, start._population) &&
-			eq(_generation, start._generation)
-		);
-	}
-
-	/**
-	 * Create an new {@code EvolutionStart} object from the given values.
-	 *
-	 * @param population the start population
-	 * @param generation the current generation
-	 * @param <G> the gene type
-	 * @param <C> the fitness type
-	 * @return a new evolution start object
-	 * @throws java.lang.NullPointerException if the give {@code population} is
-	 *         {@code null}
-	 */
-	static <G extends Gene<?, G>, C extends Comparable<? super C>>
-	EvolutionStart<G, C> of(
-		final Population<G, C> population,
-		final int generation
-	) {
-		return new EvolutionStart<>(population, generation);
-	}
 }
