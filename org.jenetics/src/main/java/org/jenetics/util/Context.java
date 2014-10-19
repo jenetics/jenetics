c484761711e1d3e2d2106d409f89b06fcaced3a0
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-10-19 23:12:22 +0200

    Remove 'Scoped' interface.

--- org.jenetics/src/main/java/org/jenetics/util/Context.java
+++ org.jenetics/src/main/java/org/jenetics/util/Context.java
@@ -21,14 +21,11 @@ package org.jenetics.util;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
-import java.util.function.Supplier;
-
-import org.jenetics.util.Scoped;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version 2.0 &mdash; <em>$Date$</em>
- * @since 2.0
+ * @version 2.0 &mdash; <em>$Date: 2014-10-19 $</em>
+ * @since 3.0
  */
 final class Context<T> {
 
@@ -36,26 +33,26 @@ final class Context<T> {
 	private final AtomicReference<Entry<T>> _entry;
 	private final ThreadLocal<Entry<T>> _threadLocalEntry = new ThreadLocal<>();
 
-	public Context(final T defaultValue) {
+	Context(final T defaultValue) {
 		_default = defaultValue;
 		_entry = new AtomicReference<>(new Entry<>(defaultValue));
 	}
 
-	public void set(final T value) {
+	void set(final T value) {
 		final Entry<T> e = _threadLocalEntry.get();
 		if (e != null) e.value = value; else _entry.set(new Entry<T>(value));
 	}
 
-	public T get() {
+	T get() {
 		final Entry<T> e = _threadLocalEntry.get();
 		return (e != null ? e : _entry.get()).value;
 	}
 
-	public void reset() {
+	void reset() {
 		set(_default);
 	}
 
-	public <S extends T, R> R with(final S value, final Function<S, R> f) {
+	<S extends T, R> R with(final S value, final Function<S, R> f) {
 		final Entry<T> e = _threadLocalEntry.get();
 		if (e != null) {
 			_threadLocalEntry.set(e.inner(value));
@@ -70,35 +67,9 @@ final class Context<T> {
 		}
 	}
 
-//	public <S extends T> Scoped<S> scope(final S value) {
-//		final Entry<T> e = _threadLocalEntry.get();
-//		if (e != null) {
-//			_threadLocalEntry.set(e.inner(value));
-//		} else {
-//			_threadLocalEntry.set(new Entry<T>(value, Thread.currentThread()));
-//		}
-//
-//		return new Scope<S, T>(value, _threadLocalEntry);
-//	}
-
-//	public <S> Scoped<S> scope(final T value, final Supplier<? extends S> supplier) {
-//		final Scoped<T> scoped = scope(value);
-//		return new Scoped<S>() {
-//			@Override
-//			public S get() {
-//				return supplier.get();
-//			}
-//
-//			@Override
-//			public void close() {
-//				scoped.close();
-//			}
-//		};
-//	}
-
 	/**
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
-	 * @version 2.0 &mdash; <em>$Date$</em>
+	 * @version 2.0 &mdash; <em>$Date: 2014-10-19 $</em>
 	 * @since 2.0
 	 */
 	private static final class Entry<T> {
@@ -128,43 +99,4 @@ final class Context<T> {
 
 	}
 
-	/**
-	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
-	 * @version 2.0 &mdash; <em>$Date$</em>
-	 * @since 2.0
-	 */
-	private static final class Scope<A, B> implements Scoped<A> {
-
-		private final A _value;
-		private final ThreadLocal<Entry<B>> _threadLocalEntry;
-
-		Scope(final A value, final ThreadLocal<Entry<B>> threadLocalEntry) {
-			_value = value;
-			_threadLocalEntry = threadLocalEntry;
-		}
-
-		@Override
-		public A get() {
-			return _value;
-		}
-
-		@Override
-		public void close() {
-			final Entry<B> e = _threadLocalEntry.get();
-			if (e != null) {
-				if (e.thread != Thread.currentThread()) {
-					throw new IllegalStateException(
-						"Value context must be closed by the creating thread."
-					);
-				}
-
-				_threadLocalEntry.set(e.parent);
-			} else {
-				throw new IllegalStateException(
-					"Value context has been already close."
-				);
-			}
-		}
-
-	}
 }
