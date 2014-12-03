dfead70df8119ffb3950c11226e6712e2d5c76fe
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-12-03 22:18:17 +0100

    Optimize 'Seq' factory methods.

--- org.jenetics/src/main/java/org/jenetics/util/MSeq.java
+++ org.jenetics/src/main/java/org/jenetics/util/MSeq.java
@@ -41,7 +41,7 @@ import org.jenetics.internal.collection.ObjectArrayProxy;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 3.0 &mdash; <em>$Date: 2014-10-20 $</em>
+ * @version 3.0 &mdash; <em>$Date: 2014-12-03 $</em>
  */
 public interface MSeq<T> extends Seq<T>, Copyable<MSeq<T>> {
 
@@ -290,10 +290,14 @@ public interface MSeq<T> extends Seq<T>, Copyable<MSeq<T>> {
 	 * @return a new {@code MSeq} with the given values.
 	 * @throws NullPointerException if the {@code values} array is {@code null}.
 	 */
+	@SuppressWarnings("unchecked")
 	public static <T> MSeq<T> of(final Iterable<? extends T> values) {
 		MSeq<T> mseq = null;
-		if (values instanceof Collection<?>) {
-			@SuppressWarnings("unchecked")
+		if (values instanceof ISeq<?>) {
+			mseq = ((ISeq<T>)values).copy();
+		} else if (values instanceof MSeq<?>) {
+			mseq = (MSeq<T>)values;
+		} else if (values instanceof Collection<?>) {
 			final Collection<T> collection = (Collection<T>)values;
 			mseq = MSeq.<T>ofLength(collection.size()).setAll(values);
 		} else {
