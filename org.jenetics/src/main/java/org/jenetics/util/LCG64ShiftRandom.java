ec9a285ff96c2b1af3894feb3f23cf33b9b30aa0
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-12-19 13:18:11 +0100

    Fix typos in javadoc.

--- org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
+++ org.jenetics/src/main/java/org/jenetics/util/LCG64ShiftRandom.java
@@ -75,7 +75,7 @@ import org.jenetics.internal.util.Hash;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.1
- * @version 2.0 &mdash; <em>$Date: 2014-12-08 $</em>
+ * @version 2.0 &mdash; <em>$Date: 2014-12-19 $</em>
  */
 public class LCG64ShiftRandom extends Random64 {
 
@@ -109,7 +109,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 3.0 &mdash; <em>$Date: 2014-12-08 $</em>
+	 * @version 3.0 &mdash; <em>$Date: 2014-12-19 $</em>
 	 */
 	public static final class ThreadLocal
 		extends java.lang.ThreadLocal<LCG64ShiftRandom>
@@ -123,7 +123,7 @@ public class LCG64ShiftRandom extends Random64 {
 
 		/**
 		 * Create a new <i>thread local</i> instance of the
-		 * {@code LCG64ShiftRandom} PRGN with the {@code DEFAULT} parameters.
+		 * {@code LCG64ShiftRandom} PRNG with the {@code DEFAULT} parameters.
 		 */
 		public ThreadLocal() {
 			this(Param.DEFAULT);
@@ -131,7 +131,7 @@ public class LCG64ShiftRandom extends Random64 {
 
 		/**
 		 * Create a new <i>thread local</i> instance of the
-		 * {@code LCG64ShiftRandom} PRGN with the given parameters.
+		 * {@code LCG64ShiftRandom} PRNG with the given parameters.
 		 *
 		 * @param param the LC parameters.
 		 * @throws NullPointerException if the given parameters are null.
@@ -193,12 +193,12 @@ public class LCG64ShiftRandom extends Random64 {
 	}
 
 	/**
-	 * This is a <i>thread safe</i> variation of the this PRGN&mdash;by
+	 * This is a <i>thread safe</i> variation of the this PRNG&mdash;by
 	 * synchronizing the random number generation.
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 3.0 &mdash; <em>$Date: 2014-12-08 $</em>
+	 * @version 3.0 &mdash; <em>$Date: 2014-12-19 $</em>
 	 */
 	public static final class ThreadSafe extends LCG64ShiftRandom {
 		private static final long serialVersionUID = 1L;
@@ -277,7 +277,7 @@ public class LCG64ShiftRandom extends Random64 {
 	 *
 	 * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
 	 * @since 1.1
-	 * @version 2.0 &mdash; <em>$Date: 2014-12-08 $</em>
+	 * @version 2.0 &mdash; <em>$Date: 2014-12-19 $</em>
 	 */
 	public static final class Param implements Serializable {
 
