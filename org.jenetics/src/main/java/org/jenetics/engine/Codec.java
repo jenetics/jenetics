0de93464f3097ea23c4ffa6502325a1da12dda66
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-03-06 16:05:49 +0100

    Copy classes from test directory.

---
+++ org.jenetics/src/main/java/org/jenetics/engine/Codec.java
@@ -0,0 +1,66 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package org.jenetics.diagram.problem;
+
+import java.util.function.Function;
+
+import org.jenetics.DoubleChromosome;
+import org.jenetics.DoubleGene;
+import org.jenetics.Gene;
+import org.jenetics.Genotype;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ */
+public interface Codec<G extends Gene<?, G>, S> {
+
+	public Function<S, Genotype<G>> encoder();
+
+	public Function<Genotype<G>, S> decoder();
+
+	public static <G extends Gene<?, G>, S> Codec<G, S> of(
+		final Function<S, Genotype<G>> encoder,
+		final Function<Genotype<G>, S> decoder
+	) {
+		return new Codec<G, S>() {
+			@Override
+			public Function<S, Genotype<G>> encoder() {
+				return encoder;
+			}
+
+			@Override
+			public Function<Genotype<G>, S> decoder() {
+				return decoder;
+			}
+		};
+	}
+
+	public static Codec<DoubleGene, Double> ofDouble(
+		final double min,
+		final double max
+	) {
+		return of(
+			value -> Genotype
+				.of(DoubleChromosome.of(DoubleGene.of(value, min, max))),
+			gt -> gt.getChromosome().getGene().getAllele()
+		);
+	}
+
+}
---
+++ org.jenetics/src/main/java/org/jenetics/engine/Problem.java
@@ -0,0 +1,72 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package org.jenetics.diagram.problem;
+
+import java.util.function.Function;
+
+import org.jenetics.Gene;
+import org.jenetics.Genotype;
+import org.jenetics.util.Factory;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ */
+public interface Problem<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+{
+
+	public Factory<Genotype<G>> genotype();
+
+	public Function<Genotype<G>, C> function();
+
+
+	public static <G extends Gene<?, G>, C extends Comparable<? super C>>
+	Problem<G, C> of(
+		final Factory<Genotype<G>> genotype,
+		final Function<Genotype<G>, C> function
+	) {
+		return new Problem<G, C>() {
+			@Override
+			public Factory<Genotype<G>> genotype() {
+				return genotype;
+			}
+
+			@Override
+			public Function<Genotype<G>, C> function() {
+				return function;
+			}
+		};
+	}
+
+	public static <G extends Gene<?, G>, C extends Comparable<? super C>, S>
+	Problem<G, C> of(
+		final Factory<Genotype<G>> genotype,
+		final Function<S, C> function,
+		final Function<Genotype<G>, S> decoder
+	) {
+		return of(
+			genotype,
+			function.compose(decoder)
+		);
+	}
+
+}
