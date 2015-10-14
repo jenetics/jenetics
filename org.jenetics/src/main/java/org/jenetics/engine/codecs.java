60b441418745b5ea755822275ecdb57da96e393a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-10-14 22:55:27 +0200

    Rename 'ofSubset' to 'ofSubSet'.

--- org.jenetics/src/main/java/org/jenetics/engine/codecs.java
+++ org.jenetics/src/main/java/org/jenetics/engine/codecs.java
@@ -529,7 +529,7 @@ public final class codecs {
 	 * @throws IllegalArgumentException if the {@code basicSet} size is smaller
 	 *         than one.
 	 */
-	public static <T> Codec<ISeq<T>, BitGene> ofSubset(final ISeq<T> basicSet) {
+	public static <T> Codec<ISeq<T>, BitGene> ofSubSet(final ISeq<T> basicSet) {
 		requireNonNull(basicSet);
 		require.positive(basicSet.length());
 
