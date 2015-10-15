877493e59ff4c2bedc9080564625e06332fb12ac
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-10-15 18:44:27 +0200

    Javadoc changes.

--- org.jenetics/src/main/java/org/jenetics/engine/codecs.java
+++ org.jenetics/src/main/java/org/jenetics/engine/codecs.java
@@ -55,8 +55,8 @@ import org.jenetics.util.LongRange;
  * This class contains factory methods for creating common  problem encodings.
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version 3.3
  * @since 3.2
+ * @version 3.3
  */
 public final class codecs {
 
