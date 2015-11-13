f3baec1b1f1a3f22d00b7efcf0ba8acbc03d7a6d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-11-13 19:12:30 +0100

    Override 'isValid' for 'DoubleGene' class.
    A gene is invalid if the value is equal to the maximal value.

--- org.jenetics/src/main/java/org/jenetics/DoubleGene.java
+++ org.jenetics/src/main/java/org/jenetics/DoubleGene.java
@@ -19,6 +19,8 @@
  */
 package org.jenetics;
 
+import static java.lang.Double.compare;
+import static java.lang.String.format;
 import static org.jenetics.internal.math.random.nextDouble;
 import static org.jenetics.util.RandomRegistry.getRandom;
 
@@ -77,6 +79,16 @@ public final class DoubleGene
 	 */
 	DoubleGene(final Double value, final Double min, final Double max) {
 		super(value, min, max);
+		if (compare(value, min) < 0 || compare(value, max) >= 0) {
+			throw new IllegalArgumentException(format(
+				"Value %s is out of range [%s..%s): ", value, min, max
+			));
+		}
+	}
+
+	@Override
+	public boolean isValid() {
+		return compare(_value, _min) >= 0 && compare(_value, _max) < 0;
 	}
 
 	@Override
