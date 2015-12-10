5e2540b523a75569259ff06a60175ba56fd5785e
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-12-10 21:16:56 +0100

    Fix wrong package name.

--- org.jenetics/src/test/java/org/jenetics/problem/Knapsack.java
+++ org.jenetics/src/test/java/org/jenetics/problem/Knapsack.java
@@ -17,7 +17,7 @@
  * Author:
  *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
  */
-package org.jentics.problem;
+package org.jenetics.problem;
 
 import static java.util.Objects.requireNonNull;
 
@@ -31,7 +31,7 @@ import org.jenetics.BitGene;
 import org.jenetics.engine.Codec;
 import org.jenetics.engine.Problem;
 import org.jenetics.engine.codecs;
-import org.jentics.problem.Knapsack.Item;
+import org.jenetics.problem.Knapsack.Item;
 import org.jenetics.util.ISeq;
 import org.jenetics.util.MSeq;
 
