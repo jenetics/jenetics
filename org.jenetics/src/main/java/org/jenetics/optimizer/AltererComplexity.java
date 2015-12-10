d716978123d4f398ffc56a18e56f032a4746ee75
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-12-10 19:40:55 +0100

    #32: Refactoring of 'AltererComplexity' class.

--- org.jenetics/src/main/java/org/jenetics/optimizer/AltererComplexity.java
+++ org.jenetics/src/main/java/org/jenetics/optimizer/AltererComplexity.java
@@ -28,6 +28,7 @@ import org.jenetics.GaussianMutator;
 import org.jenetics.MeanAlterer;
 import org.jenetics.MultiPointCrossover;
 import org.jenetics.Mutator;
+import org.jenetics.PartiallyMatchedCrossover;
 import org.jenetics.Recombinator;
 import org.jenetics.SinglePointCrossover;
 import org.jenetics.SwapMutator;
@@ -37,42 +38,70 @@ import org.jenetics.SwapMutator;
  * @version !__version__!
  * @since !__version__!
  */
-final class AltererComplexity {
+final class AltererComplexity implements Complexity<Alterer<?, ?>> {
 
-	private static final
-	Map<Class<?>, Complexity<?>> COMPLEXITIES = new HashMap<>();
+	static final AltererComplexity INSTANCE = new AltererComplexity();
+
+	private static final Map<Class<?>, Complexity<?>> COMP = new HashMap<>();
 
 	static {
 		// Alterers
-		put(Mutator.class, a -> 1.0 + alt(a));
-		put(SwapMutator.class, a -> 1.0 + alt(a));
-		put(GaussianMutator.class, a -> 1.5 + alt(a));
+		put(
+			Mutator.class,
+			a -> 1.0 + baseComplexity(a)
+		);
+		put(
+			SwapMutator.class,
+			a -> 1.0 + baseComplexity(a)
+		);
+		put(
+			PartiallyMatchedCrossover.class,
+			a -> 1.3 + baseComplexity(a)
+		);
+		put(
+			GaussianMutator.class,
+			a -> 1.5 + baseComplexity(a)
+		);
 
 		// Recombinators
-		put(SinglePointCrossover.class, a -> 1.0 + comb(a));
-		put(MultiPointCrossover.class, a -> 1.0 + comb(a) + a.getN()*0.2);
-		put(MeanAlterer.class, a -> 1.5 + comb(a));
+		put(
+			SinglePointCrossover.class,
+			a -> 1.0 + recombinationComplexity(a)
+		);
+		put(
+			MultiPointCrossover.class,
+			a -> 1.0 + recombinationComplexity(a) + a.getN()*0.2
+		);
+		put(
+			MeanAlterer.class, a -> 1.5 + recombinationComplexity(a)
+		);
 	}
 
 	private static <T> void put(final Class<T> t, final Complexity<T> c) {
-		COMPLEXITIES.put(t, c);
+		COMP.put(t, c);
 	}
 
-	private static double comb(final Recombinator<?, ?> alterer) {
-		return alterer.getOrder() + alt(alterer);
+	private static double recombinationComplexity(
+		final Recombinator<?, ?> recombinator
+	) {
+		return recombinator.getOrder() + baseComplexity(recombinator);
 	}
 
-	private static double alt(final AbstractAlterer<?, ?> alterer) {
+	private static double baseComplexity(final AbstractAlterer<?, ?> alterer) {
 		return alterer.getProbability()*10;
 	}
 
-	@SuppressWarnings("unchecked")
-	public static double of(final Alterer<?, ?> alterer) {
-		final Complexity<Alterer<?, ?>> complexity =
-			(Complexity<Alterer<?, ?>>)COMPLEXITIES.get(alterer.getClass());
-
-		return (complexity != null ? complexity : (Complexity<Alterer<?, ?>>)a -> 1.5)
-			.complexity(alterer);
+	private AltererComplexity() {
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public double complexity(final Alterer<?, ?> alterer) {
+		final Complexity<Alterer<?, ?>> complexity =
+			(Complexity<Alterer<?, ?>>) COMP.get(alterer.getClass());
+
+		return complexity != null
+				? complexity.complexity(alterer)
+				: 1.5;
+	}
 }
