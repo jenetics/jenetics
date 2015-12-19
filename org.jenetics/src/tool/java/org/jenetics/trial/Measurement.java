4c9da704b3952516a930dfbd73864b0bc7cb3b40
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-12-19 14:06:32 +0100

    #79: Implement 'Measurement' class.

--- org.jenetics/src/tool/java/org/jenetics/trial/Measurement.java
+++ org.jenetics/src/tool/java/org/jenetics/trial/Measurement.java
@@ -19,10 +19,66 @@
  */
 package org.jenetics.trial;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.jenetics.util.ISeq;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
  */
 public class Measurement {
+
+	private final String _name;
+	private final String _description;
+
+	private final ISeq<Parameter> _parameters;
+	private final Map<String, Data> _data = new HashMap<>();
+
+	public Measurement(
+		final String name,
+		final String description,
+		final ISeq<Parameter> parameters
+	) {
+		_name = name;
+		_description = description;
+		_parameters = requireNonNull(parameters);
+	}
+
+	public Optional<String> getName() {
+		return Optional.ofNullable(_name);
+	}
+
+	public Optional<String> getDescription() {
+		return Optional.ofNullable(_description);
+	}
+
+	public ISeq<Parameter> getParameters() {
+		return _parameters;
+	}
+
+	public void execute(final Function<Parameter, Optional<double[]>> function) {
+
+	}
+
+	public static void main(final String[] args) {
+		final Measurement measurement = null;
+		measurement.execute(Measurement::foo);
+	}
+
+	static Optional<double[]> foo(final Parameter parameter) {
+		return Optional.empty();
+	}
+
+
+	static final class Model {
+
+	}
+
 }
