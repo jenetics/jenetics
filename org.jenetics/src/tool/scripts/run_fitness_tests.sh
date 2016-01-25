929c057262bc1ed93e071feb306a71beb671ff42
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-01-25 21:58:17 +0100

    #79: Add engine performance run script.

--- org.jenetics/src/tool/scripts/run_fitness_tests.sh
+++ org.jenetics/src/tool/scripts/run_fitness_tests.sh
@@ -1,1 +1,34 @@
 #!/usr/bin/env bash
+
+function read_link() {
+    local path=$1
+    if [ -d $path ] ; then
+        local abspath=$(cd $path; pwd)
+    else
+        local prefix=$(cd $(dirname -- $path); pwd)
+        local suffix=$(basename $path)
+        local abspath="$prefix/$suffix"
+    fi
+    echo $abspath
+}
+
+TESTS=(
+	"KnapsackExecutionTime:knapsack_execution_time-perf.xml"
+	"KnapsackFitnessThreshold:knapsack_fitness_threshold-perf.xml"
+	"KnapsackFixedGeneration:knapsack_fixed_generation-perf.xml"
+	"KnapsackSteadyFitness:knapsack_steady_fitness-perf.xml"
+)
+
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+RESULT_BASE_PATH=`read_link "../resources/org/jenetics/trail"`
+
+JRUN=`read_link "../../../../jrun"`
+
+
+for test in ${TESTS[@]}
+do
+    CLASS="org.jenetics.evaluation.${test%%:*}"
+    RESULT="${RESULT_BASE_PATH}/${test#*:}"
+
+    ${JRUN} ${CLASS} --result-file ${RESULT} --sample-count 5
+done
