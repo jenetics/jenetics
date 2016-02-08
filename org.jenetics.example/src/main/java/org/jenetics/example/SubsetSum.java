7504cb0f740c299ff75875945749e7509e6142e4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-02-08 21:54:10 +0100

    Add 'SubsetSum' problem class.

--- org.jenetics.example/src/main/java/org/jenetics/example/SubsetSum.java
+++ org.jenetics.example/src/main/java/org/jenetics/example/SubsetSum.java
@@ -19,10 +19,82 @@
  */
 package org.jenetics.example;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Random;
+import java.util.function.Function;
+
+import org.jenetics.EnumGene;
+import org.jenetics.Mutator;
+import org.jenetics.PartiallyMatchedCrossover;
+import org.jenetics.Phenotype;
+import org.jenetics.engine.Codec;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.EvolutionResult;
+import org.jenetics.engine.Problem;
+import org.jenetics.engine.codecs;
+import org.jenetics.engine.limit;
+import org.jenetics.util.ISeq;
+import org.jenetics.util.LCG64ShiftRandom;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version !__version__!
- * @since !__version__!
+ * @version 3.4
+ * @since 3.4
  */
-public class SubsetSum {
+public class SubsetSum
+	implements Problem<ISeq<Integer>, EnumGene<Integer>, Integer>
+{
+
+	private final ISeq<Integer> _basicSet;
+	private final int _size;
+
+	public SubsetSum(final ISeq<Integer> basicSet, final int size) {
+		_basicSet = requireNonNull(basicSet);
+		_size = size;
+	}
+
+	@Override
+	public Function<ISeq<Integer>, Integer> fitness() {
+		return subset -> Math.abs(
+			subset.stream()
+				.mapToInt(Integer::intValue)
+				.sum()
+		);
+	}
+
+	@Override
+	public Codec<ISeq<Integer>, EnumGene<Integer>> codec() {
+		return codecs.ofSubSet(_basicSet, _size);
+	}
+
+	public static SubsetSum of(final int n, final int k, final Random random) {
+		return new SubsetSum(
+			random.doubles()
+				.limit(n)
+				.mapToObj(d -> (int)((d - 0.5)*n))
+				.collect(ISeq.toISeq()),
+			k
+		);
+	}
+
+
+	public static void main(final String[] args) {
+		final SubsetSum problem = of(500, 15, new LCG64ShiftRandom(101010));
+
+		final Engine<EnumGene<Integer>, Integer> engine = Engine.builder(problem)
+			.minimizing()
+			.maximalPhenotypeAge(5)
+			.alterers(
+				new PartiallyMatchedCrossover<>(0.4),
+				new Mutator<>(0.3))
+			.build();
+
+		final Phenotype<EnumGene<Integer>, Integer> result = engine.stream()
+			.limit(limit.bySteadyFitness(55))
+			.collect(EvolutionResult.toBestPhenotype());
+
+		System.out.print(result);
+	}
+
 }
