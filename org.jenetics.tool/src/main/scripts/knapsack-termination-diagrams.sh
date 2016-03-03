96ec76c7895f3c4c45dd5646a2567da79efd3c9b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-03-03 18:40:16 +0100

    Update diagrams.

--- org.jenetics.tool/src/main/scripts/knapsack-termination-diagrams.sh
+++ org.jenetics.tool/src/main/scripts/knapsack-termination-diagrams.sh
@@ -13,7 +13,11 @@ read_link() {
 }
 
 RESULTS=(
-	"Knapsack-selector_comparison.xml"
+	"Knapsack-execution_time_termination.xml:Generation,Fitness"
+	"Knapsack-fitness_threshold_termination.xml:Generation,Fitness"
+	"Knapsack-fixed_generation_termination.xml:Generation,Fitness"
+	"Knapsack-steady_fitness_termination.xml:Generation,Fitness"
+	"Knapsack-selector_comparison.xml:Fitness1,Fitness2"
 )
 
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
@@ -22,7 +26,7 @@ JRUN=`read_link "${SCRIPT_DIR}/../../../../jrun"`
 
 for item in ${RESULTS[@]}
 do
-	RESULT="${RESULT_BASE_PATH}/${item}"
-	echo $RESULT
-	${JRUN} org.jenetics.tool.evaluation.Diagram --input ${RESULT} --samples "Fitness1,Fitness2"
+	RESULT="${RESULT_BASE_PATH}/${item%%:*}"
+	SAMPLES="${item#*:}"
+	${JRUN} org.jenetics.tool.evaluation.Diagram --input ${RESULT} --samples ${SAMPLES}
 done
