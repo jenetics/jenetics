dde76884fee5e53c67d3d30e3714032f400fbca4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-03-18 19:40:51 +0100

    Fix indentation.

--- org.jenetix/src/main/java/org/jenetix/WeaselMutator.java
+++ org.jenetix/src/main/java/org/jenetix/WeaselMutator.java
@@ -15,60 +15,60 @@ import org.jenetics.util.MSeq;
  * @author Franz Wilhelmstötter <franz.wilhelmstoetter@emarsys.com>
  */
 public class WeaselMutator<
-    G extends Gene<?, G>,
-    C extends Comparable<? super C>
->
-    extends Mutator<G, C>
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+	>
+	extends Mutator<G, C>
 {
 
-    public WeaselMutator(final double probability) {
-        super(probability);
-    }
+	public WeaselMutator(final double probability) {
+		super(probability);
+	}
 
-    public WeaselMutator() {
-        this(0.05);
-    }
+	public WeaselMutator() {
+		this(0.05);
+	}
 
-    @Override
-    public int alter(final Population<G, C> population, final long generation) {
-        final IntRef alterations = new IntRef(0);
+	@Override
+	public int alter(final Population<G, C> population, final long generation) {
+		final IntRef alterations = new IntRef(0);
 
-        for (int i = 0; i < population.size(); ++i) {
-            final Phenotype<G, C> pt = population.get(i);
+		for (int i = 0; i < population.size(); ++i) {
+			final Phenotype<G, C> pt = population.get(i);
 
-            final Genotype<G> gt = pt.getGenotype();
-            final Genotype<G> mgt = mutate(gt, alterations);
-                                              // Is package private.
-            final Phenotype<G, C> mpt = null; //pt.newInstance(mgt, generation);
-            population.set(i, mpt);
-        }
+			final Genotype<G> gt = pt.getGenotype();
+			final Genotype<G> mgt = mutate(gt, alterations);
+			// Is package private.
+			final Phenotype<G, C> mpt = null; //pt.newInstance(mgt, generation);
+			population.set(i, mpt);
+		}
 
-        return alterations.value;
-    }
+		return alterations.value;
+	}
 
-    private Genotype<G> mutate(
-        final Genotype<G> genotype,
-        final IntRef alterations
-    ) {
-        final MSeq<Chromosome<G>> chromosomes = genotype.toSeq().copy();
+	private Genotype<G> mutate(
+		final Genotype<G> genotype,
+		final IntRef alterations
+	) {
+		final MSeq<Chromosome<G>> chromosomes = genotype.toSeq().copy();
 
-        alterations.value += IntStream.range(0, chromosomes.size())
-                .map(i -> mutate(chromosomes, i))
-                .sum();
+		alterations.value += IntStream.range(0, chromosomes.size())
+			.map(i -> mutate(chromosomes, i))
+			.sum();
 
-                     // Is package private.
-        return null; //genotype.newInstance(chromosomes.toISeq());
-    }
+		// Is package private.
+		return null; //genotype.newInstance(chromosomes.toISeq());
+	}
 
-    private int mutate(final MSeq<Chromosome<G>> c, final int index) {
-        final Chromosome<G> chromosome = c.get(index);
-        final MSeq<G> genes = chromosome.toSeq().copy();
+	private int mutate(final MSeq<Chromosome<G>> c, final int index) {
+		final Chromosome<G> chromosome = c.get(index);
+		final MSeq<G> genes = chromosome.toSeq().copy();
 
-        final int mutations = mutate(genes, _probability);
-        if (mutations > 0) {
-            c.set(index, chromosome.newInstance(genes.toISeq()));
-        }
-        return mutations;
-    }
+		final int mutations = mutate(genes, _probability);
+		if (mutations > 0) {
+			c.set(index, chromosome.newInstance(genes.toISeq()));
+		}
+		return mutations;
+	}
 
 }
--- org.jenetix/src/main/java/org/jenetix/WeaselSelector.java
+++ org.jenetix/src/main/java/org/jenetix/WeaselSelector.java
@@ -14,31 +14,31 @@ import org.jenetics.stat.MinMax;
  * @author Franz Wilhelmstötter <franz.wilhelmstoetter@emarsys.com>
  */
 public class WeaselSelector<
-    G extends Gene<?, G>,
-    C extends Comparable<? super C>
->
-    implements Selector<G, C>
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+	>
+	implements Selector<G, C>
 {
-    @Override
-    public Population<G, C> select(
-        final Population<G, C> population,
-        final int count,
-        final Optimize opt
-    ) {
-        requireNonNull(population, "Population");
-        requireNonNull(opt, "Optimization");
-        if (count < 0) {
-            throw new IllegalArgumentException(format(
-                "Selection count must be greater or equal then zero, but was %s",
-                count
-            ));
-        }
+	@Override
+	public Population<G, C> select(
+		final Population<G, C> population,
+		final int count,
+		final Optimize opt
+	) {
+		requireNonNull(population, "Population");
+		requireNonNull(opt, "Optimization");
+		if (count < 0) {
+			throw new IllegalArgumentException(format(
+				"Selection count must be greater or equal then zero, but was %s",
+				count
+			));
+		}
 
-        final MinMax<Phenotype<G, C>> minMax = MinMax.of(opt.ascending());
-        population.forEach(minMax);
+		final MinMax<Phenotype<G, C>> minMax = MinMax.of(opt.ascending());
+		population.forEach(minMax);
 
-        final Population<G, C> selection = new Population<>(count);
-        selection.fill(minMax::getMax, count);
-        return selection;
-    }
+		final Population<G, C> selection = new Population<>(count);
+		selection.fill(minMax::getMax, count);
+		return selection;
+	}
 }
