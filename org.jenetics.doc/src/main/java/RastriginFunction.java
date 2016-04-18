d3c23eb5ff9c517331faeed7aa781027a2c51083
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-04-18 17:56:46 +0200

    #101: Add 'RastriginFunction' class.

--- org.jenetics.doc/src/main/java/RastriginFunction.java
+++ org.jenetics.doc/src/main/java/RastriginFunction.java
@@ -1,27 +1,56 @@
-/*
- * Java Genetic Algorithm Library (@__identifier__@).
- * Copyright (c) @__year__@ Franz Wilhelmstötter
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author:
- *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
- */
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static org.jenetics.engine.EvolutionResult.toBestPhenotype;
+import static org.jenetics.engine.limit.bySteadyFitness;
 
-/**
- * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version !__version__!
- * @since !__version__!
- */
+import org.jenetics.DoubleGene;
+import org.jenetics.MeanAlterer;
+import org.jenetics.Mutator;
+import org.jenetics.Optimize;
+import org.jenetics.Phenotype;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.EvolutionStatistics;
+import org.jenetics.engine.codecs;
+import org.jenetics.util.DoubleRange;
+
 public class RastriginFunction {
+
+	private static final double A = 10;
+	private static final double R = 5.12;
+	private static final int N = 2;
+
+	private static double fitness(final double[] x) {
+		double value = A*N;
+		for (int i = 0; i < N; ++i) {
+			value += x[i]*x[i] - A*cos(2.0*PI*x[i]);
+		}
+
+		return value;
+	}
+
+	public static void main(final String[] args) {
+		final Engine<DoubleGene, Double> engine = Engine
+			.builder(
+				RastriginFunction::fitness,
+				// Codec for 'x' vector.
+				codecs.ofVector(DoubleRange.of(-R, R), N))
+			.populationSize(500)
+			.optimize(Optimize.MINIMUM)
+			.alterers(
+				new Mutator<>(0.03),
+				new MeanAlterer<>(0.6))
+			.build();
+
+		final EvolutionStatistics<Double, ?>
+			statistics = EvolutionStatistics.ofNumber();
+
+		final Phenotype<DoubleGene, Double> best = engine.stream()
+			.limit(bySteadyFitness(7))
+			.peek(statistics)
+			.collect(toBestPhenotype());
+
+		System.out.println(statistics);
+		System.out.println(best);
+	}
+
 }
