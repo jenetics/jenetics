200edf04a0416d058d464be9ad737292ba40bd0f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-05-18 20:18:36 +0200

    #111: Fix evolution engine dead-lock.

--- org.jenetics/src/test/java/org/jenetics/engine/EngineTest.java
+++ org.jenetics/src/test/java/org/jenetics/engine/EngineTest.java
@@ -19,6 +19,10 @@
  */
 package org.jenetics.engine;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 import java.util.stream.LongStream;
 
@@ -125,6 +129,38 @@ public class EngineTest {
 			.collect(EvolutionResult.toBestEvolutionResult());
 
 		//Assert.assertEquals(25L, result.getTotalGenerations());
+	}
+
+	// https://github.com/jenetics/jenetics/issues/111
+	@Test(dataProvider = "executors", timeOut = 2_000L)
+	public void executorDeadLock(final Executor executor) {
+		try {
+			final Engine<DoubleGene, Double> engine = Engine
+				.builder(gt -> gt.getGene().doubleValue(), DoubleChromosome.of(0, 1))
+				.executor(executor)
+				.populationSize(10)
+				.build();
+
+			engine.stream()
+				.limit(100)
+				.collect(EvolutionResult.toBestEvolutionResult());
+		} finally {
+			if (executor instanceof ExecutorService) {
+				((ExecutorService)executor).shutdown();
+			}
+		}
+	}
+
+	@DataProvider(name = "executors")
+	public Object[][] executors() {
+		return new Object[][] {
+			{(Executor)Runnable::run},
+			{Executors.newFixedThreadPool(1)},
+			{Executors.newSingleThreadExecutor()},
+			{Executors.newFixedThreadPool(10)},
+			{new ForkJoinPool(1)},
+			{new ForkJoinPool(10)}
+		};
 	}
 
 }
