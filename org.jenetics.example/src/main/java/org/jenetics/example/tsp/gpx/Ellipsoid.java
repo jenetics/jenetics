479f94885f43c9c688609a11189c7a6fa229896b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-07-17 19:15:46 +0200

    Add 'Ellipsoid' class.

---
+++ org.jenetics.example/src/main/java/org/jenetics/example/tsp/gpx/Ellipsoid.java
@@ -0,0 +1,34 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package org.jenetics.example.tsp.gpx;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version !__version__!
+ * @since !__version__!
+ */
+public final class Ellipsoid implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+}
