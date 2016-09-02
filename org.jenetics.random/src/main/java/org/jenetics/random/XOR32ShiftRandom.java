557affc0cce1b79c3435896bfe92336d355b3c1f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-09-02 21:37:23 +0200

    Improve Javadoc.

--- org.jenetics.random/src/main/java/org/jenetics/random/XOR32ShiftRandom.java
+++ org.jenetics.random/src/main/java/org/jenetics/random/XOR32ShiftRandom.java
@@ -27,6 +27,12 @@ import java.io.Serializable;
 import java.util.Objects;
 
 /**
+ * This generator was discovered and characterized by George Marsaglia
+ * [<a href="http://www.jstatsoft.org/v08/i14/paper">Xorshift RNGs</a>]. In just
+ * three XORs and three shifts (generally fast operations) it produces a full
+ * period of 2<sup>32</sup> - 1 on 32 bits. (The missing value is zero, which
+ * perpetuates itself and must be avoided.) High and low bits pass Diehard.
+ * <p>
  * Implementation of the XOR shift PRNG. The following listing shows the actual
  * PRNG implementation.
  * <pre>{@code
@@ -84,100 +90,6 @@ public class XOR32ShiftRandom extends Random32 {
 			new Param(8, 23, 7),   // p=100, w=4, f=10
 			new Param(19, 9, 11),  // p=100, w=4, f=10
 			new Param(6, 21, 7),   // p=100, w=2, f=12
-			new Param(20, 5, 3),   // p=99, w=6, f=9
-			new Param(13, 17, 11), // p=99, w=5, f=10
-			new Param(16, 7, 11),  // p=99, w=5, f=10
-			new Param(4, 5, 15),   // p=99, w=4, f=11
-			new Param(17, 13, 3),  // p=99, w=4, f=11
-			new Param(5, 6, 13),   // p=99, w=2, f=13
-			new Param(2, 15, 9),   // p=98, w=7, f=9
-			new Param(12, 25, 7),  // p=98, w=7, f=9
-			new Param(5, 7, 9),    // p=98, w=6, f=10
-			new Param(9, 5, 7),    // p=98, w=6, f=10
-			new Param(7, 25, 13),  // p=98, w=5, f=11
-			new Param(6, 5, 13),   // p=98, w=5, f=11
-			new Param(22, 5, 3),   // p=98, w=5, f=11
-			new Param(2, 9, 7),    // p=98, w=5, f=11
-			new Param(9, 12, 23),  // p=98, w=4, f=12
-			new Param(11, 17, 13), // p=97, w=9, f=8
-			new Param(7, 23, 8),   // p=97, w=6, f=11
-			new Param(23, 12, 9),  // p=97, w=6, f=11
-			new Param(13, 6, 5),   // p=97, w=6, f=11
-			new Param(6, 13, 21),  // p=97, w=5, f=12
-			new Param(7, 25, 12),  // p=97, w=5, f=12
-			new Param(21, 13, 6),  // p=97, w=4, f=13
-			new Param(13, 11, 17), // p=97, w=4, f=13
-			new Param(7, 9, 2),    // p=97, w=4, f=13
-			new Param(13, 5, 6),   // p=97, w=4, f=13
-			new Param(21, 7, 6),   // p=97, w=3, f=14
-			new Param(5, 25, 3),   // p=97, w=2, f=15
-			new Param(11, 19, 9),  // p=97, w=10, f=7
-			new Param(2, 15, 5),   // p=97, w=1, f=16
-			new Param(9, 21, 10),  // p=96, w=8, f=10
-			new Param(7, 5, 9),    // p=96, w=8, f=10
-			new Param(11, 6, 1),   // p=96, w=8, f=10
-			new Param(2, 21, 9),   // p=96, w=6, f=12
-			new Param(1, 6, 11),   // p=96, w=6, f=12
-			new Param(11, 9, 19),  // p=96, w=5, f=13
-			new Param(1, 16, 11),  // p=96, w=4, f=14
-			new Param(15, 5, 4),   // p=96, w=4, f=14
-			new Param(23, 9, 12),  // p=96, w=4, f=14
-			new Param(10, 9, 25),  // p=96, w=4, f=14
-			new Param(5, 9, 25),   // p=96, w=3, f=15
-			new Param(2, 7, 9),    // p=95, w=9, f=10
-			new Param(13, 19, 5),  // p=95, w=8, f=11
-			new Param(11, 7, 16),  // p=95, w=7, f=12
-			new Param(11, 21, 13), // p=95, w=7, f=12
-			new Param(25, 5, 9),   // p=95, w=6, f=13
-			new Param(21, 5, 3),   // p=95, w=6, f=13
-			new Param(9, 15, 2),   // p=95, w=5, f=14
-			new Param(16, 11, 7),  // p=95, w=5, f=14
-			new Param(9, 7, 2),    // p=95, w=4, f=15
-			new Param(1, 9, 5),    // p=95, w=3, f=16
-			new Param(6, 17, 3),   // p=95, w=1, f=18
-			new Param(15, 17, 20), // p=95, w=0, f=19
-			new Param(15, 9, 2),   // p=94, w=8, f=12
-			new Param(25, 7, 13),  // p=94, w=8, f=12
-			new Param(8, 27, 5),   // p=94, w=7, f=13
-			new Param(12, 9, 23),  // p=94, w=6, f=14
-			new Param(28, 9, 5),   // p=94, w=6, f=14
-			new Param(7, 11, 16),  // p=94, w=4, f=16
-			new Param(9, 23, 8),   // p=94, w=4, f=16
-			new Param(23, 15, 17), // p=94, w=4, f=16
-			new Param(27, 8, 5),   // p=94, w=4, f=16
-			new Param(19, 5, 13),  // p=94, w=4, f=16
-			new Param(9, 10, 25),  // p=94, w=4, f=16
-			new Param(17, 4, 3),   // p=94, w=2, f=18
-			new Param(13, 25, 7),  // p=93, w=9, f=12
-			new Param(3, 13, 17),  // p=93, w=7, f=14
-			new Param(8, 23, 9),   // p=93, w=7, f=14
-			new Param(5, 27, 8),   // p=93, w=7, f=14
-			new Param(9, 21, 16),  // p=93, w=3, f=18
-			new Param(16, 21, 9),  // p=93, w=3, f=18
-			new Param(20, 15, 17), // p=93, w=2, f=19
-			new Param(13, 5, 19),  // p=93, w=1, f=20
-			new Param(7, 13, 25),  // p=92, w=8, f=14
-			new Param(3, 5, 20),   // p=92, w=7, f=15
-			new Param(19, 5, 1),   // p=92, w=7, f=15
-			new Param(3, 17, 4),   // p=92, w=4, f=18
-			new Param(5, 2, 15),   // p=92, w=4, f=18
-			new Param(3, 17, 6),   // p=92, w=3, f=19
-			new Param(17, 3, 6),   // p=92, w=3, f=19
-			new Param(25, 5, 3),   // p=92, w=11, f=11
-			new Param(5, 9, 1),    // p=91, w=6, f=17
-			new Param(3, 25, 5),   // p=91, w=6, f=17
-			new Param(27, 3, 13),  // p=91, w=4, f=19
-			new Param(13, 7, 25),  // p=91, w=2, f=21
-			new Param(27, 5, 8),   // p=91, w=11, f=12
-			new Param(17, 6, 3),   // p=90, w=8, f=16
-			new Param(17, 6, 9),   // p=90, w=8, f=16
-			new Param(25, 7, 12),  // p=90, w=8, f=16
-			new Param(25, 10, 9),  // p=90, w=8, f=16
-			new Param(15, 2, 5),   // p=90, w=7, f=17
-			new Param(7, 25, 20),  // p=90, w=5, f=19
-			new Param(21, 2, 5),   // p=90, w=4, f=20
-			new Param(23, 8, 7),   // p=90, w=10, f=14
-			new Param(5, 27, 25),  // p=90, w=1, f=23
 		};
 
 		/**
@@ -264,7 +176,7 @@ public class XOR32ShiftRandom extends Random32 {
 		private final Boolean _sentry = Boolean.TRUE;
 
 		private TLXOR32ShiftRandom() {
-			super(nextParam(), KISS32Random.seedBytes());
+			super(nextParam(), XOR32ShiftRandom.seedBytes());
 		}
 
 		private static Param nextParam() {
