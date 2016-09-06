a20ab9526629ca8383acfcf63e5019f11c94a737
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-09-06 23:35:36 +0200

    Add 'KISS64Random' compatibility test.

--- org.jenetics.random/src/test/java/org/jenetics/random/KISS64RandomCompatibilityTest.java
+++ org.jenetics.random/src/test/java/org/jenetics/random/KISS64RandomCompatibilityTest.java
@@ -19,10 +19,72 @@
  */
 package org.jenetics.random;
 
+import static java.lang.String.format;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version !__version__!
- * @since !__version__!
  */
 public class KISS64RandomCompatibilityTest {
+
+	@Test(dataProvider = "data")
+	public void random(final TestData data) {
+		final String[] parameters = data.getParameters();
+		final long seed = Long.parseLong(parameters[0]);
+
+		final KISS64Random random = new KISS64Random(seed);
+
+		for (final String[] value : data) {
+			final long expected = Long.parseLong(value[0]);
+			Assert.assertEquals(random.nextLong(), expected);
+		}
+	}
+
+	@Test(dataProvider = "data")
+	public void threadSafeRandom(final TestData data) {
+		final String[] parameters = data.getParameters();
+		final long seed = Long.parseLong(parameters[0]);
+
+		final KISS64Random random = new KISS64Random.ThreadSafe(seed);
+
+		for (final String[] value : data) {
+			final long expected = Long.parseLong(value[0]);
+			Assert.assertEquals(random.nextLong(), expected);
+		}
+	}
+
+	@DataProvider(name = "data")
+	public Object[][] data() {
+		return TestData.list("/org/jenetics/random/KISS64Random")
+			.map(data -> new Object[]{data})
+			.toArray(Object[][]::new);
+	}
+
+	public static void main(final String[] args) throws IOException {
+		final String dir = "org.jenetics.random/src/test/resources/" +
+			"org/jenetics/random/KISS64Random";
+
+		for (int i = 0; i < 20; ++i) {
+			final long seed = i*12345678;
+			final Random random = new KISS64Random(seed);
+
+			final File file = new File(dir, format("random[%d].dat", seed));
+			file.getParentFile().mkdirs();
+
+			try (PrintWriter writer = new PrintWriter(file)) {
+				for (int j = 0; j < 150; ++j) {
+					writer.println(random.nextLong());
+				}
+			}
+		}
+	}
+
 }
