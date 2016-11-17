ba9dc5bff4a0cce13ec6c33f491d055bc3d8956a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2016-11-17 22:07:38 +0100

    #152: Implement 'sameState' methods.

--- org.jenetics/src/test/java/org/jenetics/stat/MinMaxTest.java
+++ org.jenetics/src/test/java/org/jenetics/stat/MinMaxTest.java
@@ -176,4 +176,19 @@ public class MinMaxTest {
 		Assert.assertEquals(100, minMax.getCount());
 	}
 
+	@Test
+	public void sameState() {
+		final MinMax<Long> mm1 = MinMax.of();
+		final MinMax<Long> mm2 = MinMax.of();
+
+		final Random random = new Random();
+		for (int i = 0; i < 100; ++i) {
+			final long value = random.nextInt(1_000_000);
+			mm1.accept(value);
+			mm2.accept(value);
+
+			Assert.assertTrue(mm1.sameState(mm2));
+		}
+	}
+
 }
