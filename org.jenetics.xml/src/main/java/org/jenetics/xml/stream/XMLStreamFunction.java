9841b8f37e7e81d07b319d9e05b50b3b682d4829
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-03-28 23:39:18 +0200

    #181: Special XML stream function.

---
+++ org.jenetics.xml/src/main/java/org/jenetics/xml/stream/XMLStreamFunction.java
@@ -0,0 +1,46 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
+ */
+package org.jenetics.xml.stream;
+
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * Represents a function that accepts one argument and produces a result.
+ *
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ *
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @version !__version__!
+ * @since !__version__!
+ */
+@FunctionalInterface
+public interface XMLStreamFunction<T, R> {
+
+	/**
+	 * Applies this function to the given argument.
+	 *
+	 * @param t the function argument
+	 * @return the function result
+	 * @throws XMLStreamException if the function call fails
+	 */
+	public R apply(final T t) throws XMLStreamException;
+
+}
