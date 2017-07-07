b91bc49f325c9e195b84922673d877fcd44650e1
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-07-07 21:49:17 +0200

    #227: Move 'Program' to 'ops' package.

--- org.jenetics.programming/src/main/java/org/jenetics/programming/ops/Program.java
+++ org.jenetics.programming/src/main/java/org/jenetics/programming/ops/Program.java
@@ -17,7 +17,7 @@
  * Author:
  *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmx.at)
  */
-package org.jenetics.programming;
+package org.jenetics.programming.ops;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
