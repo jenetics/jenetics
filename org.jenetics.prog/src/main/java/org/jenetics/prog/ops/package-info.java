ac49fba953ea1bfb561e3bc52f440b4b8ac92b6d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-07-19 16:37:50 +0200

    #227: Improve Javadoc.

--- org.jenetics.prog/src/main/java/org/jenetics/prog/ops/package-info.java
+++ org.jenetics.prog/src/main/java/org/jenetics/prog/ops/package-info.java
@@ -19,6 +19,88 @@
  */
 
 /**
+ * <h2>Operations</h2>
+ *
+ * When creating a new program tree, it is not necessary to implement own
+ * instance of the {@code ProgramGene} or {@code ProgramChromosome} class. The
+ * extension point for own programs is the {@code Op} interface.
+ *
+ * <pre>{@code
+ * public interface Op<T> {
+ *     public String name();
+ *     public int arity();
+ *     public T apply(T[] args);
+ * }
+ * }</pre>
+ *
+ * <pre>{@code
+ * final Op<Double> myop = Op.of("myop", 3, v -> v[0]*v[1] + v[2]);
+ * }</pre>
+ *
+ * In the example above, a new operation with the "myop" and arity 3 is defined.
+ * Whenever the operation is evaluated, the function <em>f(x, y, z) = x*y + z</em>
+ * is executed.
+ * <p>
+ *     <b>NOTE</b>: <em>The class {@link org.jenetics.prog.ops.MathOp} in the
+ *     defines a set of mathematical standard operations/functions.</em>
+ * </p>
+ *
+ * When creating a new {@link org.jenetics.prog.ProgramChromosome} we must
+ * distinguish two different kind of operations:
+ * <ol>
+ *     <li><em>Non-terminal</em> operations have an arity greater than zero and
+ *     form their own sub-tree</li>
+ *     <li><em>Terminal</em> operations have an arity of zero and form the
+ *     leaves of a program tree.</li>
+ * </ol>
+ *
+ * There are currently three different types of non-terminal operations
+ * implemented, {@link org.jenetics.prog.ops.Var},
+ * {@link org.jenetics.prog.ops.Const} and
+ * {@link org.jenetics.prog.ops.EphemeralConst}.
+ *
+ * <h3>Var</h3>
+ *
+ * The {@code Var} operation defines a variable of a program, which are set
+ * from the program arguments.
+ *
+ * <pre>{@code
+ * final ISeq<Op<Double>> terminals = ISeq.of(
+ *     Var.of("x", 0), Var.of("y", 1), Var.of("z", 2)
+ * );
+ * }</pre>
+ *
+ * The terminal operation list in the example code above will lead to a program
+ * which takes three input parameters, <em>x</em>, <em>y</em> and <em>z</em>,
+ * with the argument indices <em>0</em>, <em>1</em> and <em>2</em>.
+ *
+ * <h3>Const</h3>
+ *
+ * The {@code Const} operation will always return the same, constant, value
+ * when evaluated.
+ *
+ * <pre>{@code
+ * final Op<Double> one = Const.of(1.0);
+ * final Op<Double> pi = Const.of("π", Math.PI);
+ * }</pre>
+ *
+ * We can create a constant operation in to flavours, with a value only and with
+ * a dedicated name. If a constant has a name, the <em>symbolic</em> name is
+ * used, instead of the value, when the program tree is printing.
+ *
+ * <h3>EphemeralConst</h3>
+ *
+ * An ephemeral constant is a special constant, which is only constant within an
+ * tree. If a new tree is created, a new constant is created, by the `
+ * {@link java.util.function.Supplier} function the ephemeral constant is created
+ * with.
+ *
+ * <pre>{@code
+ * final Op<Double> rand1 = EphemeralConst.of(Math::random);
+ * final Op<Double> rand2 = EphemeralConst.of("R", Math::random);
+ * }</pre>
+ *
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
