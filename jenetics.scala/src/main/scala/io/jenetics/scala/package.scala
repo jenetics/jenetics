92217c7036af59bdf626614dab0674eccb819fae
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-09-11 21:18:12 +0200

    #246: Add Scala wrapper project.

---
+++ jenetics.scala/src/main/scala/io/jenetics/scala/package.scala
@@ -0,0 +1,10 @@
+package io.jenetics
+
+/**
+  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+  * @since !__version__!
+  * @version !__version__!
+  */
+package object scala {
+
+}
