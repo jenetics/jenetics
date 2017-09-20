72e5883e0d3eaa15e83d08bdc0aeaed278dbcdfa
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-09-20 11:02:10 +0200

    Add module file.

---
+++ jenetics/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ * @since !__version__!
+ * @version !__version__!
+ */
+module io.jenetics.base {
+	exports io.jenetics;
+	exports io.jenetics.util;
+	exports io.jenetics.stat;
+	exports io.jenetics.engine;
+}
