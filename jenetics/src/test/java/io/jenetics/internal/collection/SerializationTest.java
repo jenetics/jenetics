fc731a8e70fd0dfee98230498ad2a0a198301a26
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-10-04 17:42:43 +0200

    #266: Write serialization test for Seq implementations.

--- jenetics/src/test/java/io/jenetics/internal/collection/SerializationTest.java
+++ jenetics/src/test/java/io/jenetics/internal/collection/SerializationTest.java
@@ -19,10 +19,36 @@
  */
 package io.jenetics.internal.collection;
 
+import static java.lang.String.format;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import io.jenetics.util.IO;
+import io.jenetics.util.MSeq;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
- * @version !__version__!
- * @since !__version__!
  */
 public class SerializationTest {
+
+
+	public static void main(final String[] args) throws IOException {
+		final File baseDir = new File("jenetics/src/test/resources/io/jenetics/collection/serialization");
+		if (!baseDir.isDirectory() && !baseDir.mkdirs()) {
+			throw new IOException("Error while creating directory " + baseDir);
+		}
+
+		for (int i = 0; i < 10; ++i) {
+			final Random random = new Random(i);
+			final MSeq<Integer> seq = MSeq.ofLength(i);
+			seq.fill(random::nextInt);
+
+			IO.object.write(seq, new File(baseDir, format("MSeq[%s].dat", i)));
+			IO.object.write(seq.toISeq(), new File(baseDir, format("ISeq[%s].dat", i)));
+		}
+	}
+
+
 }
