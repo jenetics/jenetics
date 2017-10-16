8a7d608edd72e75e7fa9d11028512088ebd042b3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-10-17 00:08:32 +0200

    #121: Add Javadoc.

--- jenetics/src/main/java/io/jenetics/MutationResult.java
+++ jenetics/src/main/java/io/jenetics/MutationResult.java
@@ -22,12 +22,20 @@ package io.jenetics;
 import static java.util.Objects.requireNonNull;
 
 import java.io.Serializable;
+import java.util.Random;
 import java.util.function.Function;
 
 /**
+ * Represents the result pair of one of the four {@code Mutator.mutate} calls.
+ *
+ * @see #mutate(Phenotype, long, double, Random)
+ * @see #mutate(Genotype, double, Random)
+ * @see #mutate(Chromosome, double, Random)
+ * @see #mutate(Gene, Random)
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
- * @version !__version__!
- * @since !__version__!
+ * @version 4.0
+ * @since 4.0
  */
 public final class MutationResult<T> implements Serializable {
 
@@ -41,22 +49,61 @@ public final class MutationResult<T> implements Serializable {
 		_mutations = mutations;
 	}
 
-	public <B> MutationResult<B> map(final Function<? super T, ? extends B> mapper) {
+	/**
+	 * Maps this mutation result to type {@code B} using the given {@code mapper}.
+	 *
+	 * @param mapper the mutation result mapper
+	 * @param <B> the new mutation result type
+	 * @return a new mapped mutation result
+	 * @throws NullPointerException if the given {@code mapper} is {@code null}
+	 */
+	public <B> MutationResult<B> map(
+		final Function<? super T, ? extends B> mapper
+	) {
+		requireNonNull(mapper);
 		return of(mapper.apply(_result), _mutations);
 	}
 
+	/**
+	 * Return the mutation result.
+	 *
+	 * @return the mutation result
+	 */
 	public T getResult() {
 		return _result;
 	}
 
+	/**
+	 * Return the number of mutations for this mutation result.
+	 *
+	 * @return the number of mutations
+	 */
 	public int getMutations() {
 		return _mutations;
 	}
 
+	/**
+	 * Create a new mutation result with the given values.
+	 *
+	 * @param result the mutation result
+	 * @param mutations the number of mutations
+	 * @param <T> the mutation result type
+	 * @return a new mutation result
+	 * @throws NullPointerException if the given mutation result is {@code null}
+	 */
 	public static <T> MutationResult<T> of(final T result, final int mutations) {
 		return new MutationResult<>(result, mutations);
 	}
 
+	/**
+	 * Create a new mutation result with the given result. The number of
+	 * mutations is set to zero.
+	 *
+	 * @param result the mutation result
+	 * @param <T> the mutation result type
+	 * @return a new mutation result
+	 * @throws NullPointerException if the given mutation result is {@code null}
+	 */
 	public static <T> MutationResult<T> of(final T result) {
 		return new MutationResult<>(result, 0);
 	}
