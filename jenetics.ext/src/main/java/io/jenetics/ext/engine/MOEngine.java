a6da6b4c5ea82b251f49480e0176133f9c184bec
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-12-01 18:36:18 +0100

    #223: Add multi-objective GA class.

---
+++ jenetics.ext/src/main/java/io/jenetics/ext/engine/MOEngine.java
@@ -0,0 +1,53 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
+package io.jenetics.ext.engine;
+
+import java.util.function.Supplier;
+
+import io.jenetics.Gene;
+import io.jenetics.engine.EvolutionInit;
+import io.jenetics.engine.EvolutionStart;
+import io.jenetics.engine.EvolutionStream;
+import io.jenetics.engine.EvolutionStreamable;
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ * @version !__version__!
+ * @since !__version__!
+ */
+public class MOEngine<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+	implements EvolutionStreamable<G, C>
+{
+
+	@Override
+	public EvolutionStream<G, C>
+	stream(final Supplier<EvolutionStart<G, C>> start) {
+		return null;
+	}
+
+	@Override
+	public EvolutionStream<G, C> stream(final EvolutionInit<G> init) {
+		return null;
+	}
+
+}
