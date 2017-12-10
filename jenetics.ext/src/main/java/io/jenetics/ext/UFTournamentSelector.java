6b1e3d2dad3586ac02c9853c28579761055e35c3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-12-10 21:30:31 +0100

    #223: Add Javadoc.

--- jenetics.ext/src/main/java/io/jenetics/ext/UFTournamentSelector.java
+++ jenetics.ext/src/main/java/io/jenetics/ext/UFTournamentSelector.java
@@ -19,10 +19,52 @@
  */
 package io.jenetics.ext;
 
+import io.jenetics.Gene;
+import io.jenetics.Optimize;
+import io.jenetics.Phenotype;
+import io.jenetics.Selector;
+import io.jenetics.util.ISeq;
+import io.jenetics.util.Seq;
+
 /**
+ * The last front selection procedure takes two arguments: a front F and the
+ * number of solutions to select k. The algorithm starts by sorting the unique
+ * fitnesses associated with the members of F in descending order of crowding
+ * distance (>dist) and stores the result in a list F (line 2). It then proceeds
+ * to fill the initially empty set of solutions S by cycling over the sorted
+ * unique fitnesses F. For each fitness, the algorithm first selects each
+ * individual sharing this fitness and puts the result in set T (line 6). If
+ * the resulting set is not empty, the procedure randomly picks one solution
+ * from T, adds it to set S and finally removes it from the front F so that
+ * individuals can be picked only once (lines 8 to 10). When all k solutions
+ * have been selected, the loop stops and set S is returned (line 14).
+ *
+ * <p>
+ *  <b>Reference:</b><em>
+ *      Félix-Antoine Fortin and Marc Parizeau. 2013. Revisiting the NSGA-II
+ *      crowding-distance computation. In Proceedings of the 15th annual
+ *      conference on Genetic and evolutionary computation (GECCO '13),
+ *      Christian Blum (Ed.). ACM, New York, NY, USA, 623-630.
+ *      DOI=<a href="http://dx.doi.org/10.1145/2463372.2463456">
+ *          10.1145/2463372.2463456</a></em>
+ *
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
  */
-public class UFTournamentSelector {
+public class UFTournamentSelector<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+	implements Selector<G, C>
+{
+	@Override
+	public ISeq<Phenotype<G, C>> select(
+		final Seq<Phenotype<G, C>> population,
+		final int count,
+		final Optimize opt
+	) {
+		return null;
+	}
 }
