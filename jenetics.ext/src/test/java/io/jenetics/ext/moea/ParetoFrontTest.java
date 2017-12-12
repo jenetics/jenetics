bd3badc1d63aea0e3d6d02c9d4c1fc3dc4503f52
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-12-13 00:10:30 +0100

    #223: Move MOEA classes to 'moea' package.

--- jenetics.ext/src/test/java/io/jenetics/ext/moea/ParetoFrontTest.java
+++ jenetics.ext/src/test/java/io/jenetics/ext/moea/ParetoFrontTest.java
@@ -17,7 +17,7 @@
  * Author:
  *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
  */
-package io.jenetics.ext.util;
+package io.jenetics.ext.moea;
 
 import java.util.ArrayList;
 import java.util.HashSet;
