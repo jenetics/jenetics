648ebbafa6e5221d6631931233a782e78e4bbd17
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-12-20 13:48:19 +0100

    #298: #298: Making concrete 'Const' class serializable.

--- jenetics.prog/src/main/java/io/jenetics/prog/op/Const.java
+++ jenetics.prog/src/main/java/io/jenetics/prog/op/Const.java
@@ -35,10 +35,12 @@ import java.util.Objects;
  * }</pre>
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
- * @version 3.9
+ * @version !__version__!
  * @since 3.9
  */
 public final class Const<T> implements Op<T>, Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private final String _name;
 	private final T _const;
