c94e646c2210ff72b0f805990ff90105b941dec6
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2017-12-21 15:29:48 +0100

    #297: Fixe JMH compile error.

--- jenetics/src/jmh/java/io/jenetics/ProbabilitySelectorIndexOfPerf.java
+++ jenetics/src/jmh/java/io/jenetics/ProbabilitySelectorIndexOfPerf.java
@@ -20,7 +20,7 @@
 package io.jenetics;
 
 import static io.jenetics.ProbabilitySelector.incremental;
-import static io.jenetics.internal.math.arithmetic.normalize;
+import static io.jenetics.internal.math.base.normalize;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
