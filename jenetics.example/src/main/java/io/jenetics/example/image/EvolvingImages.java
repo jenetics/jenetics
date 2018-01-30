e401cb2bee3a18a656453817a2759d9c66d5c038
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2018-01-30 21:15:11 +0100

    #294: Code simplification.

--- jenetics.example/src/main/java/io/jenetics/example/image/EvolvingImages.java
+++ jenetics.example/src/main/java/io/jenetics/example/image/EvolvingImages.java
@@ -506,15 +506,20 @@ public class EvolvingImages extends JFrame {
 					break;
 				}
 			}
-		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(EvolvingImages.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(EvolvingImages.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(EvolvingImages.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(EvolvingImages.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (ClassNotFoundException | 
+				InstantiationException | 
+				IllegalAccessException | 
+				javax.swing.UnsupportedLookAndFeelException ex) 
+		{
+			java.util.logging.Logger
+					.getLogger(EvolvingImages.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
 		}
+		//</editor-fold>
+		//</editor-fold>
+		//</editor-fold>
+		//</editor-fold>
+		
 		//</editor-fold>
 		//</editor-fold>
 		//</editor-fold>
