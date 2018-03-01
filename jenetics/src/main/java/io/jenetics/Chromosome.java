9ca4c6242c84a401fdd56bd0421dd14318a38110
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2018-03-01 18:35:11 +0100

    Add implementation requirements in Javadoc.

--- jenetics/src/main/java/io/jenetics/Chromosome.java
+++ jenetics/src/main/java/io/jenetics/Chromosome.java
@@ -30,7 +30,7 @@ import io.jenetics.util.Verifiable;
  * method for creating new, random chromosome instances of the same type and the
  * same constraint.
  *
- * @apiNote
+ * @implSpec
  * Implementations of the {@code Chromosome} interface must be <em>immutable</em>
  * and guarantee an efficient random access ({@code O(1)}) to the genes. A
  * {@code Chromosome} must contains at least one {@code Gene}.
