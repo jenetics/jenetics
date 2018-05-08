d80edffc5827c401d97b2cfd79bcdb5e1bc12eab
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2018-05-08 18:58:38 +0200

    #341: Fix 'bit.increment(byte[])' method.
    The carry state is calculated correctly now.

--- jenetics/src/main/java/io/jenetics/internal/util/bit.java
+++ jenetics/src/main/java/io/jenetics/internal/util/bit.java
@@ -534,6 +534,15 @@ public final class bit {
 			(data[3] & 255);
 	}
 
+	public static byte[] toBytes(final int value) {
+		final byte[] bytes = new byte[4];
+		bytes[0] = (byte)(value >>> 24);
+		bytes[1] = (byte)(value >>> 16);
+		bytes[2] = (byte)(value >>>  8);
+		bytes[3] = (byte) value;
+		return bytes;
+	}
+
 	public static long toLong(final byte[] data) {
 		return
 			((long)data[0] << 56) +
