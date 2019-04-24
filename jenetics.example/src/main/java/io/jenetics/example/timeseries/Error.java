42a7fad5a36de43827153b21594558c39ef5edbc
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-04-24 21:27:46 +0200

    #302: Add Javadoc.

--- jenetics.example/src/main/java/io/jenetics/example/timeseries/Error.java
+++ jenetics.example/src/main/java/io/jenetics/example/timeseries/Error.java
@@ -19,11 +19,20 @@
  */
 package io.jenetics.example.timeseries;
 
+import static java.util.Objects.requireNonNull;
+
 import io.jenetics.ext.util.Tree;
 
 import io.jenetics.prog.op.Op;
 
 /**
+ * This function calculates the <em>overall</em> error of a given program tree.
+ * The error is calculated from the {@link LossFunction} and, if desired, the
+ * program {@link Complexity}.
+ *
+ * @see LossFunction
+ * @see Complexity
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
@@ -31,6 +40,18 @@ import io.jenetics.prog.op.Op;
 @FunctionalInterface
 public interface Error {
 
+	/**
+	 * Calculates the <em>overall</em> error of a given program tree. The error
+	 * is calculated from the {@link LossFunction} and, if desired, the program
+	 * {@link Complexity}.
+	 *
+	 * @param program the program tree which calculated the {@code calculated}
+	 *        values
+	 * @param calculated the calculated function values
+	 * @param expected the expected function values
+	 * @return the overall program error
+	 * @throws NullPointerException if one of the arguments is {@code null}
+	 */
 	public double apply(
 		final Tree<Op<Double>, ?> program,
 		final double[] calculated,
@@ -38,11 +59,38 @@ public interface Error {
 	);
 
 
+	/**
+	 * Creates an error function which only uses the given {@code loss} function
+	 * for calculating the program error
+	 *
+	 * @param loss the loss function to use for calculating the program error
+	 * @return an error function which uses the loss function for error
+	 *         calculation
+	 * @throws NullPointerException if the given {@code loss} function is
+	 *         {@code null}
+	 */
 	public static Error of(final LossFunction loss) {
+		requireNonNull(loss);
+
 		return (p, c, e) -> loss.apply(c, e);
 	}
 
+	/**
+	 * Creates an error function by combining the given {@code loss} function
+	 * and program {@code complexity}. The loss function and program complexity
+	 * is combined in the following way: {@code error = loss + loss*complexity}.
+	 * The complexity function penalizes programs which grows to big.
+	 *
+	 * @param loss the loss function
+ 	 * @param complexity the program complexity measure
+	 * @return a new error function by combining the given loss and complexity
+	 *         function
+	 * @throws NullPointerException if one of the functions is {@code null}
+	 */
 	public static Error of(final LossFunction loss, final Complexity complexity) {
+		requireNonNull(loss);
+		requireNonNull(complexity);
+
 		return (p, c, e) -> {
 			final double lss = loss.apply(c, e);
 			final double cpx = complexity.apply(p);
