86aad57e7205380d1e02927610605226d9b1a507
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-05-28 21:01:04 +0200

    #499: Implement 'PartialShuffleMutator' class.

--- jenetics.ext/src/main/java/io/jenetics/ext/PartialShuffleMutator.java
+++ jenetics.ext/src/main/java/io/jenetics/ext/PartialShuffleMutator.java
@@ -19,10 +19,82 @@
  */
 package io.jenetics.ext;
 
+import java.util.Random;
+
+import io.jenetics.Chromosome;
+import io.jenetics.Gene;
+import io.jenetics.Mutator;
+import io.jenetics.MutatorResult;
+import io.jenetics.internal.math.probability;
+import io.jenetics.util.MSeq;
+
 /**
+ * The Partial Transfer Shuffle (PSM), change part of the order of the genes in
+ * the chromosome.
+ *
+ * @see <a href="https://arxiv.org/ftp/arxiv/papers/1203/1203.3099.pdf">
+ *     Analyzing the Performance of Mutation Operators to Solve the Travelling
+ *     Salesman Problem</a>
+ * @see <a href="https://www.researchgate.net/publication/282732991_A_New_Mutation_Operator_for_Solving_an_NP-Complete_Problem_Travelling_Salesman_Problem">
+ *     A New Mutation Operator for Solving an NP-Complete Problem: Travelling Salesman Problem</a>
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
  */
-public class PartialShuffleMutator {
+public class PartialShuffleMutator<
+	G extends Gene<?, G>,
+	C extends Comparable<? super C>
+>
+	extends Mutator<G, C>
+{
+
+	/**
+	 * Create a new <em>PSM</em> with the given mutation probability.
+	 *
+	 * @param probability the mutation probability
+	 */
+	public PartialShuffleMutator(final double probability) {
+		super(probability);
+	}
+
+	/**
+	 * Create a new <em>PSM</em> with the {@link #DEFAULT_ALTER_PROBABILITY}.
+	 */
+	public PartialShuffleMutator() {
+	}
+
+	/**
+	 * Partially changes the gene order of the given chromosome.
+	 *
+	 * @param chromosome the chromosome to mutate
+	 * @param p the mutation probability for the underlying genetic objects
+	 * @param random the random engine used for the genotype mutation
+	 * @return the mutation result
+	 */
+	@Override
+	protected MutatorResult<Chromosome<G>> mutate(
+		final Chromosome<G> chromosome,
+		final double p,
+		final Random random
+	) {
+		int mutations = 0;
+		MSeq<G> genes = null;
+
+		for (int i = 0; i < chromosome.length(); ++i) {
+			final int P = probability.toInt(p);
+			if (random.nextInt() < P) {
+				if (genes == null) genes = chromosome.toSeq().copy();
+
+				final int j = random.nextInt(chromosome.length());
+				genes.swap(i, j);
+				++mutations;
+			}
+		}
+
+		return genes != null
+			? MutatorResult.of(chromosome.newInstance(genes.toISeq()), mutations)
+			: MutatorResult.of(chromosome);
+	}
+
 }
