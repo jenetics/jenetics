a63bb5be20c893e8af1b332aef0bc4876810a5b4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-05-29 19:08:04 +0200

    #500: Implement mutator.

--- jenetics.ext/src/main/java/io/jenetics/ext/ReverseSequenceMutator.java
+++ jenetics.ext/src/main/java/io/jenetics/ext/ReverseSequenceMutator.java
@@ -19,10 +19,24 @@
  */
 package io.jenetics.ext;
 
+import java.util.Random;
+
+import io.jenetics.AbstractAlterer;
+import io.jenetics.Chromosome;
 import io.jenetics.Gene;
 import io.jenetics.Mutator;
+import io.jenetics.MutatorResult;
+import io.jenetics.internal.math.comb;
+import io.jenetics.util.MSeq;
 
 /**
+ * The reverse sequence mutation, two positions i and j are randomly chosen The
+ * gene order in a chromosome will then be reversed between this two points.
+ * This mutation operator can also be used for combinatorial problems, where no
+ * duplicated genes within a chromosome are allowed, e.g. for the TSP.
+ *
+ * @see io.jenetics.SwapMutator
+ *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
  * @since !__version__!
@@ -33,4 +47,47 @@ public class ReverseSequenceMutator<
 >
 	extends Mutator<G, C>
 {
+
+	/**
+	 * Constructs an alterer with a given recombination probability.
+	 *
+	 * @param probability the crossover probability.
+	 * @throws IllegalArgumentException if the {@code probability} is not in the
+	 *          valid range of {@code [0, 1]}.
+	 */
+	public ReverseSequenceMutator(final double probability) {
+		super(probability);
+	}
+
+	/**
+	 * Default constructor, with default mutation probability
+	 * ({@link AbstractAlterer#DEFAULT_ALTER_PROBABILITY}).
+	 */
+	public ReverseSequenceMutator() {
+		this(DEFAULT_ALTER_PROBABILITY);
+	}
+
+	@Override
+	protected MutatorResult<Chromosome<G>> mutate(
+		final Chromosome<G> chromosome,
+		final double p,
+		final Random random
+	) {
+		final MutatorResult<Chromosome<G>> result;
+		if (chromosome.length() > 1) {
+			final int[] points = comb.subset(chromosome.length(), 2);
+			final MSeq<G> genes = chromosome.toSeq().copy();
+			genes.subSeq(points[0], points[1]).reverse();
+
+			result = MutatorResult.of(
+				chromosome.newInstance(genes.toISeq()),
+				points[1] - points[0] - 1
+			);
+		} else {
+			result = MutatorResult.of(chromosome);
+		}
+
+		return result;
+	}
+
 }
