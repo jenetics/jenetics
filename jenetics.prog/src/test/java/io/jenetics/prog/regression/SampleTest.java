bcb024327242f32d846a3c0c69bbe6ecfc20b8ae
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-07-29 21:56:35 +0200

    #550: Fix 'Sample.argAt(int)' index check.

--- jenetics.prog/src/test/java/io/jenetics/prog/regression/SampleTest.java
+++ jenetics.prog/src/test/java/io/jenetics/prog/regression/SampleTest.java
@@ -19,8 +19,89 @@
  */
 package io.jenetics.prog.regression;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  */
 public class SampleTest {
+
+	@Test(dataProvider = "aritySamples")
+	public void arity(final Sample<?> sample, final int arity) {
+		Assert.assertEquals(sample.arity(), arity);
+	}
+
+	@DataProvider
+	public Object[][] aritySamples() {
+		return new Object[][] {
+			{Sample.ofDouble(1, 2), 1},
+			{Sample.ofDouble(1, 2, 3), 2},
+			{Sample.ofDouble(1, 2, 3, 4), 3},
+			{Sample.of(new Integer[]{1, 2}), 1},
+			{Sample.of(new Integer[]{1, 2, 3}), 2},
+			{Sample.of(new Integer[]{1, 2, 3, 4}), 3},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5}), 4},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5, 6}), 5}
+		};
+	}
+
+	@Test(dataProvider = "resultSamples")
+	public void result(final Sample<?> sample, final Object result) {
+		Assert.assertEquals(sample.result(), result);
+	}
+
+	@DataProvider
+	public Object[][] resultSamples() {
+		return new Object[][] {
+			{Sample.ofDouble(1, 2), 2.0},
+			{Sample.ofDouble(1, 2, 3), 3.0},
+			{Sample.ofDouble(1, 2, 3, 4), 4.0},
+			{Sample.of(new Integer[]{1, 2}), 2},
+			{Sample.of(new Integer[]{1, 2, 3}), 3},
+			{Sample.of(new Integer[]{1, 2, 3, 4}), 4},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5}), 5},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5, 6}), 6}
+		};
+	}
+
+	@Test(dataProvider = "argAtSamples")
+	public void argAt(final Sample<?> sample, final Object[] result) {
+		Assert.assertEquals(sample.arity(), result.length);
+		for (int i = 0; i < sample.arity(); ++i) {
+			Assert.assertEquals(sample.argAt(i), result[i]);
+		}
+	}
+
+	@Test(
+		dataProvider = "argAtSamples",
+		expectedExceptions = ArrayIndexOutOfBoundsException.class
+	)
+	public void argAtIndexToSmall(final Sample<?> sample, final Object[] result) {
+		sample.argAt(-1);
+	}
+
+	@Test(
+		dataProvider = "argAtSamples",
+		expectedExceptions = ArrayIndexOutOfBoundsException.class
+	)
+	public void argAtIndexToBig(final Sample<?> sample, final Object[] result) {
+		sample.argAt(result.length);
+	}
+
+	@DataProvider
+	public Object[][] argAtSamples() {
+		return new Object[][] {
+			{Sample.ofDouble(1, 2), new Double[]{1.0}},
+			{Sample.ofDouble(1, 2, 3), new Double[]{1.0, 2.0}},
+			{Sample.ofDouble(1, 2, 3, 4), new Double[]{1.0, 2.0, 3.0}},
+			{Sample.of(new Integer[]{1, 2}), new Integer[]{1}},
+			{Sample.of(new Integer[]{1, 2, 3}), new Integer[]{1, 2}},
+			{Sample.of(new Integer[]{1, 2, 3, 4}), new Integer[]{1, 2, 3}},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5}), new Integer[]{1, 2, 3, 4}},
+			{Sample.of(new Integer[]{1, 2, 3, 4, 5, 6}), new Integer[]{1, 2, 3, 4, 5}}
+		};
+	}
+
 }
