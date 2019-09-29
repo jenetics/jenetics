72735235eee6868f25c140f335879978f0af5524
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-09-29 20:59:45 +0200

    #579: Improve Javadoc.

--- jenetics/src/main/java/io/jenetics/util/Buffer.java
+++ jenetics/src/main/java/io/jenetics/util/Buffer.java
@@ -22,6 +22,7 @@ package io.jenetics.util;
 import static java.lang.Math.min;
 import static java.lang.System.arraycopy;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.function.IntFunction;
 
@@ -30,7 +31,18 @@ import io.jenetics.internal.collection.ArrayMSeq;
 import io.jenetics.internal.collection.ObjectStore;
 
 /**
- * Ring buffer implementation.
+ * This class is a bounded buffer, which can store only a given amount of
+ * elements. If the buffer is full, it starts <em>overwriting</em> previously
+ * inserted elements at the beginning of the buffer. A <em>full</em> buffer
+ * neither blocks the insertion of new elements, nor does it throw an exception.
+ * <p>
+ * If you want to access the elements of the buffer, you have to take a
+ * <em>snapshot</em> of the current content. This can be done with the
+ * {@link #toArray()}, {@link #toArray(IntFunction)} and {@link #toSeq()} methods.
+ *
+ * @implNote
+ * This class is not thread-safe. If two threads accesses the buffer
+ * concurrently it must be <em>synchronized</em> externally.
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @version !__version__!
@@ -46,13 +58,16 @@ public final class Buffer<T> implements Iterable<T> {
 	 * Create a new ring buffer with the given {@code capacity}.
 	 *
 	 * @param capacity the buffer capacity
+	 * @throws NegativeArraySizeException if the the given {@code capacity} is
+	 *         negative
 	 */
 	private Buffer(final int capacity) {
 		_buffer = new Object[capacity];
 	}
 
 	/**
-	 * Return the capacity of {@code this} buffer.
+	 * Return the capacity of {@code this} buffer. This is the number of elements
+	 * the buffer is able to store.
 	 *
 	 * @return the capacity of {@code this} buffer
 	 */
@@ -61,7 +76,7 @@ public final class Buffer<T> implements Iterable<T> {
 	}
 
 	/**
-	 * Return the current buffer size.
+	 * Return the current number of elements {@code this} buffer holds.
 	 *
 	 * @return the current buffer size
 	 */
@@ -70,7 +85,7 @@ public final class Buffer<T> implements Iterable<T> {
 	}
 
 	/**
-	 * Add a new element to te ring buffer.
+	 * Add a new element to the buffer.
 	 *
 	 * @param value the value to add
 	 */
@@ -86,9 +101,10 @@ public final class Buffer<T> implements Iterable<T> {
 	}
 
 	/**
-	 * Add the given {@code values} to {@code this} buffer.
+	 * Add the given {@code values} to the buffer.
 	 *
 	 * @param values the values to add
+	 * @throws NullPointerException if the given parameter is {@code null}
 	 */
 	@SafeVarargs
 	public final void addAll(final T... values) {
@@ -99,10 +115,14 @@ public final class Buffer<T> implements Iterable<T> {
 	 * Add the given values to the ring buffer.
 	 *
 	 * @param values the values being added to the ring buffer
+	 * @throws NullPointerException if the given parameter is {@code null}
 	 */
 	public void addAll(final Iterable<? extends T> values) {
 		if (values instanceof Buffer) {
 			final Object[] array = ((Buffer<?>)values).toArray();
+			addAll(array, 0, array.length);
+		} else if (values instanceof Collection) {
+			final Object[] array = ((Collection<?>)values).toArray();
 			addAll(array, 0, array.length);
 		} else {
 			for (T value : values) {
@@ -150,6 +170,12 @@ public final class Buffer<T> implements Iterable<T> {
 		}
 	}
 
+	/**
+	 * Return an iterator from an element snapshot, taken with the {@link #toSeq()}
+	 * method.
+	 *
+	 * @return a new iterator from a snapshot of the current elements
+	 */
 	@Override
 	public Iterator<T> iterator() {
 		return toSeq().iterator();
@@ -173,6 +199,7 @@ public final class Buffer<T> implements Iterable<T> {
 	 * @return the buffer snapshot
 	 * @throws ArrayStoreException if an element in the {@code value} array
 	 *         could not be stored into the dest array because of a type mismatch
+	 * @throws NullPointerException if the given {@code generator} is {@code null}
 	 */
 	public <A> A[] toArray(final IntFunction<A[]> generator) {
 		final A[] result = generator.apply(_size);
@@ -200,12 +227,19 @@ public final class Buffer<T> implements Iterable<T> {
 		return new ArrayMSeq<T>(Array.of(ObjectStore.of(toArray()))).toISeq();
 	}
 
+	@Override
+	public String toString() {
+		return toSeq().toString();
+	}
+
 	/**
 	 * Create a new ring buffer with the given {@code capacity}.
 	 *
 	 * @param <T> the element type
 	 * @param capacity the buffer capacity
 	 * @return a new ring buffer with the given capacity
+	 * @throws NegativeArraySizeException if the the given {@code capacity} is
+	 *         negative
 	 */
 	public static <T> Buffer<T> ofCapacity(final int capacity) {
 		return new Buffer<T>(capacity);
