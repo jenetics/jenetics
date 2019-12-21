2fa09153ec7c0ce3f4943dd6881f479c04c6c910
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2019-12-21 23:40:01 +0100

    #623: Implement 'Table' class.

--- jenetics.tool/src/main/java/io/jenetics/tool/measurement/Table.java
+++ jenetics.tool/src/main/java/io/jenetics/tool/measurement/Table.java
@@ -1,15 +1,70 @@
 package io.jenetics.tool.measurement;
 
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Table {
+public final class Table {
 
-	private final List<String> _header = new ArrayList<>();
-	private final List<String> _rows = new ArrayList<>();
+	private final List<String> _header;
+	private final List<String[]> _rows = new ArrayList<>();
+
+	private Table(final List<String> header) {
+		_header = Collections.unmodifiableList(header);
+	}
 
 	public List<String> header() {
 		return _header;
+	}
+
+	public List<String[]> rows() {
+		return Collections.unmodifiableList(_rows);
+	}
+
+	public void add(final String... row) {
+		if (row.length != _header.size()) {
+			throw new IllegalArgumentException(format(
+				"Invalid number of columns. Expected %d, but got %d.",
+				_header.size(), row.length
+			));
+		}
+		_rows.add(row);
+	}
+
+	public void write(final OutputStream out) throws IOException {
+		final OutputStreamWriter osw = new OutputStreamWriter(out);
+		final BufferedWriter writer = new BufferedWriter(osw);
+
+		writer.write(toString(_header));
+		writer.write("\n");
+
+		for (String[] row : _rows) {
+			writer.write(toString(asList(row)));
+			writer.write("\n");
+		}
+
+		writer.flush();
+	}
+
+	private static String toString(final List<String> row) {
+		return String.join(", ", row);
+	}
+
+	public static Table read(final InputStream in) throws IOException {
+		return null;
+	}
+
+
+	public static Table of(final String... header) {
+		return new Table(asList(header));
 	}
 
 }
