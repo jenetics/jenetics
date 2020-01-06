80da0e395928517b42b604323fc3bf78b1c72085
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-01-06 16:07:58 +0100

    #623: Some table experiments.

--- jenetics.tool/src/main/java/io/jenetics/tool/measurement/Table.java
+++ jenetics.tool/src/main/java/io/jenetics/tool/measurement/Table.java
@@ -1,70 +1,190 @@
 package io.jenetics.tool.measurement;
 
-import static java.lang.String.format;
-import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public final class Table {
+import io.jenetics.tool.measurement.Table.Row;
 
-	private final List<String> _header;
-	private final List<String[]> _rows = new ArrayList<>();
+/**
+ * Represents a two-dimensional collection of elements of type {@code T}.
+ *
+ * @param <T> the element type
+ */
+public interface Table<T> extends Iterable<Table.Row<T>> {
 
-	private Table(final List<String> header) {
-		_header = Collections.unmodifiableList(header);
+	/**
+	 * Represents a table row.
+	 *
+	 * @param <T> the element type
+	 */
+	public static interface Row<T> extends Iterable<T> {
+		public int size();
+		public T get(final int index);
+
+		@Override
+		public default Iterator<T> iterator() {
+			return new Iterator<T>() {
+				private int _cursor = 0;
+				private int _lastElement = -1;
+
+				@Override
+				public boolean hasNext() {
+					return _cursor != size();
+				}
+
+				@Override
+				public T next() {
+					final int i = _cursor;
+					if (_cursor >= size()) {
+						throw new NoSuchElementException();
+					}
+
+					_cursor = i + 1;
+					return get(_lastElement = i);
+				}
+			};
+		}
+
+		public default Object[] toArray() {
+			final Object[] array = new Object[size()];
+			for (int i = 0; i < array.length; ++i) {
+				array[i] = get(i);
+			}
+			return array;
+		}
+
+		@SafeVarargs
+		static <T> Row<T> of(final T... row) {
+			requireNonNull(row);
+			if (row.length == 0) {
+				throw new IllegalArgumentException();
+			}
+
+			return new ArrayRow<>(row);
+		}
+
 	}
 
+	/**
+	 * Return the (optional) header line. If the returned list is empty, no
+	 * header line has been defined.
+	 *
+	 * @return the header line, if available
+	 */
+	public List<String> header();
+
+	public int size();
+
+	public Row<T> get(final int index);
+
+	@Override
+	public default Iterator<Row<T>> iterator() {
+		return new Iterator<Row<T>>() {
+			private int _cursor = 0;
+			private int _lastElement = -1;
+
+			@Override
+			public boolean hasNext() {
+				return _cursor != size();
+			}
+
+			@Override
+			public Row<T> next() {
+				final int i = _cursor;
+				if (_cursor >= size()) {
+					throw new NoSuchElementException();
+				}
+
+				_cursor = i + 1;
+				return get(_lastElement = i);
+			}
+		};
+	}
+
+	public void add(final Row<T> row);
+
+
+//	public void write(final OutputStream out) throws IOException {
+//		final OutputStreamWriter osw = new OutputStreamWriter(out);
+//		final BufferedWriter writer = new BufferedWriter(osw);
+//
+//		writer.write(toString(_header));
+//		writer.write("\n");
+//
+//		for (String[] row : _rows) {
+//			writer.write(toString(asList(row)));
+//			writer.write("\n");
+//		}
+//
+//		writer.flush();
+//	}
+//
+//	private static String toString(final List<String> row) {
+//		return String.join(", ", row);
+//	}
+//
+//	public static Table read(final InputStream in) throws IOException {
+//		return null;
+//	}
+//
+//
+//	public static Table of(final String... header) {
+//		return new Table(asList(header));
+//	}
+
+}
+
+final class ArrayTable<T> implements Table<T> {
+
+	private final List<String> _header;
+	private final List<Row<T>> _rows = new ArrayList<>();
+
+	ArrayTable(final List<String> header) {
+		_header = unmodifiableList(new ArrayList<>(header));
+	}
+
+	@Override
 	public List<String> header() {
 		return _header;
 	}
 
-	public List<String[]> rows() {
-		return Collections.unmodifiableList(_rows);
+	@Override
+	public int size() {
+		return _rows.size();
 	}
 
-	public void add(final String... row) {
-		if (row.length != _header.size()) {
-			throw new IllegalArgumentException(format(
-				"Invalid number of columns. Expected %d, but got %d.",
-				_header.size(), row.length
-			));
-		}
-		_rows.add(row);
+	@Override
+	public Row<T> get(final int index) {
+		return _rows.get(index);
 	}
 
-	public void write(final OutputStream out) throws IOException {
-		final OutputStreamWriter osw = new OutputStreamWriter(out);
-		final BufferedWriter writer = new BufferedWriter(osw);
+	@Override
+	public void add(final Row<T> row) {
+		_rows.add(new ArrayRow<>(row.toArray()));
+	}
+}
 
-		writer.write(toString(_header));
-		writer.write("\n");
+final class ArrayRow<T> implements Row<T> {
+	private final Object[] _row;
 
-		for (String[] row : _rows) {
-			writer.write(toString(asList(row)));
-			writer.write("\n");
-		}
-
-		writer.flush();
+	ArrayRow(final Object[] row) {
+		_row = requireNonNull(row);
 	}
 
-	private static String toString(final List<String> row) {
-		return String.join(", ", row);
+	@Override
+	public int size() {
+		return _row.length;
 	}
 
-	public static Table read(final InputStream in) throws IOException {
-		return null;
-	}
-
-
-	public static Table of(final String... header) {
-		return new Table(asList(header));
+	@Override
+	@SuppressWarnings("unchecked")
+	public T get(final int index) {
+		return (T)_row[index];
 	}
 
 }
--- jenetics.tool/src/test/java/io/jenetics/tool/measurement/TableTest.java
+++ jenetics.tool/src/test/java/io/jenetics/tool/measurement/TableTest.java
@@ -7,24 +7,24 @@ import org.testng.annotations.Test;
 
 public class TableTest {
 
-	@Test
-	public void write() throws IOException {
-		final Table table = Table.of("col1", "col2", "col3");
-		table.add("1", "2", "3");
-		table.add("1", "2", "3");
-		table.add("1", "2", "3");
-
-		table.write(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {
-				System.out.write(b);
-			}
-
-			@Override
-			public void close() throws IOException {
-				System.out.println("CLOSE");
-			}
-		});
-	}
+//	@Test
+//	public void write() throws IOException {
+//		final Table table = Table.of("col1", "col2", "col3");
+//		table.add("1", "2", "3");
+//		table.add("1", "2", "3");
+//		table.add("1", "2", "3");
+//
+//		table.write(new OutputStream() {
+//			@Override
+//			public void write(int b) throws IOException {
+//				System.out.write(b);
+//			}
+//
+//			@Override
+//			public void close() throws IOException {
+//				System.out.println("CLOSE");
+//			}
+//		});
+//	}
 
 }
