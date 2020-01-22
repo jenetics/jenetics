91b1c27da7f51a9bb94d2243c77b447bf7995210
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-01-22 17:56:31 +0100

    Update version strings.

--- jenetics/src/main/java/io/jenetics/Chromosome.java
+++ jenetics/src/main/java/io/jenetics/Chromosome.java
@@ -40,7 +40,7 @@ import io.jenetics.util.Verifiable;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version !__version__!
+ * @version 5.2
  */
 public interface Chromosome<G extends Gene<?, G>>
 	extends
--- jenetics/src/main/java/io/jenetics/Genotype.java
+++ jenetics/src/main/java/io/jenetics/Genotype.java
@@ -69,7 +69,7 @@ import io.jenetics.util.Verifiable;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version !__version__!
+ * @version 5.2
  */
 public final class Genotype<G extends Gene<?, G>>
 	implements
