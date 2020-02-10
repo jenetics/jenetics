9465e730a8c2b0fe407958b29bf26dc7af5f1ad5
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-02-10 21:42:54 +0100

    'BaseSeq' extends 'RandomAccess'.

--- jenetics/src/main/java/io/jenetics/util/BaseSeq.java
+++ jenetics/src/main/java/io/jenetics/util/BaseSeq.java
@@ -21,6 +21,7 @@ package io.jenetics.util;
 
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.RandomAccess;
 import java.util.Spliterator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -35,7 +36,7 @@ import io.jenetics.internal.collection.BaseSeqSpliterator;
  * @since 5.2
  * @version 5.2
  */
-public interface BaseSeq<T> extends Iterable<T> {
+public interface BaseSeq<T> extends Iterable<T>, RandomAccess {
 
 	/**
 	 * Return the value at the given {@code index}.
