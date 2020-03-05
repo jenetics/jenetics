f5004660cb9db90a34f60dd97da65d5185a0eb73
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-03-05 21:08:19 +0100

    #665: Implement 'CombineAlterer'.

--- jenetics/src/main/java/io/jenetics/MeanAlterer.java
+++ jenetics/src/main/java/io/jenetics/MeanAlterer.java
@@ -19,30 +19,24 @@
  */
 package io.jenetics;
 
-import static java.lang.Math.min;
-import static java.lang.String.format;
-
-import java.util.Random;
-
-import io.jenetics.util.BaseSeq;
-import io.jenetics.util.MSeq;
 import io.jenetics.util.Mean;
-import io.jenetics.util.RandomRegistry;
 
 /**
+ * Alters a chromosome by replacing two genes by its mean value.
+ *
  * <p>
- * The order ({@link #order()}) of this Recombination implementation is two.
+ * The order ({@link #order()}) of this recombination implementation is two.
  * </p>
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 5.2
+ * @version !__version__!
  */
 public class MeanAlterer<
 	G extends Gene<?, G> & Mean<G>,
 	C extends Comparable<? super C>
 >
-	extends Recombinator<G, C>
+	extends CombineAlterer<G, C>
 {
 
 	/**
@@ -53,7 +47,7 @@ public class MeanAlterer<
 	 *         valid range of {@code [0, 1]}.
 	 */
 	public MeanAlterer(final double probability) {
-		super(probability, 2);
+		super(Mean::mean, probability);
 	}
 
 	/**
@@ -61,47 +55,6 @@ public class MeanAlterer<
 	 */
 	public MeanAlterer() {
 		this(0.05);
-	}
-
-	@Override
-	protected int recombine(
-		final MSeq<Phenotype<G, C>> population,
-		final int[] individuals,
-		final long generation
-	) {
-		final Random random = RandomRegistry.random();
-
-		final Phenotype<G, C> pt1 = population.get(individuals[0]);
-		final Phenotype<G, C> pt2 = population.get(individuals[1]);
-		final Genotype<G> gt1 = pt1.genotype();
-		final Genotype<G> gt2 = pt2.genotype();
-
-		//Choosing the Chromosome index for crossover.
-		final int cindex = random.nextInt(min(gt1.length(), gt2.length()));
-
-		final MSeq<Chromosome<G>> c1 = MSeq.of(gt1);
-
-		// Calculate the mean value of the gene array.
-		final MSeq<G> mean = mean(c1.get(cindex), gt2.get(cindex));
-
-		c1.set(cindex, c1.get(cindex).newInstance(mean.toISeq()));
-		population.set(individuals[0], Phenotype.of(Genotype.of(c1), generation));
-
-		return 1;
-	}
-
-	private static <G extends Gene<?, G> & Mean<G>>
-	MSeq<G> mean(final BaseSeq<G> a, final BaseSeq<G> b) {
-		final MSeq<G> result = MSeq.ofLength(a.length());
-		for (int i = a.length(); --i >= 0;) {
-			result.set(i, a.get(i).mean(b.get(i)));
-		}
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return format("%s[p=%f]", getClass().getSimpleName(), _probability);
 	}
 
 }
