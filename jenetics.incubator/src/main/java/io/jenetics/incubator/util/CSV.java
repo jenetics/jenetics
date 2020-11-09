b14b6fe3e7e7b27e4d07bfb87680f3320c75dc15
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-11-09 23:28:16 +0100

    #302: Reorder methods.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/util/CSV.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/util/CSV.java
@@ -55,6 +55,163 @@ public final class CSV {
 	}
 
 	/**
+	 * Splits a given CSV row into it's columns. It supports CSV records defined
+	 * in <a href="https://tools.ietf.org/html/rfc4180">RFC-4180</a>.
+	 *
+	 * @see <a href="https://tools.ietf.org/html/rfc4180">RFC-4180</a>
+	 * @see #joinCols(Iterable)
+	 *
+	 * @param line the CSV {@code row} to split
+	 * @return the split columns of the given CSV {@code row}
+	 * @throws IllegalArgumentException if the given {@code roes} isn't a valid
+	 *         CSV row
+	 * @throws NullPointerException if the given {@code row} is {@code null}
+	 */
+	public static List<String> splitLine(final CharSequence line) {
+		final List<String> columns = new ArrayList<>();
+
+		boolean quoted = false;
+		boolean escaped = false;
+
+		final var col = new StringBuilder();
+
+		final var tokens = tokenize(line);
+
+		for (int i = 0; i < tokens.size(); ++i) {
+			final var previous = i > 0 ? tokens.get(i - 1) : null;
+			final var current = tokens.get(i);
+			final var next = i + 1 < tokens.size() ? tokens.get(i + 1) : null;
+
+			switch (current) {
+				case QUOTE_STR:
+					if (quoted) {
+						if (QUOTE_STR.equals(next) && !escaped) {
+							escaped = true;
+						} else {
+							if (escaped) {
+								col.append(QUOTE);
+								escaped = false;
+							} else {
+								if (next != null && !SEPARATOR_STR.equals(next)) {
+									throw new IllegalArgumentException(format(
+										"No other token than '%s' allowed after " +
+											"quote, but found '%s'.",
+										SEPARATOR_STR, next
+									));
+								}
+
+								columns.add(col.toString());
+								col.setLength(0);
+								quoted = false;
+							}
+						}
+					} else {
+						if (previous != null && !SEPARATOR_STR.equals(previous)) {
+							throw new IllegalArgumentException(format(
+								"No other token than '%s' allowed before " +
+									"quote, but found '%s'.",
+								SEPARATOR_STR, previous
+							));
+						}
+						quoted = true;
+					}
+					break;
+				case SEPARATOR_STR:
+					if (quoted) {
+						col.append(current);
+					} else if (SEPARATOR_STR.equals(previous) || previous == null) {
+						columns.add(col.toString());
+						col.setLength(0);
+					}
+					break;
+				default:
+					col.append(current);
+					if (!quoted) {
+						columns.add(col.toString());
+						col.setLength(0);
+					}
+					break;
+			}
+		}
+
+		if (quoted) {
+			throw new IllegalArgumentException("Unbalanced quote character.");
+		}
+		if (tokens.isEmpty() ||
+			SEPARATOR_STR.equals(tokens.get(tokens.size() - 1)))
+		{
+			columns.add("");
+		}
+
+		return columns;
+	}
+
+	private static List<String> tokenize(final CharSequence value) {
+		final List<String> tokens = new ArrayList<>();
+		int pos = 0;
+		final StringBuilder token = new StringBuilder(64);
+		for (int i = 0; i < value.length(); ++i) {
+			final char c = value.charAt(i);
+			if (isTokenSeparator(c)) {
+				if (token.length() > 0) {
+					tokens.add(token.toString());
+				}
+				tokens.add(Character.toString(c));
+				token.setLength(0);
+			} else {
+				token.append(c);
+			}
+		}
+		if (token.length() > 0) {
+			tokens.add(token.toString());
+		}
+		return tokens;
+	}
+
+	private static boolean isTokenSeparator(final char c) {
+		return c == SEPARATOR || c == QUOTE;
+	}
+
+	/**
+	 * Joins the given columns to a CSV row string.
+	 *
+	 * @param cols the CSV columns to join
+	 * @return a new CSV row, joined from the given columns
+	 */
+	public static String joinCols(final Iterable<?> cols) {
+		final var row = new StringBuilder(64);
+
+		final var it = cols.iterator();
+		while (it.hasNext()) {
+			final var column = it.next();
+			row.append(escape(column));
+			if (it.hasNext()) {
+				row.append(SEPARATOR_STR);
+			}
+		}
+
+		return row.toString();
+	}
+
+	private static String escape(final Object value) {
+		if (value == null) {
+			return "";
+		} else {
+			var valueString = value.toString();
+			var string = valueString.replace(QUOTE_STR, DOUBLE_QUOTE_STR);
+
+			if (valueString.length() != string.length() ||
+				string.contains(SEPARATOR_STR) ||
+				string.contains("\n"))
+			{
+				return QUOTE_STR + string + QUOTE_STR;
+			} else {
+				return valueString;
+			}
+		}
+	}
+
+	/**
 	 * Splits the given {@code input} stream into a  {@code Stream} of CSV rows.
 	 * The rows are split at line breaks, as long as they are not part of a
 	 * quoted column. <em>The returned stream must be closed by the caller.</em>
@@ -256,163 +413,6 @@ public final class CSV {
 	 */
 	public static List<String> readAllLines(final Path path) throws IOException {
 		return readAllLines(path, Charset.defaultCharset());
-	}
-
-	/**
-	 * Splits a given CSV row into it's columns. It supports CSV records defined
-	 * in <a href="https://tools.ietf.org/html/rfc4180">RFC-4180</a>.
-	 *
-	 * @see <a href="https://tools.ietf.org/html/rfc4180">RFC-4180</a>
-	 * @see #joinCols(Iterable)
-	 *
-	 * @param line the CSV {@code row} to split
-	 * @return the split columns of the given CSV {@code row}
-	 * @throws IllegalArgumentException if the given {@code roes} isn't a valid
-	 *         CSV row
-	 * @throws NullPointerException if the given {@code row} is {@code null}
-	 */
-	public static List<String> splitLine(final CharSequence line) {
-		final List<String> columns = new ArrayList<>();
-
-		boolean quoted = false;
-		boolean escaped = false;
-
-		final var col = new StringBuilder();
-
-		final var tokens = tokenize(line);
-
-		for (int i = 0; i < tokens.size(); ++i) {
-			final var previous = i > 0 ? tokens.get(i - 1) : null;
-			final var current = tokens.get(i);
-			final var next = i + 1 < tokens.size() ? tokens.get(i + 1) : null;
-
-			switch (current) {
-				case QUOTE_STR:
-					if (quoted) {
-						if (QUOTE_STR.equals(next) && !escaped) {
-							escaped = true;
-						} else {
-							if (escaped) {
-								col.append(QUOTE);
-								escaped = false;
-							} else {
-								if (next != null && !SEPARATOR_STR.equals(next)) {
-									throw new IllegalArgumentException(format(
-										"No other token than '%s' allowed after " +
-											"quote, but found '%s'.",
-										SEPARATOR_STR, next
-									));
-								}
-
-								columns.add(col.toString());
-								col.setLength(0);
-								quoted = false;
-							}
-						}
-					} else {
-						if (previous != null && !SEPARATOR_STR.equals(previous)) {
-							throw new IllegalArgumentException(format(
-								"No other token than '%s' allowed before " +
-									"quote, but found '%s'.",
-								SEPARATOR_STR, previous
-							));
-						}
-						quoted = true;
-					}
-					break;
-				case SEPARATOR_STR:
-					if (quoted) {
-						col.append(current);
-					} else if (SEPARATOR_STR.equals(previous) || previous == null) {
-						columns.add(col.toString());
-						col.setLength(0);
-					}
-					break;
-				default:
-					col.append(current);
-					if (!quoted) {
-						columns.add(col.toString());
-						col.setLength(0);
-					}
-					break;
-			}
-		}
-
-		if (quoted) {
-			throw new IllegalArgumentException("Unbalanced quote character.");
-		}
-		if (tokens.isEmpty() ||
-			SEPARATOR_STR.equals(tokens.get(tokens.size() - 1)))
-		{
-			columns.add("");
-		}
-
-		return columns;
-	}
-
-	private static List<String> tokenize(final CharSequence value) {
-		final List<String> tokens = new ArrayList<>();
-		int pos = 0;
-		final StringBuilder token = new StringBuilder(64);
-		for (int i = 0; i < value.length(); ++i) {
-			final char c = value.charAt(i);
-			if (isTokenSeparator(c)) {
-				if (token.length() > 0) {
-					tokens.add(token.toString());
-				}
-				tokens.add(Character.toString(c));
-				token.setLength(0);
-			} else {
-				token.append(c);
-			}
-		}
-		if (token.length() > 0) {
-			tokens.add(token.toString());
-		}
-		return tokens;
-	}
-
-	private static boolean isTokenSeparator(final char c) {
-		return c == SEPARATOR || c == QUOTE;
-	}
-
-	/**
-	 * Joins the given columns to a CSV row string.
-	 *
-	 * @param cols the CSV columns to join
-	 * @return a new CSV row, joined from the given columns
-	 */
-	public static String joinCols(final Iterable<?> cols) {
-		final var row = new StringBuilder(64);
-
-		final var it = cols.iterator();
-		while (it.hasNext()) {
-			final var column = it.next();
-			row.append(escape(column));
-			if (it.hasNext()) {
-				row.append(SEPARATOR_STR);
-			}
-		}
-
-		return row.toString();
-	}
-
-	private static String escape(final Object value) {
-		if (value == null) {
-			return "";
-		} else {
-			var valueString = value.toString();
-			var string = valueString.replace(QUOTE_STR, DOUBLE_QUOTE_STR);
-
-			if (valueString.length() != string.length() ||
-				string.contains(SEPARATOR_STR) ||
-				string.contains("\n"))
-			{
-				return QUOTE_STR + string + QUOTE_STR;
-			} else {
-				return valueString;
-			}
-		}
 	}
 
 	/**
