39734ea71dcfdeb5cee31981e1def35b6b17aae2
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-11-16 20:44:41 +0100

    #302: Add perf test result.

--- jenetics.incubator/src/jmh/java/io/jenetics/incubator/util/CSVPerf.java
+++ jenetics.incubator/src/jmh/java/io/jenetics/incubator/util/CSVPerf.java
@@ -1,7 +1,32 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
 package io.jenetics.incubator.util;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import com.opencsv.ICSVParser;
 import com.opencsv.RFC4180Parser;
+
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -9,11 +34,9 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ */
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -69,7 +92,8 @@ CSVPerf.splitOpenCSV  avgt   25   362,261 ±  11,799  ns/op
 
 // Linux
 /*
-Benchmark             Mode  Cnt     Score     Error  Units
-CSVPerf.split         avgt   25  1141.269 ± 115.791  ns/op
-CSVPerf.splitOpenCSV  avgt   25   466.242 ±  18.458  ns/op
+Benchmark                      Mode  Cnt     Score     Error  Units
+CSVPerf.split                  avgt   25  1095.412 ± 115.254  ns/op
+CSVPerf.splitMultiLineOpenCSV  avgt   25   459.943 ±   6.771  ns/op
+CSVPerf.splitOpenCSV           avgt   25   448.073 ±   5.421  ns/op
 */
