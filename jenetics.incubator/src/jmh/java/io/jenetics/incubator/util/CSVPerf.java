249f40314b3788475551bc04dbb9cac773197106
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2020-11-18 23:13:51 +0100

    #302: Improve performance tests.

--- jenetics.incubator/src/jmh/java/io/jenetics/incubator/util/CSVPerf.java
+++ jenetics.incubator/src/jmh/java/io/jenetics/incubator/util/CSVPerf.java
@@ -20,9 +20,13 @@
 package io.jenetics.incubator.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.opencsv.ICSVParser;
 import com.opencsv.RFC4180Parser;
@@ -42,28 +46,33 @@ import org.openjdk.jmh.annotations.State;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class CSVPerf {
 
+	private static final String DEFAULT_CHARACTERS =
+		"abcdefghijklmnopqrstuvwxyz !\"$%&/()=?`{[]}\\+~*#';.:,-_<>|@^'\t\n\r";
+
 	private static final ICSVParser PARSER = new RFC4180Parser();
 
 	@State(Scope.Benchmark)
 	public static class Lines {
 		Random random = new Random(1231234);
-		String line = CSV.join(nextRow(random));
+		String line = CSV.join(nextRow(30, random));
 	}
 
-	private static List<String> nextRow(final Random random) {
-		return List.of(
-			"" + random.nextDouble(),
-			"" + random.nextBoolean(),
-			"" + random.nextFloat(),
-			"" + random.nextInt(),
-			"" + random.nextLong(),
-			"" + random.nextLong(),
-			"" + random.nextLong(),
-			"" + random.nextLong(),
-			"" + random.nextLong(),
-			"" + random.nextLong(),
-			"" + random.nextLong()
-		);
+	private static List<String> nextRow(final int columns, final Random random) {
+		final List<String> cols = new ArrayList<>(columns);
+		for (int i = 0; i < columns; ++i) {
+			cols.add(nextString(random.nextInt(30) + 30, random));
+		}
+		return cols;
+	}
+
+	private static String nextString(final int length, final Random random) {
+		final Supplier<Character> generator = () -> DEFAULT_CHARACTERS
+			.charAt(random.nextInt(DEFAULT_CHARACTERS.length()));
+
+		return Stream.generate(generator)
+			.limit(length)
+			.map(String::valueOf)
+			.collect(Collectors.joining());
 	}
 
 	@Benchmark
@@ -92,10 +101,10 @@ CSVPerf.splitMultiLineOpenCSV  avgt   25  376,209 ± 24,265  ns/op
 CSVPerf.splitOpenCSV           avgt   25  363,144 ±  7,188  ns/op
 */
 
-// Linux
+// Linux: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz
 /*
 Benchmark                      Mode  Cnt     Score     Error  Units
-CSVPerf.split                  avgt   25  1095.412 ± 115.254  ns/op
-CSVPerf.splitMultiLineOpenCSV  avgt   25   459.943 ±   6.771  ns/op
-CSVPerf.splitOpenCSV           avgt   25   448.073 ±   5.421  ns/op
+CSVPerf.split                  avgt   25  7749.107 ± 228.656  ns/op
+CSVPerf.splitMultiLineOpenCSV  avgt   25  5967.683 ± 124.829  ns/op
+CSVPerf.splitOpenCSV           avgt   25  5879.939 ± 121.605  ns/op
 */
