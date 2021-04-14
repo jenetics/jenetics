9cd047cd1a50ab7ebe50e283faa801ee3bfe2c8a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-04-14 20:24:26 +0200

    Set the 'targetCompatibility' to the actual build JDK.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -37,5 +37,5 @@ repositories {
 
 configure<JavaPluginConvention> {
 	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.current()
 }
