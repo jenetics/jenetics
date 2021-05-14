b9f863f2ca2f4ceda1db7437b9edd1a9e16fde7c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-05-14 23:00:44 +0200

    #767: Add 'TreeGenerator' interface.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/grammar/SentenceGenerator.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/grammar/SentenceGenerator.java
@@ -3,18 +3,14 @@ package io.jenetics.incubator.grammar;
 import java.util.List;
 import java.util.Random;
 
+import io.jenetics.incubator.grammar.Grammar.Terminal;
+
 @FunctionalInterface
 public interface SentenceGenerator {
 
-	List<Grammar.Terminal> generate(
-		final Grammar grammar,
-		final Index index
-	);
+	List<Terminal> generate(final Grammar grammar, final Index index);
 
-	default List<Grammar.Terminal> generate(
-		final Grammar grammar,
-		final Random random
-	) {
+	default List<Terminal> generate(final Grammar grammar, final Random random) {
 		return generate(grammar, random::nextInt);
 	}
 
---
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/grammar/TreeGenerator.java
@@ -0,0 +1,10 @@
+package io.jenetics.incubator.grammar;
+
+import io.jenetics.incubator.grammar.Grammar.Terminal;
+
+import io.jenetics.ext.util.Tree;
+
+@FunctionalInterface
+public interface TreeGenerator {
+	Tree<Terminal, ?> generate(final Grammar grammar, final Index index);
+}
