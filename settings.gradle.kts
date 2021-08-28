5e79a9bdb937cb5234d516fbb18021dd376d00e3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-08-28 17:16:00 +0200

    Update versions of external libraries.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,12 +35,12 @@ dependencyResolutionManagement {
 	versionCatalogs {
 		create("libs") {
 			alias("commons-math").to("org.apache.commons:commons-math3:3.6.1")
-			alias("equalsverifier").to("nl.jqno.equalsverifier:equalsverifier:3.6")
-			alias("testng").to("org.testng:testng:7.3.0")
-			alias("assertj").to("org.assertj:assertj-core:3.19.0")
+			alias("equalsverifier").to("nl.jqno.equalsverifier:equalsverifier:3.7.1")
+			alias("testng").to("org.testng:testng:7.4.0")
+			alias("assertj").to("org.assertj:assertj-core:3.20.2")
 			alias("jpx").to("io.jenetics:jpx:2.2.0")
 			alias("prngine").to("io.jenetics:prngine:1.0.2")
-			alias("rxjava").to("io.reactivex.rxjava2:rxjava:2.2.19")
+			alias("rxjava").to("io.reactivex.rxjava2:rxjava:2.2.21")
 		}
 	}
 }
