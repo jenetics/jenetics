c48a448c75aff7ed4c692737af25396ceb4b1743
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-08-31 19:15:33 +0200

    #773: Convert 'Token' to record.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/parser/Token.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/parser/Token.java
@@ -1,28 +1,9 @@
 package io.jenetics.incubator.parser;
 
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
-public final class Token {
-	private final int _type;
-	private final String _value;
-
-	public Token(final int type, final String value) {
-		_type = type;
-		_value = requireNonNull(value);
+public final record Token(int type, String value) {
+	public Token {
+		requireNonNull(value);
 	}
-
-	public int type() {
-		return _type;
-	}
-
-	public String value() {
-		return _value;
-	}
-
-	@Override
-	public String toString() {
-		return format("Token[%d, '%s']", _type, _value);
-	}
-
 }
