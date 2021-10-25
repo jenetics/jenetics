f2644a5f0d438c0c2ea291ca780c50d6e6bee55f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-10-25 19:56:07 +0200

    #805: Add some conversion methods.

--- jenetics/src/main/java/io/jenetics/util/Conversions.java
+++ jenetics/src/main/java/io/jenetics/util/Conversions.java
@@ -4,21 +4,35 @@ public final class Conversions {
 	private Conversions() {
 	}
 
-	public static int[] toIntArray(final double[] values) {
+	public static int[] toIntArray(final double... values) {
 		final var result = new int[values.length];
-		for (int i = 0; i < result.length; ++i) {
+		for (int i = values.length; --i >= 0;) {
 			result[i] = (int)values[i];
 		}
-
 		return result;
 	}
 
-	public static long[] toLongArray(final double[] values) {
+	public static long[] toLongArray(final double... values) {
 		final var result = new long[values.length];
-		for (int i = 0; i < result.length; ++i) {
+		for (int i = values.length; --i >= 0;) {
 			result[i] = (long)values[i];
 		}
+		return result;
+	}
 
+	public static Double[] box(final double... values) {
+		final Double[] result = new Double[values.length];
+		for (int i = values.length; --i >= 0;) {
+			result[i] = values[i];
+		}
+		return result;
+	}
+
+	public static double[] unbox(final Double... values) {
+		final double[] result = new double[values.length];
+		for (int i = values.length; --i >= 0;) {
+			result[i] = values[i];
+		}
 		return result;
 	}
 
