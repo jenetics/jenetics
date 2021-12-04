4cb26e7773406aba459ec5d97b7b8c3121b7a834
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-12-04 22:52:09 +0100

    Update library versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,10 +35,10 @@ dependencyResolutionManagement {
 	versionCatalogs {
 		create("libs") {
 			alias("commons-math").to("org.apache.commons:commons-math3:3.6.1")
-			alias("equalsverifier").to("nl.jqno.equalsverifier:equalsverifier:3.7.1")
+			alias("equalsverifier").to("nl.jqno.equalsverifier:equalsverifier:3.7.2")
 			alias("testng").to("org.testng:testng:7.4.0")
 			alias("assertj").to("org.assertj:assertj-core:3.20.2")
-			alias("jpx").to("io.jenetics:jpx:2.2.0")
+			alias("jpx").to("io.jenetics:jpx:2.3.0")
 			alias("prngine").to("io.jenetics:prngine:2.0.0")
 			alias("rxjava").to("io.reactivex.rxjava2:rxjava:2.2.21")
 		}
