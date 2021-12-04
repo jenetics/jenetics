9d217d1733ec53f2e9775dc3ad4eae9dee64ab68
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-12-04 08:43:13 +0100

    Update to Gradle 7.3.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 rootProject.version = Jenetics.VERSION
 
 tasks.named<Wrapper>("wrapper") {
-	gradleVersion = "7.3"
+	gradleVersion = "7.3.1"
 	distributionType = Wrapper.DistributionType.ALL
 }
 
