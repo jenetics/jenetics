2797a382590fd135ba2755cf6a770b31e3b6d1d0
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-12-10 19:43:47 +0100

    #767: Move 'SentenceGenerator' to 'grammar' package.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/grammar/SentenceGenerator.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/grammar/SentenceGenerator.java
@@ -1,19 +1,12 @@
 package io.jenetics.incubator.grammar;
 
 import java.util.List;
-import java.util.Random;
 
-import io.jenetics.incubator.grammar.SymbolIndex;
-import io.jenetics.incubator.grammar_old.Grammar;
-import io.jenetics.incubator.grammar_old.Grammar.Terminal;
+import io.jenetics.incubator.grammar.Cfg.Terminal;
 
 @FunctionalInterface
 public interface SentenceGenerator {
 
-	List<Terminal> generate(final Grammar grammar, final SymbolIndex index);
-
-	default List<Terminal> generate(final Grammar grammar, final Random random) {
-		return generate(grammar, random::nextInt);
-	}
+	List<Terminal> generate(final Cfg cfg, final SymbolIndex index);
 
 }
