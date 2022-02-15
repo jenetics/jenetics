e93275c37c3fa5745376ce2a557fb8474926cfea
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-02-15 21:03:27 +0100

    Update Gradle to 7.4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 rootProject.version = Jenetics.VERSION
 
 tasks.named<Wrapper>("wrapper") {
-	gradleVersion = "7.3.2"
+	gradleVersion = "7.4"
 	distributionType = Wrapper.DistributionType.ALL
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,18 +30,17 @@ pluginManagement {
 	}
 }
 
-enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
 	versionCatalogs {
 		create("libs") {
-			alias("assertj").to("org.assertj:assertj-core:3.20.2")
-			alias("commons-math").to("org.apache.commons:commons-math3:3.6.1")
-			alias("equalsverifier").to("nl.jqno.equalsverifier:equalsverifier:3.7.2")
-			alias("guava").to("com.google.guava:guava:31.0.1-jre")
-			alias("jpx").to("io.jenetics:jpx:2.3.0")
-			alias("prngine").to("io.jenetics:prngine:2.0.0")
-			alias("rxjava").to("io.reactivex.rxjava2:rxjava:2.2.21")
-			alias("testng").to("org.testng:testng:7.4.0")
+			library("assertj", "org.assertj:assertj-core:3.20.2")
+			library("commons-math", "org.apache.commons:commons-math3:3.6.1")
+			library("equalsverifier", "nl.jqno.equalsverifier:equalsverifier:3.7.2")
+			library("guava", "com.google.guava:guava:31.0.1-jre")
+			library("jpx", "io.jenetics:jpx:2.3.0")
+			library("prngine", "io.jenetics:prngine:2.0.0")
+			library("rxjava", "io.reactivex.rxjava2:rxjava:2.2.21")
+			library("testng", "org.testng:testng:7.4.0")
 		}
 	}
 }
