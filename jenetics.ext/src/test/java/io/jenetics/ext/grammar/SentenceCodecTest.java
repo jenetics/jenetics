6d6b5dbeb844ff6bf0da9ee1002e5aa321c4ec93
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-03-14 21:32:32 +0100

    #815: Add 'SentenceCodecTest' class.

---
+++ jenetics.ext/src/test/java/io/jenetics/ext/grammar/SentenceCodecTest.java
@@ -0,0 +1,20 @@
+package io.jenetics.ext.grammar;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import io.jenetics.IntegerGene;
+import io.jenetics.engine.Codec;
+
+import io.jenetics.ext.grammar.Cfg.Terminal;
+
+public class SentenceCodecTest {
+
+	@Test
+	public void create() {
+		final Cfg<String> cfg = null;
+		Codec<List<Terminal<String>>, IntegerGene> codec =
+			new SentenceCodec<>(cfg, null, null);
+	}
+}
