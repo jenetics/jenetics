cf1cb030c1875bc09a58d104ed31a5e673d0705c
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-03-23 19:57:30 +0100

    #815: Add Cfg unit tests.

--- jenetics.ext/src/test/java/io/jenetics/ext/grammar/CfgTest.java
+++ jenetics.ext/src/test/java/io/jenetics/ext/grammar/CfgTest.java
@@ -21,14 +21,18 @@ package io.jenetics.ext.grammar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Set;
 
 import org.testng.annotations.Test;
 
+import io.jenetics.ext.grammar.Cfg.Expression;
 import io.jenetics.ext.grammar.Cfg.NonTerminal;
+import io.jenetics.ext.grammar.Cfg.Rule;
 import io.jenetics.ext.grammar.Cfg.Symbol;
+import io.jenetics.ext.grammar.Cfg.Terminal;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
@@ -91,21 +95,187 @@ public class CfgTest {
 			<num>  ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 			"""
 		);
+		assertThat(getSymbolInstances(cfg).size()).isEqualTo(22);
 
-		final List<Symbol<String>> symbols = cfg.rules().stream()
-			.flatMap(rule -> Stream.concat(
-					Stream.of(rule.start()),
-					rule.alternatives().stream()
-						.flatMap(expr -> expr.symbols().stream())
+		final Cfg<String> cfg2 = new Cfg<>(
+			List.of(
+				new NonTerminal<>("expr"),
+				new NonTerminal<>("num"),
+				new NonTerminal<>("var"),
+				new NonTerminal<>("op")
+			),
+			List.of(
+				new Terminal<>("(", "("),
+				new Terminal<>(")", ")"),
+				new Terminal<>("+", "+"),
+				new Terminal<>("-", "-"),
+				new Terminal<>("*", "*"),
+				new Terminal<>("/", "/"),
+				new Terminal<>("x", "x"),
+				new Terminal<>("y", "y"),
+				new Terminal<>("0", "0"),
+				new Terminal<>("1", "1"),
+				new Terminal<>("2", "2"),
+				new Terminal<>("3", "3"),
+				new Terminal<>("4", "4"),
+				new Terminal<>("5", "5"),
+				new Terminal<>("6", "6"),
+				new Terminal<>("7", "7"),
+				new Terminal<>("8", "8"),
+				new Terminal<>("9", "9")
+			),
+			List.of(
+				new Rule<>(
+					new NonTerminal<>("expr"),
+					List.of(
+						new Expression<>(
+							List.of(
+								new NonTerminal<>("num")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new NonTerminal<>("var")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("(", "("),
+								new NonTerminal<>("expr"),
+								new NonTerminal<>("op"),
+								new NonTerminal<>("expr"),
+								new Terminal<>(")", ")")
+							)
+						)
+					)
+				),
+				new Rule<>(
+					new NonTerminal<>("op"),
+					List.of(
+						new Expression<>(
+							List.of(
+								new Terminal<>("+", "+")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("-", "-")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("*", "*")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("/", "/")
+							)
+						)
+					)
+				),
+				new Rule<>(
+					new NonTerminal<>("var"),
+					List.of(
+						new Expression<>(
+							List.of(
+								new Terminal<>("x", "x")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("y", "y")
+							)
+						)
+					)
+				),
+				new Rule<>(
+					new NonTerminal<>("num"),
+					List.of(
+						new Expression<>(
+							List.of(
+								new Terminal<>("0", "0")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("1", "1")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("2", "2")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("3", "3")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("4", "4")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("5", "5")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("6", "6")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("7", "7")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("8", "8")
+							)
+						),
+						new Expression<>(
+							List.of(
+								new Terminal<>("9", "9")
+							)
+						)
+					)
 				)
-			)
-			.toList();
+			),
+			new NonTerminal<>("expr")
+		);
 
-		assertThat(symbols.size()).isEqualTo(27);
+		assertThat(cfg).isEqualTo(cfg2);
+		assertThat(getSymbolInstances(cfg2).size()).isEqualTo(50);
+		assertThat(getUniqueSymbols(cfg2).size()).isEqualTo(22);
 
-		final var map = new IdentityHashMap<>();
-		symbols.forEach(s -> map.put(s, ""));
-		assertThat(map.size()).isEqualTo(22);
+		final var cfg3 = Cfg.of(cfg2.rules());
+		assertThat(cfg3).isEqualTo(cfg2);
+		assertThat(getSymbolInstances(cfg3).size()).isEqualTo(22);
+	}
+
+	private static Set<Symbol<?>> getSymbolInstances(final Cfg<?> cfg) {
+		final var symbols = new IdentityHashMap<Symbol<?>, String>();
+		cfg.nonTerminals().forEach(nt -> symbols.put(nt, ""));
+		cfg.terminals().forEach(t -> symbols.put(t, ""));
+		symbols.put(cfg.start(), "");
+		for (var rule : cfg.rules()) {
+			symbols.put(rule.start(), "");
+			rule.alternatives().forEach(expr ->
+				expr.symbols().forEach(s ->
+					symbols.put(s, "")
+				)
+			);
+		}
+
+		return symbols.keySet();
+	}
+
+	private static Set<Symbol<?>> getUniqueSymbols(final Cfg<?> cfg) {
+		return new HashSet<>(getSymbolInstances(cfg));
 	}
 
 	/*
