b8405d33a73cb62352852903b1714683288e61b0
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-04-01 20:00:22 +0200

    Simplify code.

--- jenetics.ext/src/main/java/io/jenetics/ext/util/TreeNode.java
+++ jenetics.ext/src/main/java/io/jenetics/ext/util/TreeNode.java
@@ -295,20 +295,20 @@ public final class TreeNode<T>
 		final Optional<TreeNode<T>> old = childAtPath(path);
 		final Optional<TreeNode<T>> parent = old.flatMap(TreeNode::parent);
 
-		if (parent.isPresent()) {
-			parent.orElseThrow(AssertionError::new)
-				.replace(path.get(path.length() - 1), child);
-		} else {
-			removeAllChildren();
-			value(child.value());
+		parent.ifPresentOrElse(
+			p -> p.replace(path.get(path.length() - 1), child),
+			() -> {
+				removeAllChildren();
+				value(child.value());
 
-			final ISeq<TreeNode<T>> nodes = child.childStream()
-				.collect(ISeq.toISeq());
+				// Need to create a copy of the children, before attaching it.
+				final List<TreeNode<T>> nodes = child._children != null
+					? List.copyOf(child._children)
+					: List.of();
 
-			for (TreeNode<T> node : nodes) {
-				attach(node);
+				nodes.forEach(this::attach);
 			}
-		}
+		);
 
 		return old.isPresent();
 	}
