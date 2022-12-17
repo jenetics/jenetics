8c3b54b2defe5f53e22d595150caa65f52aae51b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-12-17 23:18:49 +0100

    Upgrade Gradle to 7.6.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 rootProject.version = Jenetics.VERSION
 
 tasks.named<Wrapper>("wrapper") {
-	gradleVersion = "7.5.1"
+	gradleVersion = "7.6"
 	distributionType = Wrapper.DistributionType.ALL
 }
 
