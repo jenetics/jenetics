fbd43c0b62361a3b9d0c32f93424beae9f22b467
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-04-16 21:04:45 +0200

    Update Gradle to 8.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 rootProject.version = Jenetics.VERSION
 
 tasks.named<Wrapper>("wrapper") {
-	gradleVersion = "8.0.2"
+	gradleVersion = "8.1"
 	distributionType = Wrapper.DistributionType.ALL
 }
 
