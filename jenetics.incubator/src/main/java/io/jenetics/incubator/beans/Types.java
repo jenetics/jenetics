d7b9d54407cc9d6fc6867c03fd44f38844382ccb
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-07-31 21:47:59 +0200

    Improve type handling.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

--- jenetics.incubator/src/main/java/io/jenetics/incubator/beans/Types.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/beans/Types.java
@@ -1,3 +1,22 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
 package io.jenetics.incubator.beans;
 
 import java.lang.constant.Constable;
@@ -6,12 +25,20 @@ import java.lang.reflect.Type;
 import java.time.temporal.TemporalAccessor;
 import java.util.List;
 
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ * @version !__version__!
+ * @since !__version__!
+ */
 public final class Types {
 	private Types() {
 	}
 
-	public record ArrayType(Class<?> arrayType, Class<?> componentType) {
-		public static Object of(final Type type) {
+	public sealed interface Trait {
+	}
+
+	public record ArrayType(Class<?> arrayType, Class<?> componentType) implements Trait {
+		public static Trait of(final Type type) {
 			if (type instanceof Class<?> arrayType &&
 				arrayType.isArray() &&
 				!arrayType.getComponentType().isPrimitive())
@@ -23,8 +50,8 @@ public final class Types {
 		}
 	}
 
-	public record ListType(Class<?> listType, Class<?> componentType) {
-		public static Object of(final Type type) {
+	public record ListType(Class<?> listType, Class<?> componentType) implements Trait {
+		public static Trait of(final Type type) {
 			if (type instanceof ParameterizedType parameterizedType &&
 				parameterizedType.getRawType() instanceof Class<?> listType &&
 				List.class.isAssignableFrom(listType))
@@ -47,8 +74,8 @@ public final class Types {
 		}
 	}
 
-	public record RecordType(Class<?> type) {
-		public static Object of(final Type type) {
+	public record RecordType(Class<?> type) implements Trait {
+		public static Trait of(final Type type) {
 			if (type instanceof Class<?> cls && cls.isRecord()) {
 				return new RecordType(cls);
 			} else {
@@ -57,8 +84,8 @@ public final class Types {
 		}
 	}
 
-	public record BeanType(Class<?> type) {
-		public static Object of(final Type type) {
+	public record BeanType(Class<?> type) implements Trait {
+		public static Trait of(final Type type) {
 			if (type instanceof ParameterizedType pt &&
 				pt.getRawType() instanceof Class<?> rt
 			) {
@@ -69,6 +96,30 @@ public final class Types {
 				return null;
 			}
 		}
+	}
+
+	public static Trait trait(final Type type) {
+		Trait trait = ArrayType.of(type);
+		if (trait != null) {
+			return trait;
+		}
+
+		trait = ListType.of(type);
+		if (trait != null) {
+			return trait;
+		}
+
+		trait = RecordType.of(type);
+		if (trait != null) {
+			return trait;
+		}
+
+		trait = BeanType.of(type);
+		if (trait != null) {
+			return trait;
+		}
+
+		return trait;
 	}
 
 	public static boolean isIdentityType(final Object object) {
