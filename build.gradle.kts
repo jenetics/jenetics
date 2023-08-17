fef752a3c46a659aa3f9f9868c970b0552e16e54
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-08-17 19:16:57 +0200

    Update Gradle to 8.3.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 rootProject.version = Jenetics.VERSION
 
 tasks.named<Wrapper>("wrapper") {
-	gradleVersion = "8.2.1"
+	gradleVersion = "8.3"
 	distributionType = Wrapper.DistributionType.ALL
 }
 
