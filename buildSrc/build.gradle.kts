f7dd5303bfa30b0ae75fa4d8f664e972fb31bc72
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-09-04 22:09:22 +0200

    Fix build scripts.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,3 +38,8 @@ repositories {
 tasks.withType<KotlinCompile>().configureEach {
 	kotlinOptions.jvmTarget = "17"
 }
+
+configure<JavaPluginExtension> {
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
