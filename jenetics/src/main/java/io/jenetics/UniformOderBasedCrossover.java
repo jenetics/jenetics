d074ae4f5de6f7f16709f7a99c813e514186da0a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2024-02-16 22:18:21 +0100

    Cleanup 'UniformOrderBasedCrossover'.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

--- jenetics/src/main/java/io/jenetics/UniformOderBasedCrossover.java
+++ jenetics/src/main/java/io/jenetics/UniformOderBasedCrossover.java
@@ -1,28 +1,45 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jenetics;
 
+import io.jenetics.internal.math.Subset;
+import io.jenetics.util.BaseSeq;
 import io.jenetics.util.MSeq;
-
-import java.util.*;
+import io.jenetics.util.RandomRegistry;
+import io.jenetics.util.Seq;
 
 /**
  * The {@code UniformOderBasedCrossover} guarantees that all {@link Gene}s
  * are found exactly once in each chromosome. No gene is duplicated by this
- * crossover. The PMX can be applied usefully in the TSP or other permutation
- * problem encodings. Permutation encoding is useful for all problems where the
- * fitness only depends on the ordering of the genes within the chromosome. This
- * is the case in many combinatorial optimization problems. Other crossover
- * operators for combinatorial optimization are:
+ * crossover. This crossover can be applied usefully in the TSP or other
+ * permutation problem encodings. Permutation encoding is useful for all problems
+ * where the fitness only depends on the ordering of the genes within the
+ * chromosome. This is the case in many combinatorial optimization problems.
+ * Other crossover operators for combinatorial optimization are:
  * <ul>
  *     <li>order crossover</li>
  *     <li>cycle crossover</li>
  *     <li>edge recombination crossover</li>
  *     <li>edge assembly crossover</li>
- * 	   <li>partially matched crossover</li>
+ *     <li>partially matched crossover</li>
  * </ul>
  * <p>
- * Within the uniform oder based crossover, a set of positions is chosen randomly.
- * The genes at the positions are reordered in the order they occur in the other
- * parent.
+ * Within the uniform order-based crossover, a set of positions is chosen
+ * randomly. The genes at the positions are reordered in the order they occur in
+ * the other parent.
  * <pre>
  *    C1 = 0123456789
  *    C2 = 9876543210
@@ -40,18 +57,20 @@ import java.util.*;
  *    C1 = 0183756429
  *    C2 = 9246573810
  * </pre>
- * For more details see: Kruse et al.. Elements of evolutionary algorithms. In Kruse et al., Computational
- * intelligence: A methodological introduction (pp. 255–285). Springer International Publishing.
- * <a href="https://doi.org/10.1007/978-3-030-42227-1_12">https://doi.org/10.1007/978-3-030-42227-1_12</a>
  *
+ * @see <a href="https://doi.org/10.1007/978-3-030-42227-1_12">
+ *     Elements of evolutionary algorithms. Computational intelligence:
+ *     A methodological introduction (pp. 255–285).</a>
  * @see PermutationChromosome
  *
  * @author <a href="mailto:feichtenschlager10@gmail.com">Paul Feichtenschlager</a>
- * @version 7.2
- * @since 7.2
+ * @version 8.0
+ * @since 8.0
  */
 
-public class UniformOderBasedCrossover<T, C extends Comparable<? super C>> extends Crossover<EnumGene<T>, C> {
+public class UniformOderBasedCrossover<T, C extends Comparable<? super C>>
+	extends Crossover<EnumGene<T>, C>
+{
 
 	/**
 	 * Create a new UniformOrderBasedCrossover instance
@@ -62,66 +81,72 @@ public class UniformOderBasedCrossover<T, C extends Comparable<? super C>> exten
 	 * @throws IllegalArgumentException if the probability is not in the
 	 *         valid range of {@code [0, 1]}
 	 */
-    public UniformOderBasedCrossover(double probability) {
-        super(probability);
-    }
+	public UniformOderBasedCrossover(double probability) {
+	    super(probability);
+	}
 
 	/**
-	 * Applies uniform order based crossover to two sequences. A set of positions is chosen,
-	 * the genes at those positions are reordered as they occur in the other sequence.
+	 * Applies uniform order-based crossover to two sequences. A set of positions
+	 * is chosen, the genes at those positions are reordered as they occur in
+	 * the other sequence.
 	 *
 	 * @param that first sequence
 	 * @param other second sequence
-	 *
-	 * @throws IllegalArgumentException if the two input sequences have a different length}
+	 * @throws IllegalArgumentException if the two input sequences have a
+	 *         different length
 	 */
-    @Override
-    protected int crossover(MSeq<EnumGene<T>> that, MSeq<EnumGene<T>> other) {
-        if (that.length() != other.length()) {
-            throw new IllegalArgumentException(String.format("Required chromosomes with same length: %s != %s", that.length(), other.length()));
-        } else {
-            List<Integer> changePositions = getPositions(that.length());
-            List<EnumGene<T>> orderValuesRemovedOne = getRemovedValues(changePositions, that);
-            List<EnumGene<T>> orderValuesRemovedTwo = getRemovedValues(changePositions, other);
-            List<EnumGene<T>> reorderedOne = reorderRemovedValues(orderValuesRemovedOne, other);
-            List<EnumGene<T>> reorderedTwo = reorderRemovedValues(orderValuesRemovedTwo, that);
-            changePositionsOccurrences(that, reorderedOne, changePositions);
-            changePositionsOccurrences(other, reorderedTwo, changePositions);
-        }
-        return 0;
-    }
+	@Override
+	protected int crossover(
+		final MSeq<EnumGene<T>> that,
+		final MSeq<EnumGene<T>> other
+	) {
+		if (that.length() != other.length()) {
+			throw new IllegalArgumentException(
+				"Required chromosomes with same length: %d != %d"
+					.formatted(that.length(), other.length())
+			);
+		} else if (that.length() >= 2) {
+			final var random = RandomRegistry.random();
+			final var positions = Subset.next(
+				random, that.length(), that.length()/2
+			);
 
-    private void changePositionsOccurrences(MSeq<EnumGene<T>> sequence, List<EnumGene<T>> orderedValues, List<Integer> changePositions) {
-        int i = 0;
-        for(Integer pos : changePositions) {
-            sequence.set(pos, orderedValues.get(i));
-            i++;
-        }
-    }
+			final var removed1 = selectAt(positions, that);
+			final var removed2 = selectAt(positions, other);
 
-    private List<EnumGene<T>> reorderRemovedValues(List<EnumGene<T>> orderAtPositionOne, MSeq<EnumGene<T>> other) {
-        return orderAtPositionOne.stream()
-                .map(other::lastIndexOf)
-                .sorted(Integer::compareTo)
-                .map(other::get)
-                .toList();
-    }
+			final var reordered1 = reorder(removed1, other);
+			final var reordered2 = reorder(removed2, that);
 
-    private List<EnumGene<T>> getRemovedValues(List<Integer> changePositions, MSeq<EnumGene<T>> that) {
-        List<EnumGene<T>> orderOfValues = new LinkedList<>();
-        for(Integer pos : changePositions) {
-            orderOfValues.add(that.get(pos));
-        }
-        return orderOfValues;
-    }
+			exchange(positions, reordered1, that);
+			exchange(positions, reordered2, other);
 
-    private List<Integer> getPositions(int length) {
-        Random rand = new Random();
-        Set<Integer> sortedSet = new TreeSet<>();
-        while(sortedSet.size() < length/2) {
-            Integer newPosition = rand.nextInt(length);
-            sortedSet.add(newPosition);
-        }
-        return sortedSet.stream().toList();
-    }
+			return positions.length;
+		}
+
+		return 0;
+	}
+
+	private static <T> void
+	exchange(final int[] indexes, final BaseSeq<T> ordered, final MSeq<T> seq) {
+		for (int i = 0; i < indexes.length; ++i) {
+			seq.set(indexes[i], ordered.get(i));
+		}
+	}
+
+	private static <T> Seq<T> reorder(final Seq<T> removed, final Seq<T> seq) {
+	    return removed.stream()
+			.map(seq::lastIndexOf)
+			.sorted(Integer::compareTo)
+			.map(seq::get)
+			.collect(Seq.toSeq());
+	}
+
+	private static <T> Seq<T> selectAt(final int[] indexes, final BaseSeq<T> seq) {
+		final var selected = MSeq. <T>ofLength(indexes.length);
+		for (int i = 0; i < indexes.length; ++i) {
+			selected.set(i, seq.get(indexes[i]));
+		}
+		return selected;
+	}
+
 }
