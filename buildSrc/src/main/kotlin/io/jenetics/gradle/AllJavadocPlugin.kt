853117b2b02333d38951dbf23d4aac9402dd13e8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2024-09-07 11:10:48 +0200

    #822: Cleanup build scripts.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

--- buildSrc/src/main/kotlin/io/jenetics/gradle/AllJavadocPlugin.kt
+++ buildSrc/src/main/kotlin/io/jenetics/gradle/AllJavadocPlugin.kt
@@ -1,10 +1,29 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
 package io.jenetics.gradle
 
 import io.jenetics.gradle.dsl.allJava
 import io.jenetics.gradle.dsl.compileClasspath
 import io.jenetics.gradle.dsl.isModule
 import io.jenetics.gradle.dsl.moduleName
-import io.jenetics.gradle.dsl.snippetPath
+import io.jenetics.gradle.dsl.snippetPathString
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.ListProperty
@@ -14,7 +33,14 @@ import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
 import org.gradle.kotlin.dsl.create
 
-
+/**
+ * Plugin which configures the {@code javaodc} task and adds an {@code alljavadoc}
+ * tasks, which creates a combined Javadoc for all modules.
+ *
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ * @since !__version__!
+ * @version !__version__!
+ */
 interface AllJavadocExtension {
 
     /**
@@ -57,7 +83,7 @@ class AllJavadocPlugin : Plugin<Project> {
 
 				val opts = options as StandardJavadocDocletOptions
 
-				modules.snippetPath?.apply {
+				modules.snippetPathString?.apply {
 					opts.addStringOption("-snippet-path", this)
 				}
 
@@ -86,7 +112,7 @@ class AllJavadocPlugin : Plugin<Project> {
 					val opts = options as StandardJavadocDocletOptions
 					extensions.options.orNull?.invoke(opts)
 
-					prj.snippetPath?.apply {
+					prj.snippetPathString?.apply {
 						opts.addStringOption("-snippet-path", this)
 					}
 
--- buildSrc/src/main/kotlin/io/jenetics/gradle/dsl/Extensions.kt
+++ buildSrc/src/main/kotlin/io/jenetics/gradle/dsl/Extensions.kt
@@ -1,3 +1,22 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
 package io.jenetics.gradle.dsl
 
 import org.gradle.api.Project
@@ -9,42 +28,75 @@ import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.kotlin.dsl.extra
 import java.io.File
 
+/**
+ * Gets all source sets for java projects.
+ */
 val Project.sourceSets: SourceSetContainer get() =
 	this.extensions.getByType(JavaPluginExtension::class.java).sourceSets
 
+/**
+ * Gets the main source set of the project.
+ */
 val SourceSetContainer.main: SourceSet get() =
 	this.getByName(SourceSet.MAIN_SOURCE_SET_NAME)
 
+/**
+ * Gets the module name of the project, as configured in the build file.
+ */
 val Project.moduleName: String get() =
 	this.extra.get("moduleName")?.toString() ?: this.name
 
+/**
+ * Checks if the project is configured as a module.
+ */
 val Project.isModule: Boolean get() = this.extra.has("moduleName")
 
+/**
+ * Gets all Java sources of the project.
+ */
 val Project.allJava: FileTree get() = this.sourceSets.main.allJava.asFileTree
 
+/**
+ * Gets all Java sources from a list of projects.
+ */
 val List<Project>.allJava: FileTree get() =
 	this.map { it.allJava }
 		.reduce { a, b -> a.plus(b) }
 
+/**
+ * Gets the classpath of the main source set of the project.
+ */
 val Project.compileClasspath: FileCollection get() =
 	this.sourceSets.main.compileClasspath
 
+/**
+ * Gets the combined classpath from a list of projects.
+ */
 val List<Project>.compileClasspath: FileCollection get() =
 	this.map { it.compileClasspath }
 		.reduce { a, b -> a.plus(b) }
 
+/**
+ * Gets the _snippet_ paths fo a project.
+ */
 val Project.snippetPaths: Set<String> get() =
 	File("${project.projectDir}/src/main/java").walk()
 		.filter { file -> file.isDirectory && file.endsWith("snippet-files") }
 		.map { it.absolutePath }
 		.toSet()
 
-val Project.snippetPath: String? get() =
+/**
+ * Gets the _snippet_ path of a project as string.
+ */
+val Project.snippetPathString: String? get() =
 	this.snippetPaths
 		.joinToString(separator = File.pathSeparator)
 		.ifEmpty { null }
 
-val List<Project>.snippetPath: String? get() =
+/**
+ * Gets the _snippet_ path of a list of projects as string.
+ */
+val List<Project>.snippetPathString: String? get() =
 	this.flatMap { it.snippetPaths }
 		.joinToString(separator = File.pathSeparator)
 		.ifEmpty { null }
