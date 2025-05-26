31779b79474e8f9f4fef446f641201f2fd12a9db
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2025-05-26 20:54:02 +0200

    #946: Move statistic classes to 'io.jenetics.distassert' module.
    Signed-off-by: Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>

---
+++ jenetics.distassert/build.gradle.kts
@@ -0,0 +1,41 @@
+import io.jenetics.gradle.dsl.moduleName
+
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author:
+ *   Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ *
+ */
+
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmail.com">Franz Wilhelmstötter</a>
+ * @since !__version__!
+ * @version !__version__!
+ */
+plugins {
+	`java-library`
+}
+
+moduleName = "io.jenetics.distassert"
+description = "Statistical distribution assertion"
+
+dependencies {
+	implementation(libs.commons.statistics.distribution)
+}
+
