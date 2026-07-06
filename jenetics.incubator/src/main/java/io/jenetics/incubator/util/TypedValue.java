604e8cbbf043c9b9eacad03128f4115acc43dff2
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2026-07-06 16:18:00 +0200

    Add TypedValue test cases.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/util/TypedValue.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/util/TypedValue.java
@@ -19,7 +19,11 @@
  */
 package io.jenetics.incubator.util;
 
-import static java.util.Objects.requireNonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import static io.jenetics.incubator.util.TypedValue.box;
 import static io.jenetics.incubator.util.TypedValue.flatMap;
 import static io.jenetics.incubator.util.TypedValue.map;
@@ -27,11 +31,7 @@ import static io.jenetics.incubator.util.TypedValue.or;
 import static io.jenetics.incubator.util.TypedValue.orElse;
 import static io.jenetics.incubator.util.TypedValue.orElseGet;
 import static io.jenetics.incubator.util.TypedValue.unbox;
-
-import java.util.function.Function;
-import java.util.function.Supplier;
-
-import org.jspecify.annotations.Nullable;
+import static java.util.Objects.requireNonNull;
 
 /**
  * Interface for <em>typed</em> values. Instead of building APIs which only
@@ -59,171 +59,178 @@ import org.jspecify.annotations.Nullable;
  * Should this problem be addressed by this interface?
  * A box is a higher kinded type?
  *
- * @apiNote
- * The central metaphor of the {@code Typedvalue} interface ist <em>boxing</em>
+ * @param <V> the underlying, boxed value type
+ * @param <B> the <em>box</em> type
+ * @apiNote The central metaphor of the {@code Typedvalue} interface ist <em>boxing</em>
  * and <em>unboxing</em>. To make use of the actual value, it must be unboxed.
  * The main purpose is therefore to make the APIs (method- and class arguments)
  * safer.
- *
- * @param <V> the underlying, boxed value type
- * @param <B> the <em>box</em> type
  */
 public interface TypedValue<V, B extends Record & TypedValue<V, B>> {
 
-    /**
-     * Return the boxed value, which will never be {@code null}. If a value
-     * needs to be {@code null}, the box type will be {@code null}.
-     *
-     * @see #unbox(Record)
-     *
-     * @return the boxed value
-     */
-    V value();
+	/**
+	 * Creates a box of the given {@code type} and the given {@code value}.
+	 * If the value is {@code null}, the box will be {@code null} as well. Which
+	 * is the main difference for just calling the constructor of the box type.
+	 *
+	 * @param value the value to box
+	 * @param type  the box type
+	 * @param <V>   the underlying, boxed value type
+	 * @param <B>   the <em>box</em> type
+	 * @return a new boxed value, or {@code null}
+	 */
+	static <V, B extends Record & TypedValue<V, B>>
+	@Nullable B box(@Nullable V value, Class<B> type) {
+		requireNonNull(type);
+		if (type.isInterface()) {
+			throw new IllegalArgumentException(
+				"Box type '%s' is an interface".formatted(type.getName())
+			);
+		}
 
-    /**
-     * Creates a new box, with the given {@code fn} applied to the value. If the
-     * function returns {@code null}, the new box will be {@code null}.
-     *
-     * @param fn the function to apply to the value
-     * @return the new box, or {@code null}
-     */
-    @SuppressWarnings("unchecked")
-    default @Nullable B with(Function<? super V, ? extends @Nullable V> fn) {
+		if (value == null) {
+			return null;
+		}
+
+		try {
+			return type.getDeclaredConstructor(valueType(type))
+				.newInstance(value);
+		} catch (ReflectiveOperationException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	private static Class<?> valueType(Class<?> type) {
+		for (var component : type.getRecordComponents()) {
+			if (component.getName().equals("value")) {
+				return component.getType();
+			}
+		}
+
+		throw new IllegalArgumentException(
+			"Box type '%s' has no 'value' component.".formatted(type.getName())
+		);
+	}
+
+	/* *************************************************************************
+	 * Static methods that form the API for the typed-value usage. These methods
+	 * gracefully handle {@code null} values.
+	 * ************************************************************************/
+
+	/**
+	 * Returns the boxed value, or {@code null} if the box is {@code null}.
+	 *
+	 * @param box the boxed value
+	 * @param <V> the underlying, boxed value type
+	 * @param <B> the <em>box</em> type
+	 * @return the unboxed value, or {@code null}
+	 * @see #value()
+	 */
+	static <V, B extends Record & TypedValue<V, B>> @Nullable V unbox(@Nullable B box) {
+		return box != null ? box.value() : null;
+	}
+
+	/**
+	 * Transforms value of the given {@code box}. If the box is {@code null},
+	 * {@code null} is returned without calling the transformation function.
+	 *
+	 * @param box the box value
+	 * @param fn  the transformation function
+	 * @param <V> the underlying, boxed value type
+	 * @param <B> the <em>box</em> type
+	 * @return the transformed box
+	 * @see #with(Function)
+	 */
+	static <V, B extends Record & TypedValue<V, B>> @Nullable B map(
+		@Nullable B box,
+		Function<? super V, ? extends @Nullable V> fn
+	) {
+		requireNonNull(fn);
+		return box != null ? box.with(fn) : null;
+	}
+
+	/**
+	 * Transforms value of the given {@code box}. If the box is {@code null},
+	 * {@code null} is returned without calling the transformation function.
+	 *
+	 * @param box the box value
+	 * @param fn  the transformation function
+	 * @param <V> the underlying, boxed value type
+	 * @param <B> the <em>box</em> type
+	 * @return the transformed box
+	 */
+	static <V, B extends Record & TypedValue<V, B>> @Nullable B flatMap(
+		@Nullable B box,
+		Function<? super V, ? extends @Nullable B> fn
+	) {
+		requireNonNull(fn);
+		return box != null ? fn.apply(box.value()) : null;
+	}
+
+	/**
+	 * Return a box of the {@code other} value, if the {@code box} is {@code null}.
+	 *
+	 * @param box   the box value
+	 * @param other the other value
+	 * @param <V>   the underlying, boxed value type
+	 * @param <B>   the <em>box</em> type
+	 * @return a new value box
+	 */
+	static <V, B extends Record & TypedValue<V, B>> V
+	orElse(@Nullable B box, V other) {
+		requireNonNull(other);
+		return box != null ? box.value() : other;
+	}
+
+	/**
+	 * Return a box of the {@code other} value, if the {@code box} is {@code null}.
+	 *
+	 * @param box   the box value
+	 * @param other the other value
+	 * @param <V>   the underlying, boxed value type
+	 * @param <B>   the <em>box</em> type
+	 * @return a new value box
+	 */
+	static <V, B extends Record & TypedValue<V, B>> V
+	orElseGet(@Nullable B box, Supplier<? extends V> other) {
+		requireNonNull(other);
+		return box != null ? box.value() : other.get();
+	}
+
+	/**
+	 * Return the {@code other} box, if the {@code box} is {@code null}.
+	 *
+	 * @param box   the box value
+	 * @param other the other box
+	 * @param <V>   the underlying, boxed value type
+	 * @param <B>   the <em>box</em> type
+	 * @return a new value box
+	 */
+	static <V, B extends Record & TypedValue<V, B>> B
+	or(@Nullable B box, Supplier<? extends B> other) {
+		return box != null ? box : other.get();
+	}
+
+	/**
+	 * Return the boxed value, which will never be {@code null}. If a value
+	 * needs to be {@code null}, the box type will be {@code null}.
+	 *
+	 * @return the boxed value
+	 * @see #unbox(Record)
+	 */
+	V value();
+
+	/**
+	 * Creates a new box, with the given {@code fn} applied to the value. If the
+	 * function returns {@code null}, the new box will be {@code null}.
+	 *
+	 * @param fn the function to apply to the value
+	 * @return the new box, or {@code null}
+	 */
+	@SuppressWarnings("unchecked")
+	default @Nullable B with(Function<? super V, ? extends @Nullable V> fn) {
 		return box(fn.apply(value()), (Class<B>)getClass());
-    }
-
-    /* *************************************************************************
-     * Static methods that form the API for the typed-value usage. These methods
-     * gracefully handle {@code null} values.
-     * ************************************************************************/
-
-    /**
-     * Creates a box of the given {@code type} and the given {@code value}.
-     * If the value is {@code null}, the box will be {@code null} as well. Which
-     * is the main difference for just calling the constructor of the box type.
-     *
-     * @param value the value to box
-     * @param type the box type
-     * @return a new boxed value, or {@code null}
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>>
-    @Nullable B box(@Nullable V value, Class<B> type) {
-        requireNonNull(type);
-        if (type.isInterface()) {
-            throw new IllegalArgumentException(
-                "Box type '%s' is an interface".formatted(type.getName())
-            );
-        }
-
-        if (value == null) {
-            return null;
-        }
-
-        try {
-            return type.getConstructor(value.getClass())
-                .newInstance(value);
-        } catch (ReflectiveOperationException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-
-    /**
-     * Returns the boxed value, or {@code null} if the box is {@code null}.
-     *
-     * @see #value()
-     *
-     * @param box the boxed value
-     * @return the unboxed value, or {@code null}
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> @Nullable V unbox(@Nullable B box) {
-        return box != null ? box.value() : null;
-    }
-
-    /**
-     * Transforms value of the given {@code box}. If the box is {@code null},
-     * {@code null} is returned without calling the transformation function.
-     *
-     * @see #with(Function)
-     *
-     * @param box the box value
-     * @param fn the transformation function
-     * @return the transformed box
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> @Nullable B map(
-        @Nullable B box,
-        Function<? super V, ? extends @Nullable V> fn
-    ) {
-        requireNonNull(fn);
-        return box != null ? box.with(fn) : null;
-    }
-
-    /**
-     * Transforms value of the given {@code box}. If the box is {@code null},
-     * {@code null} is returned without calling the transformation function.
-     *
-     * @param box the box value
-     * @param fn the transformation function
-     * @return the transformed box
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> @Nullable B flatMap(
-        @Nullable B box,
-        Function<? super V, ? extends @Nullable B> fn
-    ) {
-        requireNonNull(fn);
-        return box != null ? fn.apply(box.value()) : null;
-    }
-
-    /**
-     * Return a box of the {@code other} value, if the {@code box} is {@code null}.
-     *
-     * @param box the box value
-     * @param other the other value
-     * @return a new value box
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> V
-    orElse(@Nullable B box, V other) {
-        requireNonNull(other);
-        return box != null ? box.value() : other;
-    }
-
-    /**
-     * Return a box of the {@code other} value, if the {@code box} is {@code null}.
-     *
-     * @param box the box value
-     * @param other the other value
-     * @return a new value box
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> V
-    orElseGet(@Nullable B box, Supplier<? extends V> other) {
-	    requireNonNull(other);
-	    return box != null ? box.value() : other.get();
-    }
-
-    /**
-     * Return the {@code other} box, if the {@code box} is {@code null}.
-     *
-     * @param box the box value
-     * @param other the other box
-     * @return a new value box
-     * @param <V> the underlying, boxed value type
-     * @param <B> the <em>box</em> type
-     */
-    static <V, B extends Record & TypedValue<V, B>> B
-    or(@Nullable B box, Supplier<? extends B> other) {
-        return box != null ? box : other.get();
-    }
+	}
 
 
 }
@@ -239,8 +246,7 @@ record Distance(Meter value) implements TypedValue<Meter, Distance> {
 }
 
 record ViennaToZurichDistance(Distance value)
-	implements TypedValue<Distance, ViennaToZurichDistance>
-{
+	implements TypedValue<Distance, ViennaToZurichDistance> {
 }
 
 record MilliSecond(Long value) implements TypedValue<Long, MilliSecond> {
@@ -256,7 +262,7 @@ final class Main {
 
 		var dist = box(box(3.4, Meter.class), Distance.class);
 		var fooo = new Distance(new Meter(3.4));
-		dist = dist.with(m -> m.with(v -> v*2.0));
+		dist = dist.with(m -> m.with(v -> v * 2.0));
 
 		Double val = unbox(unbox(dist));
 		dist.value().value();
@@ -271,7 +277,7 @@ final class Main {
 		Meter distant = box(6.5, Meter.class);
 
 		// Value transformation of instance value.
-		length = length.with(v -> 5*v);
+		length = length.with(v -> 5 * v);
 
 		// Access value from instance.
 		double v1 = length.value();
@@ -280,8 +286,8 @@ final class Main {
 		Double v2 = unbox(length);
 
 		// Null-safe transformation.
-		length = map(length, v -> 4*v);
-		length = flatMap(length, v -> box(v*6.5, Meter.class));
+		length = map(length, v -> 4 * v);
+		length = flatMap(length, v -> box(v * 6.5, Meter.class));
 
 		// Null-safe access if box is null.
 		length = or(length, () -> distant);
@@ -290,4 +296,3 @@ final class Main {
 
 	}
 }
-
--- jenetics.incubator/src/test/java/io/jenetics/incubator/util/TypedValueTest.java
+++ jenetics.incubator/src/test/java/io/jenetics/incubator/util/TypedValueTest.java
@@ -1,4 +1,38 @@
+/*
+ * Java Genetic Algorithm Library (@__identifier__@).
+ * Copyright (c) @__year__@ Franz Wilhelmstötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:
+ *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
+ */
 package io.jenetics.incubator.util;
+
+import static io.jenetics.incubator.util.TypedValue.box;
+import static io.jenetics.incubator.util.TypedValue.flatMap;
+import static io.jenetics.incubator.util.TypedValue.map;
+import static io.jenetics.incubator.util.TypedValue.or;
+import static io.jenetics.incubator.util.TypedValue.orElse;
+import static io.jenetics.incubator.util.TypedValue.orElseGet;
+import static io.jenetics.incubator.util.TypedValue.unbox;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.testng.annotations.Test;
 
 public class TypedValueTest {
 
@@ -9,6 +43,184 @@ public class TypedValueTest {
 	}
 
 	record Distance(Meter value) implements TypedValue<Meter, Distance> {
+	}
+
+	record NumberValue(Number value) implements TypedValue<Number, NumberValue> {
+	}
+
+	record BrokenValue(String value, String other)
+		implements TypedValue<String, BrokenValue>
+	{
+	}
+
+	@Test
+	public void boxValue() {
+		final Meter meter = box(12.5, Meter.class);
+
+		assertThat(meter).isEqualTo(new Meter(12.5));
+		assertThat(meter.value()).isEqualTo(12.5);
+	}
+
+	@Test
+	public void boxNullValue() {
+		assertThat(box(null, Meter.class)).isNull();
+	}
+
+	@Test
+	public void boxDeclaredValueType() {
+		final NumberValue value = box(23, NumberValue.class);
+
+		assertThat(value).isEqualTo(new NumberValue(23));
+		assertThat(value.value()).isInstanceOf(Integer.class);
+	}
+
+	@Test
+	public void boxWithNullType() {
+		assertThatNullPointerException()
+			.isThrownBy(() -> box(12.5, null));
+	}
+
+	@Test
+	public void boxWithMissingValueConstructor() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> box("foo", BrokenValue.class));
+	}
+
+	@Test
+	public void unboxValue() {
+		assertThat(unbox(new Meter(12.5))).isEqualTo(12.5);
+	}
+
+	@Test
+	public void unboxNullValue() {
+		final Double value = unbox(null);
+
+		assertThat(value).isNull();
+	}
+
+	@Test
+	public void unboxNestedValue() {
+		final var distance = box(box(12.5, Meter.class), Distance.class);
+
+		assertThat(unbox(unbox(distance))).isEqualTo(12.5);
+	}
+
+	@Test
+	public void withValue() {
+		assertThat(new Meter(12.5).with(value -> value * 2))
+			.isEqualTo(new Meter(25.0));
+	}
+
+	@Test
+	public void withNullResult() {
+		assertThat(new Meter(12.5).with(_ -> null)).isNull();
+	}
+
+	@Test
+	public void mapValue() {
+		assertThat(map(new Meter(12.5), value -> value * 2))
+			.isEqualTo(new Meter(25.0));
+	}
+
+	@Test
+	public void mapNullValue() {
+		final var called = new AtomicBoolean();
+
+		final Meter result = TypedValue.<Double, Meter>map(null, _ -> {
+			called.set(true);
+			return 12.5;
+		});
+
+		assertThat(result).isNull();
+		assertThat(called.get()).isFalse();
+	}
+
+	@Test
+	public void mapWithNullFunction() {
+		assertThatNullPointerException()
+			.isThrownBy(() -> map(new Meter(12.5), null));
+	}
+
+	@Test
+	public void flatMapValue() {
+		assertThat(flatMap(new Meter(12.5), value -> box(value * 2, Meter.class)))
+			.isEqualTo(new Meter(25.0));
+	}
+
+	@Test
+	public void flatMapNullValue() {
+		final var called = new AtomicBoolean();
+
+		final Meter result = TypedValue.<Double, Meter>flatMap(null, _ -> {
+			called.set(true);
+			return new Meter(12.5);
+		});
+
+		assertThat(result).isNull();
+		assertThat(called.get()).isFalse();
+	}
+
+	@Test
+	public void flatMapWithNullFunction() {
+		assertThatNullPointerException()
+			.isThrownBy(() -> flatMap(new Meter(12.5), null));
+	}
+
+	@Test
+	public void orElseValue() {
+		assertThat(orElse(new Meter(12.5), 23.0)).isEqualTo(12.5);
+	}
+
+	@Test
+	public void orElseNullValue() {
+		assertThat(orElse(null, 23.0)).isEqualTo(23.0);
+	}
+
+	@Test
+	public void orElseWithNullOther() {
+		assertThatNullPointerException()
+			.isThrownBy(() -> orElse(new Meter(12.5), null));
+	}
+
+	@Test
+	public void orElseGetValue() {
+		final var called = new AtomicBoolean();
+
+		assertThat(orElseGet(new Meter(12.5), () -> {
+			called.set(true);
+			return 23.0;
+		})).isEqualTo(12.5);
+		assertThat(called.get()).isFalse();
+	}
+
+	@Test
+	public void orElseGetNullValue() {
+		assertThat(orElseGet(null, () -> 23.0)).isEqualTo(23.0);
+	}
+
+	@Test
+	public void orElseGetWithNullSupplier() {
+		assertThatNullPointerException()
+			.isThrownBy(() -> orElseGet(new Meter(12.5), null));
+	}
+
+	@Test
+	public void orValue() {
+		final var called = new AtomicBoolean();
+		final var meter = new Meter(12.5);
+
+		assertThat(or(meter, () -> {
+			called.set(true);
+			return new Meter(23.0);
+		})).isSameAs(meter);
+		assertThat(called.get()).isFalse();
+	}
+
+	@Test
+	public void orNullValue() {
+		final var meter = new Meter(23.0);
+
+		assertThat(or(null, () -> meter)).isSameAs(meter);
 	}
 
 }
