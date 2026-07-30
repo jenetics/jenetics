d5564c00e679ff8786b84e7985edb4454b739df8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2026-07-30 09:02:14 +0200

    Implement 'StructureView' proxy.

--- jenetics.incubator/src/main/java/io/jenetics/incubator/structural/StructureView.java
+++ jenetics.incubator/src/main/java/io/jenetics/incubator/structural/StructureView.java
@@ -1,34 +1,122 @@
 package io.jenetics.incubator.structural;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static io.jenetics.incubator.structural.Structures.isStructure;
+import static java.util.Objects.requireNonNull;
 
 public class StructureView {
 	private StructureView() {
 	}
 
 	public static <T> T of(Map<String, Object> store, Class<T> type) {
+		requireNonNull(store);
+		requireNonNull(type);
 		Structures.check(type);
 
 		final var instance = Proxy.newProxyInstance(
 			type.getClassLoader(),
 			new Class<?>[]{ type },
-			(proxy, method, args) -> {
-				final var value = store.get(method.getName());
-				return switch (value) {
-					case Map<?, ?> map when isStructure(method.getReturnType()) -> {
-						@SuppressWarnings("unchecked")
-						final var str = (Map<String, Object>)map;
-						yield of(str, method.getReturnType());
-					}
-					default -> value;
-				};
-			}
+			new Handler(store, type)
 		);
 
 		return type.cast(instance);
+	}
+
+	private record Handler(
+		Map<String, Object> store,
+		Class<?> type
+	)
+		implements InvocationHandler
+	{
+		private Handler {
+			requireNonNull(store);
+			requireNonNull(type);
+		}
+
+		@Override
+		public Object invoke(
+			final Object proxy,
+			final Method method,
+			final Object[] args
+		) {
+			return method.getDeclaringClass() == Object.class
+				? invokeObject(method, args)
+				: invokeComponent(method);
+		}
+
+		private Object invokeObject(final Method method, final Object[] args) {
+			return switch (method.getName()) {
+				case "toString" -> toString();
+				case "hashCode" -> hashCode();
+				case "equals" -> equals(args[0]);
+				default -> throw new AssertionError("Unknown Object method: " + method);
+			};
+		}
+
+		private Object invokeComponent(final Method method) {
+			final var value = store.get(method.getName());
+			return switch (value) {
+				case Map<?, ?> map when isStructure(method.getReturnType()) -> {
+					@SuppressWarnings("unchecked")
+					final var str = (Map<String, Object>)map;
+					yield of(str, method.getReturnType());
+				}
+				default -> value;
+			};
+		}
+
+		@Override
+		public boolean equals(final Object obj) {
+			return obj instanceof Proxy proxy &&
+				Proxy.getInvocationHandler(proxy) instanceof Handler handler &&
+				type.equals(handler.type) &&
+				store.equals(handler.store);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(type, store);
+		}
+
+		@Override
+		public String toString() {
+			return format(type, store);
+		}
+	}
+
+	private static String format(
+		final Class<?> type,
+		final Map<String, Object> store
+	) {
+		final var components = Structures.components(type).stream()
+			.collect(Collectors.toMap(Method::getName, Method::getReturnType));
+
+		return store.entrySet().stream()
+			.filter(entry -> components.containsKey(entry.getKey()))
+			.map(entry ->
+				"%s=%s".formatted(
+					entry.getKey(),
+					format(components.get(entry.getKey()), entry.getValue())
+				)
+			)
+			.collect(Collectors.joining(", ", type.getSimpleName() + "[", "]"));
+	}
+
+	private static Object format(final Class<?> type, final Object value) {
+		return switch (value) {
+			case Map<?, ?> map when isStructure(type) -> {
+				@SuppressWarnings("unchecked")
+				final var str = (Map<String, Object>)map;
+				yield format(type, str);
+			}
+			default -> value;
+		};
 	}
 
 }
--- jenetics.incubator/src/test/java/io/jenetics/incubator/structural/StructureViewTest.java
+++ jenetics.incubator/src/test/java/io/jenetics/incubator/structural/StructureViewTest.java
@@ -2,26 +2,78 @@ package io.jenetics.incubator.structural;
 
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StructureViewTest {
 
 	@Test
-	public void proxy() {
-		final var ticket = StructureView.of(
-			Map.of(
-				"ticketId", "_ticket_id_",
-				"event", Map.of(
-					"id", 234234L,
-					"name", "Some fancy name"
-				)
-			),
-			Ticket.class
-		);
+	public void accessorMethodsReturnStoredValues() {
+		final var ticket = StructureView.of(ticketStore(), Ticket.class);
 
-		IO.println(ticket.ticketId());
-		IO.println(ticket.event().id());
-		IO.println(ticket.event().name());
+		assertThat(ticket.ticketId()).isEqualTo("_ticket_id_");
+		assertThat(ticket.ticketDate()).isEqualTo(LocalDate.of(2024, 4, 18));
+		assertThat(ticket.ticketType()).isEqualTo("vip");
+	}
+
+	@Test
+	public void nestedMapsAreReturnedAsStructureViews() {
+		final var ticket = StructureView.of(ticketStore(), Ticket.class);
+
+		assertThat(ticket.event().id()).isEqualTo(234234L);
+		assertThat(ticket.event().name()).isEqualTo("Some fancy name");
+	}
+
+	@Test
+	public void objectMethodsUseStructureTypeAndStore() {
+		final var store = ticketStore();
+		final var ticket = StructureView.of(store, Ticket.class);
+		final var equal = StructureView.of(ticketStore(), Ticket.class);
+		final var different = StructureView.of(ticketStore("regular"), Ticket.class);
+
+		assertThat(ticket).isEqualTo(equal);
+		assertThat(ticket).isNotEqualTo(different);
+		assertThat(ticket).isNotEqualTo(StructureView.of(store, Event.class));
+		assertThat(ticket).isNotEqualTo(null);
+		assertThat(ticket.hashCode()).isEqualTo(equal.hashCode());
+		assertThat(ticket.toString()).isEqualTo(
+			"Ticket[ticketId=_ticket_id_, ticketDate=2024-04-18, " +
+			"ticketType=vip, event=Event[id=234234, name=Some fancy name]]"
+		);
+	}
+
+	@Test
+	public void nullStoreIsRejected() {
+		assertThatExceptionOfType(NullPointerException.class)
+			.isThrownBy(() -> StructureView.of(null, Ticket.class));
+	}
+
+	@Test
+	public void nullTypeIsRejected() {
+		assertThatExceptionOfType(NullPointerException.class)
+			.isThrownBy(() -> StructureView.of(Map.of(), null));
+	}
+
+	private static Map<String, Object> ticketStore() {
+		return ticketStore("vip");
+	}
+
+	private static Map<String, Object> ticketStore(final String ticketType) {
+		final var event = new LinkedHashMap<String, Object>();
+		event.put("id", 234234L);
+		event.put("name", "Some fancy name");
+
+		final var ticket = new LinkedHashMap<String, Object>();
+		ticket.put("ticketId", "_ticket_id_");
+		ticket.put("ticketDate", LocalDate.of(2024, 4, 18));
+		ticket.put("ticketType", ticketType);
+		ticket.put("event", event);
+
+		return ticket;
 	}
 
 }
